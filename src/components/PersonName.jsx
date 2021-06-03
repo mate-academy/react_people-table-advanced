c529107078c1538baad425ec05ce8ad865a07bef
Mariia Kornieva <mariiakornieva@gmail.com>
2021-06-03 09:38:58 +0300

    Add people filtering with query string

--- src/components/PersonName.jsx
+++ src/components/PersonName.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
-import { Link, useRouteMatch } from 'react-router-dom';
+import { Link, useLocation, useRouteMatch } from 'react-router-dom';
 
 export const PersonName = ({ slug, name, sex }) => {
   const { url } = useRouteMatch();
+  const { search } = useLocation();
 
   return (
     <Link
-      to={`${url}/${slug}`}
+      to={{
+        pathname: `${url}/${slug}`,
+        search,
+      }}
       className={cn({
         'has-text-info': sex === 'm',
         'has-text-danger': sex === 'f',
--- src/pages/PeoplePage.jsx
+++ src/pages/PeoplePage.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.css';
 
-import { getPeople } from '../api';
 import { PeopleTable } from '../components/PeopleTable';
+import { getPeople } from '../api';
 
 const usePeople = () => {
   const [people, setPeople] = useState([]);
@@ -24,14 +26,68 @@ const usePeople = () => {
   return people;
 };
 
+const useSearchParams = () => {
+  const history = useHistory();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  const updateSearchParams = (key, value) => {
+    if (value) {
+      searchParams.set(key, value);
+    } else {
+      searchParams.delete(key);
+    }
+
+    history.push({ search: searchParams.toString() });
+  };
+
+  return [searchParams, updateSearchParams];
+};
+
 export const PeoplePage = () => {
   const people = usePeople();
+
+  const [searchParams, updateSearchParams] = useSearchParams();
+  const [query, setQuery] = useState(searchParams.get('query') || '');
+
+  const filteredPeople = useMemo(() => {
+    if (!query) {
+      return people;
+    }
+
+    return people.filter(({ name, motherName, fatherName }) => (
+      (name + motherName + fatherName).search(new RegExp(query, 'i')) !== -1
+    ));
+  }, [query, people]);
+
+  const handleQueryChange = (e) => {
+    const newQuery = e.target.value;
+
+    setQuery(newQuery);
+    updateSearchParams('query', newQuery);
+  };
 
   return (
     <>
       <h1 className="title">People Page</h1>
 
-      <PeopleTable people={people} />
+      <div className="field">
+        <div className="control has-icons-left">
+          <input
+            type="text"
+            className="input"
+            value={query}
+            onChange={handleQueryChange}
+            placeholder="Enter person's name"
+          />
+
+          <span className="icon is-left">
+            <i className="fas fa-search"></i>
+          </span>
+        </div>
+      </div>
+
+      <PeopleTable people={filteredPeople} />
     </>
   );
 };
