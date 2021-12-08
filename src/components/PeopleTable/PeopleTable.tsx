bc98beb1aff2fb2235ee3ed14047909545e4d6f5
VinnieJ-2k20 <ptimofejev@gmail.com>
2021-12-08 16:01:09 +0200

    add filtering by name

--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useMemo } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import { PersonFull } from '../../services/types';
 import { PeopleTableHeaders } from './PeopleTableHeaders';
 import { PeopleTableRow } from './PeopleTableRow/PeopleTableRow';
@@ -8,18 +9,66 @@ interface Props {
 }
 
 export const PeopleTable: FC<Props> = React.memo(({ people }) => {
-  return (
-    <table className="table people-table">
-      <PeopleTableHeaders />
+  const history = useHistory();
 
-      <tbody>
-        {people.map(person => (
-          <PeopleTableRow
-            person={person}
-            key={person.slug}
-          />
-        ))}
-      </tbody>
-    </table>
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const query = searchParams.get('query') || '';
+
+  const filteredPeople = useMemo(
+    () => {
+      const queryLowerCase = query.toLowerCase();
+
+      return people.filter((person) => {
+        const fields = [
+          person.name || '',
+          person.fatherName || '',
+          person.motherName || '',
+        ];
+
+        return fields.some(
+          field => field.toLowerCase().includes(queryLowerCase),
+        );
+      });
+    },
+    [query],
+  );
+
+  const handleQueryChange = useCallback(
+    (event) => {
+      searchParams.set('query', event.target.value);
+      history.push({
+        search: searchParams.toString(),
+      });
+    },
+    [],
+  );
+
+  return (
+    <div className="container">
+      <div className="columns">
+        <input
+          type="text"
+          className="input column"
+          placeholder="Type person name"
+          value={query}
+          onChange={handleQueryChange}
+        />
+      </div>
+
+      <table className="table people-table">
+        <PeopleTableHeaders />
+
+        <tbody>
+          {filteredPeople.map(person => (
+            <PeopleTableRow
+              person={person}
+              key={person.slug}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+
   );
 });
