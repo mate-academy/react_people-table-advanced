5ad8986cad28c48e3e6b1f1009a6b254967d0d40
Artem Fedoseev <afedoseev359@gmail.com>
2022-01-31 20:59:42 +0200

    Change Router basename to work with GitHub pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/react_people-table-advanced">
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
