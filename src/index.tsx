a804df02f39d416b2b3672cf1cbd775a08fafff8
Artem Fedoseev <afedoseev359@gmail.com>
2022-02-01 14:15:25 +0200

    Switch to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter basename="/react_people-table-advanced">
+  <HashRouter basename="/react_people-table-advanced">
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 );
