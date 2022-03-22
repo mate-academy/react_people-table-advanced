07c8bccb05d9ae219bd53d34aa28bcb15fbd6b93
Misha Novikov <novikov1807@gmail.com>
2022-03-22 22:30:58 +0200

    not yet...

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import {
+  Routes, Route, Link, Navigate,
+} from 'react-router-dom';
+
+import HomePage from './components/HomePage';
+import PeoplePage from './components/PeoplePage';
 
 import './App.scss';
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>People table</h1>
+    <header>
+      <nav>
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/people" className="nav-link">People</Link>
+      </nav>
+    </header>
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/people" element={<PeoplePage />} />
+
+      <Route path="/home" element={<Navigate to="/" replace />} />
+      {/* <Route path="*" element={<h1>Page Not Found</h1>} /> */}
+    </Routes>
   </div>
 );
 
