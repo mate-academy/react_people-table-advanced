b422768d90e553cc6faa3e82d3881d97bfca3c17
Olha Blokhina <olhablokhina@ip-10-10-0-199.eu-central-1.compute.internal>
2022-05-17 14:40:54 +0300

    NewTests

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -325,37 +325,37 @@ describe('Page', () => {
       .should('contain', 'Father');
 });
 
-it ('should open the table with personal data', () => {
-  cy.visit('/people');
-  person.getCell(0)
-    .should('contain', 'Carolus Haverbeke')
-    .and('contain', 'm')
-    .and('contain', '1832')
-    .and('contain', '1905')
-    .and('contain', 'Maria van Brussel')
-    .and('contain', 'Carel Haverbeke');
-  person.getCell(1)
-    .should('contain', 'Joanna Claes')
-    .and('contain', 'f')
-    .and('contain', '1876')
-    .and('contain', '1956')
-    .and('contain', 'Petrus de Milliano')
-    .and('contain', 'Sophia van Damme');
-  person.getCell(2)
-    .should('contain', 'Maria de Rycke')
-    .and('contain', 'f')
-    .and('contain', '1683')
-    .and('contain', '1724')
-    .and('contain', 'Frederik de Rycke')
-    .and('contain', 'Laurentia van Vlaenderen');
+  it ('should open the table with personal data', () => {
+    cy.visit('/people');
+    person.getCell(0)
+      .should('contain', 'Carolus Haverbeke')
+      .and('contain', 'm')
+      .and('contain', '1832')
+      .and('contain', '1905')
+      .and('contain', 'Maria van Brussel')
+      .and('contain', 'Carel Haverbeke');
+    person.getCell(1)
+      .should('contain', 'Joanna Claes')
+      .and('contain', 'f')
+      .and('contain', '1876')
+      .and('contain', '1956')
+      .and('contain', 'Petrus de Milliano')
+      .and('contain', 'Sophia van Damme');
+    person.getCell(2)
+      .should('contain', 'Maria de Rycke')
+      .and('contain', 'f')
+      .and('contain', '1683')
+      .and('contain', '1724')
+      .and('contain', 'Frederik de Rycke')
+      .and('contain', 'Laurentia van Vlaenderen');
   });
 
-it('should redirect from other URLs to the "404" page', () => {
-      cy.visit('/404');
-      cy.get('h1')
-        .should('contain', 'Page not found');
-      cy.visit('/error');
-      cy.get('h1')
-        .should('contain', 'Page not found');
+  it('should redirect from other URLs to the "404" page', () => {
+    cy.visit('/404');
+    cy.get('h1')
+      .should('contain', 'Page not found');
+    cy.visit('/error');
+    cy.get('h1')
+      .should('contain', 'Page not found');
   });
 });
