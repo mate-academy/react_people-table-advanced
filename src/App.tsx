a74ec8f6eae265d91ce3a5baabc199fa7caa6690
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-05-22 17:21:32 +0300

    The solution was implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import { Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+import PeoplePage from './components/PeoplePage/PeoplePage';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
+import Header from './components/Header/Header';
 import './App.scss';
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>People table</h1>
+    <h1 className="App__title">
+      People table application
+    </h1>
+    <Header />
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/people/*" element={<PeoplePage />} />
+      <Route path="/home" element={<Navigate to="/" />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   </div>
 );
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,14 @@
+// eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+type Person = {
+  name: string,
+  sex: string,
+  born: number,
+  died: number,
+  fatherName: string,
+  motherName: string,
+  slug: string,
+  father? : Person,
+  mother? : Person,
+};
