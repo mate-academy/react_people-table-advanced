de04f609a59e4c8c3a8b4d2cc907cb4c4815ebd1
Daria Veretyak <dariaveretyak@gmail.com>
2022-07-11 23:50:41 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Header } from './components/Header';
+import { PeoplePage } from './components/PeoplePage';
+import { HomePage } from './components/HomePage';
+import { NotFoundPage } from './components/NotFoundPage';
 import './App.scss';
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/people" element={<PeoplePage />}>
+          <Route path=":slug" element={<PeoplePage />} />
+        </Route>
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={NotFoundPage} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
