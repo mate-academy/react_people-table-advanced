f1a79242e218cc3c89dce4f1718ebe41cc4816e9
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-12 21:21:23 +0300

    Created helper functions for filtering and sorting

---
+++ src/utils/sortHelper.ts
@@ -0,0 +1,37 @@
+import { Person } from '../types';
+
+type TSortPeople = (people: Person[], key: string) => Person[];
+
+const sortByKey: TSortPeople = (people, key) => {
+  switch (key) {
+    case 'born':
+    case 'died':
+      return [...people].sort((a, b) => Number(a[key]) - Number(b[key]));
+    case 'name':
+    case 'sex':
+      return [...people].sort((a, b) => (
+        String(a[key]).localeCompare(String(b[key]))
+      ));
+    default:
+      return people;
+  }
+};
+
+export const sortPeople = (
+  peopleToSort: Person[],
+  searchParams: URLSearchParams,
+) => {
+  const sort = searchParams.get('sort') || '';
+  const order = searchParams.get('order') || '';
+  let sorted = [...peopleToSort];
+
+  if (sort) {
+    sorted = sortByKey(sorted, sort);
+  }
+
+  if (order) {
+    sorted.reverse();
+  }
+
+  return sorted;
+};
