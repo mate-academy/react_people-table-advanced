2e32debe1901fb88084ca05f0e5fb87b38af233b
Andriy Antonuk <antonandriy@ukr.net>
2022-10-21 12:36:19 +0300

    write code

--- src/components/PeopleFilters.tsx
+++ src/components/PeopleFilters.tsx
@@ -1,12 +1,62 @@
+import classNames from 'classnames';
+import { Link, useSearchParams } from 'react-router-dom';
+
 export const PeopleFilters = () => {
+  const listCenturies = ['16', '17', '18', '19', '20'];
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sex = searchParams.get('sex') || null;
+  const query = searchParams.get('query') || '';
+  const centuries = searchParams.getAll('centuries');
+
+  const createSex = (a: string | null) => {
+    if (a === null) {
+      searchParams.delete('sex');
+    } else {
+      searchParams.set('sex', a);
+    }
+
+    return searchParams.toString();
+  };
+
+  const createQuery = (quer: string) => {
+    if (quer === '') {
+      searchParams.delete('query');
+    } else {
+      searchParams.set('query', quer);
+    }
+
+    setSearchParams(searchParams);
+  };
+
   return (
     <nav className="panel">
       <p className="panel-heading">Filters</p>
 
       <p className="panel-tabs" data-cy="SexFilter">
-        <a className="is-active" href="#/people">All</a>
-        <a className="" href="#/people?sex=m">Male</a>
-        <a className="" href="#/people?sex=f">Female</a>
+        <Link
+          className={classNames({ 'is-active': sex === null })}
+          to={{
+            search: createSex(null),
+          }}
+        >
+          All
+        </Link>
+        <Link
+          className={classNames({ 'is-active': sex === 'm' })}
+          to={{
+            search: createSex('m'),
+          }}
+        >
+          Male
+        </Link>
+        <Link
+          className={classNames({ 'is-active': sex === 'f' })}
+          to={{
+            search: createSex('f'),
+          }}
+        >
+          Female
+        </Link>
       </p>
 
       <div className="panel-block">
@@ -16,6 +66,10 @@ export const PeopleFilters = () => {
             type="search"
             className="input"
             placeholder="Search"
+            value={query}
+            onChange={(event) => {
+              createQuery(event.target.value);
+            }}
           />
 
           <span className="icon is-left">
@@ -27,55 +81,29 @@ export const PeopleFilters = () => {
       <div className="panel-block">
         <div className="level is-flex-grow-1 is-mobile" data-cy="CenturyFilter">
           <div className="level-left">
-            <a
-              data-cy="century"
-              className="button mr-1"
-              href="#/people?centuries=16"
-            >
-              16
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=17"
-            >
-              17
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=18"
-            >
-              18
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=19"
-            >
-              19
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1"
-              href="#/people?centuries=20"
-            >
-              20
-            </a>
+            {listCenturies.map(centurie => (
+              <Link
+                key={centurie}
+                data-cy="century"
+                className={classNames(
+                  'button mr-1',
+                  { 'is-info': centuries.includes(centurie) },
+                )}
+                to="/"
+              >
+                {centurie}
+              </Link>
+            ))}
           </div>
 
           <div className="level-right ml-4">
-            <a
+            <Link
               data-cy="centuryALL"
               className="button is-success is-outlined"
-              href="#/people"
+              to="/people"
             >
               All
-            </a>
+            </Link>
           </div>
         </div>
       </div>
