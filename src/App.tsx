05d1443e7f639aa6cffc9a2bda15689897f9b993
Andriy Nosov <nosovandriy@gmail.com>
2022-11-09 19:16:45 +0200

    Solution ver 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { PeoplePage } from './components/PeoplePage';
+import {
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
 import { Navbar } from './components/Navbar';
 
 import './App.scss';
+import { PeoplePage } from './components/PeoplePage';
 
 export const App = () => {
   return (
@@ -10,9 +16,31 @@ export const App = () => {
 
       <div className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <Routes>
+            <Route
+              path="/"
+              element={<h1 className="title">Home Page</h1>}
+            />
+            <Route
+              path="home"
+              element={<Navigate to="/" replace />}
+            />
+            <Route path="people">
+              <Route
+                index
+                element={<PeoplePage />}
+              />
+              <Route
+                path=":personId"
+                element={<PeoplePage />}
+              />
+            </Route>
+
+            <Route
+              path="*"
+              element={<h1 className="title">Page not found</h1>}
+            />
+          </Routes>
         </div>
       </div>
     </div>
