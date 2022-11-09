997bbc1d14ea8959d485a1e36f0113d772ed51ac
Danil <dasha8854567@gmail.com>
2022-11-09 12:27:34 +0200

    commit1

--- src/components/PeopleFilters.tsx
+++ src/components/PeopleFilters.tsx
@@ -1,12 +1,74 @@
-export const PeopleFilters = () => {
+import React from 'react';
+import { Link, URLSearchParamsInit } from 'react-router-dom';
+import classNames from 'classnames';
+
+import { getSearchWith } from '../utils/searchHelper';
+
+import { Person } from '../types/Person';
+
+type SexLinkProps = {
+  searchParams: URLSearchParams
+  sex: string
+  text: string
+  to: string
+};
+
+const SexLink: React.FC<SexLinkProps> = ({
+  searchParams,
+  sex,
+  text,
+  to,
+}) => (
+  <Link
+    className={classNames({ 'is-active': sex === to })}
+    to={{ search: getSearchWith(searchParams, { sex: to || null }) }}
+  >
+    {text}
+  </Link>
+);
+
+type Props = {
+  searchParams: URLSearchParams;
+  setSearchParams: (param: URLSearchParamsInit) => void;
+  persons: Person[] | null
+};
+
+export const PeopleFilters: React.FC<Props> = ({
+  searchParams,
+  setSearchParams,
+  persons,
+}) => {
+  const query = searchParams.get('query') || '';
+  const sex = searchParams.get('sex') || '';
+  const centuries = searchParams.getAll('centuries') || [];
+
+  const centurieBorn = new Set(persons?.map(person => `${Math.ceil(person.born / 100)}`));
+
   return (
     <nav className="panel">
       <p className="panel-heading">Filters</p>
 
       <p className="panel-tabs" data-cy="SexFilter">
-        <a className="is-active" href="#/people">All</a>
-        <a className="" href="#/people?sex=m">Male</a>
-        <a className="" href="#/people?sex=f">Female</a>
+        <SexLink
+          searchParams={searchParams}
+          sex={sex}
+          text="All"
+          to=""
+        />
+
+        <SexLink
+          searchParams={searchParams}
+          sex={sex}
+          text="Male"
+          to="m"
+        />
+
+        <SexLink
+          searchParams={searchParams}
+          sex={sex}
+          text="Female"
+          to="f"
+        />
       </p>
 
       <div className="panel-block">
@@ -14,8 +76,15 @@ export const PeopleFilters = () => {
           <input
             data-cy="NameFilter"
             type="search"
+            value={query}
             className="input"
             placeholder="Search"
+            onChange={(e) => {
+              setSearchParams(getSearchWith(
+                searchParams,
+                { query: e.target.value || null },
+              ));
+            }}
           />
 
           <span className="icon is-left">
@@ -27,55 +96,51 @@ export const PeopleFilters = () => {
       <div className="panel-block">
         <div className="level is-flex-grow-1 is-mobile" data-cy="CenturyFilter">
           <div className="level-left">
-            <a
-              data-cy="century"
-              className="button mr-1"
-              href="#/people?centuries=16"
-            >
-              16
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=17"
-            >
-              17
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=18"
-            >
-              18
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=19"
-            >
-              19
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1"
-              href="#/people?centuries=20"
-            >
-              20
-            </a>
+            {Array.from(centurieBorn).sort().map(centurie => {
+              return (
+                <Link
+                  key={centurie}
+                  data-cy="century"
+                  className={classNames(
+                    'button mr-1',
+                    { 'is-info': centuries.includes(centurie) },
+                  )}
+                  to={{
+                    search: getSearchWith(
+                      searchParams,
+                      {
+                        centuries: centuries.includes(centurie)
+                          ? [...centuries.filter(centuriesItem => {
+                            return centuriesItem !== centurie;
+                          })]
+                          : [...centuries, centurie],
+                      },
+                    ),
+                  }}
+                >
+                  {centurie}
+                </Link>
+              );
+            })}
           </div>
 
           <div className="level-right ml-4">
-            <a
+            <Link
               data-cy="centuryALL"
-              className="button is-success is-outlined"
-              href="#/people"
+              className={classNames(
+                'button',
+                { 'is-success': centuries.length === 0 },
+                { 'is-outlined': centuries.length !== 0 },
+              )}
+              to={{
+                search: getSearchWith(
+                  searchParams,
+                  { centuries: [] },
+                ),
+              }}
             >
               All
-            </a>
+            </Link>
           </div>
         </div>
       </div>
--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,81 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
 
+import { Person } from '../types/Person';
+
+import { getPeople } from '../api';
+
 export const PeoplePage = () => {
+  const [persons, setPersons] = useState<Person[] | null>(null);
+  const [personsRender, setPersonsRender] = useState<Person[] | null>(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const query = searchParams.get('query') || '';
+  const sex = searchParams.get('sex') || '';
+  const centuries = searchParams.getAll('centuries') || [];
+  const sort = searchParams.get('sort') || '';
+  const order = searchParams.get('order') || '';
+
+  useEffect(() => {
+    getPeople()
+      .then(people => {
+        setPersons(people);
+        setPersonsRender(people);
+      })
+      .catch();
+  }, []);
+
+  useEffect(() => {
+    function getSortPersones(): Person[] {
+      if (persons) {
+        return persons.filter(person => {
+          if (person.motherName && person.fatherName) {
+            return (person.name.toLowerCase().includes(query.toLowerCase())
+            || person.motherName.toLowerCase().includes(query.toLowerCase())
+            || person.fatherName.toLowerCase().includes(query.toLowerCase()));
+          }
+
+          return false;
+        }).filter(person => person.sex.includes(sex))
+          .filter(person => {
+            if (centuries.length !== 0) {
+              return centuries.includes(`${Math.ceil(person.born / 100)}`);
+            }
+
+            return true;
+          }).sort((person1, person2) => {
+            switch (sort) {
+              case 'name':
+                return person2.name.localeCompare(person1.name);
+
+              case 'sex':
+                return person2.sex.localeCompare(person1.sex);
+
+              case 'born':
+                return person1.born - person2.born;
+
+              case 'died':
+                return person1.died - person2.died;
+              default:
+                return 1;
+                break;
+            }
+          });
+      }
+
+      return [];
+    }
+
+    if (order) {
+      setPersonsRender(getSortPersones());
+    } else {
+      setPersonsRender(getSortPersones().reverse());
+    }
+  }, [searchParams]);
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -10,22 +83,32 @@ export const PeoplePage = () => {
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
           <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
+            <PeopleFilters
+              searchParams={searchParams}
+              setSearchParams={setSearchParams}
+              persons={persons}
+            />
           </div>
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+              {/* <p data-cy="peopleLoadingError">Something went wrong</p>
 
               <p data-cy="noPeopleMessage">
                 There are no people on the server
               </p>
 
-              <p>There are no people matching the current search criteria</p>
+              <p>There are no people matching the current
+               search criteria</p> */}
 
-              <PeopleTable />
+              {(!persons && <Loader />)
+              || (personsRender && (
+                <PeopleTable
+                  persons={personsRender}
+                  searchParams={searchParams}
+                />
+              )) }
             </div>
           </div>
         </div>
