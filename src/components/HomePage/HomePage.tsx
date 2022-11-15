933539c6d9d38dd625059ebc28697b66ae2e27ff
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-11-15 16:07:48 +0200

    homepage title fixes

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export const HomePage: React.FC = () => {
   return (
-    <h1 className="title">Home Page</h1>
+    <div className="container">
+      <h1 className="title">Home Page</h1>
+    </div>
   );
 };
