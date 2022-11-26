31e48ce724e99323fb3f4dc488d82fb7edebbaa0
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-11-26 14:37:41 +0200

    first try

---
+++ src/components/PersonalLink.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import classNames from 'classnames';
+import { Link, useLocation } from 'react-router-dom';
+import { Person } from '../types';
+
+type Props = {
+  person: Person;
+};
+
+export const PersonalLink: FC<Props> = ({ person }) => {
+  const location = useLocation();
+  const isWomen = (personSex: string) => {
+    return personSex === 'f';
+  };
+
+  return (
+    <Link
+      to={`/people/${person.slug}/${location.search}`}
+      className={classNames(
+        { 'has-text-danger': isWomen(person.sex) },
+      )}
+    >
+      {person.name}
+    </Link>
+  );
+};
