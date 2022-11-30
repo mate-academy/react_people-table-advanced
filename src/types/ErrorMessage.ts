3f2a58f2868ec89b5001a6e10f5c7e05ad4e21af
Max Shylenko <maxshylenko@gmail.com>
2022-11-30 09:42:17 +0100

    corrected ErrorMessage

---
+++ src/types/ErrorMessage.ts
@@ -0,0 +1,6 @@
+export enum ErrorMessage {
+  None = '',
+  Empty = 'There are no people on the server',
+  Load = 'Something went wrong',
+  Search = 'There are no people matching the current search criteria',
+}
