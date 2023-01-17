1992329c2ef6635681b855cfc87765822ae330b3
Anastasiia Shyptia <shyptianastya27@gmail.com>
2023-01-17 09:03:53 +0200

    Implement code from previous task

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { getPeople } from '../api';
+import { Person } from '../types/Person';
 
-export const PeoplePage = () => {
+export const PeoplePage: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isLoadingFinish, setIsLoadingFinish] = useState(false);
+  const { selectedUser = '' } = useParams();
+
+  const loadPeople = async () => {
+    try {
+      setIsLoading(true);
+
+      const loadedPeople = await getPeople();
+
+      setPeople(loadedPeople);
+    } catch (error) {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+      setIsLoadingFinish(true);
+    }
+  };
+
+  useEffect(() => {
+    setIsError(false);
+    setIsLoadingFinish(false);
+    loadPeople();
+  }, []);
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -15,17 +47,26 @@ export const PeoplePage = () => {
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
+              {isLoading && <Loader />}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+              {isError && (
+                <p data-cy="peopleLoadingError">Something went wrong</p>
+              )}
 
-              <p data-cy="noPeopleMessage">
-                There are no people on the server
-              </p>
+              {isLoadingFinish && !people.length && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
 
-              <p>There are no people matching the current search criteria</p>
+              {/* <p>There are no people matching the current search criteria</p> */}
 
-              <PeopleTable />
+              {isLoadingFinish && people.length && (
+                <PeopleTable
+                  people={people}
+                  selectedUser={selectedUser}
+                />
+              )}
             </div>
           </div>
         </div>
---
+++ src/components/PersonLink.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import cn from 'classnames';
+import { Link } from 'react-router-dom';
+import { Person } from '../types/Person';
+
+type Props = {
+  person: Person;
+};
+
+export const PersonLink: React.FC<Props> = ({ person }) => (
+  <Link
+    to={`../${person.slug}`}
+    className={cn({ 'has-text-danger': person.sex === 'f' })}
+  >
+    {person.name}
+  </Link>
+);
