284a9be337506645a148e989391f0cd84f4c257b
Andrii Melnyk <andriimelnyq@gmail.com>
2023-01-19 18:07:54 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import { PeoplePage } from './components/PeoplePage';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { PeoplePage } from './pages/PeoplePage';
 import { Navbar } from './components/Navbar';
+import { HomePage } from './pages/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 import './App.scss';
 
@@ -10,9 +13,15 @@ export const App = () => {
 
       <div className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="home" element={<Navigate to="/" replace />} />
+            <Route path="*" element={<NotFoundPage />} />
+            <Route path="people">
+              <Route index element={<PeoplePage />} />
+              <Route path=":slug" element={<PeoplePage />} />
+            </Route>
+          </Routes>
         </div>
       </div>
     </div>
