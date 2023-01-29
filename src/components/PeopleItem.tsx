19d870767261174219577cf35fd9f71ff562a902
ysrckr <dyt.yasarcakir@gmail.com>
2023-01-30 00:53:22 +0300

    create peopleitem component

---
+++ src/components/PeopleItem.tsx
@@ -0,0 +1,16 @@
+export function PeopleItem() {
+  return (
+    <tr data-cy="person">
+      <td>
+        <a href="#/people/pieter-haverbeke-1602">Pieter Haverbeke</a>
+      </td>
+      <td>m</td>
+      <td>1602</td>
+      <td>1642</td>
+      <td>-</td>
+      <td>
+        <a href="#/people/lieven-van-haverbeke-1570">Lieven van Haverbeke</a>
+      </td>
+    </tr>
+  );
+}
