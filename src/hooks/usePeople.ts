45a9f8591cfe4b39fa83f34c86f66b81019254c1
ysrckr <dyt.yasarcakir@gmail.com>
2023-01-29 23:35:25 +0300

    create usePeople hook

---
+++ src/hooks/usePeople.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { getPeople } from '../api';
+import { Person } from '../types';
+
+export const usePeople = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+
+  const { isLoading, isError } = useQuery<Person[]>(['people'], getPeople, {
+    onSuccess: data => {
+      setPeople(
+        data.map(person => ({
+          ...person,
+          mother: data.find(mother => mother.name === person.motherName),
+          father: data.find(father => father.name === person.fatherName),
+        })),
+      );
+    },
+  });
+
+  return {
+    people,
+    isLoading,
+    isError,
+  };
+};
