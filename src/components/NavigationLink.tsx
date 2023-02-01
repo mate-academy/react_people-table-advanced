0f95623e7dbf37aad1b2016a630307bbc490cd27
Lefteris Loizides <loizides.lef@gmail.com>
2023-02-01 20:32:23 +0200

    implement NavBar

---
+++ src/components/NavigationLink.tsx
@@ -0,0 +1,27 @@
+import cn from 'classnames';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+type Nav = 'home' | 'people';
+
+type Props = {
+  path: Nav
+};
+
+export const NavigationLink: React.FC<Props> = ({ path }) => {
+  const [current, setCurrent] = useState<Nav>('home');
+
+  return (
+    <NavLink
+      to={`/${path}`}
+      onClick={() => setCurrent(path)}
+      aria-current={current === path && 'page'}
+      className={({ isActive }) => (
+        cn('navbar-item', {
+          'has-background-grey-lighter': isActive,
+        }))}
+    >
+      {path[0].toUpperCase() + path.slice(1)}
+    </NavLink>
+  );
+};
