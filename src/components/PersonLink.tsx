5e2ae9acb4b4c5920188b41cdbf8850067af15ca
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2023-02-07 11:20:19 +0100

    added functionality to deselect user on second click

--- src/components/PersonLink.tsx
+++ src/components/PersonLink.tsx
@@ -1,15 +1,21 @@
 import cn from 'classnames';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Person } from '../types';
 
 type Props = {
   person: Person
 };
 export const PersonLink:React.FC<Props> = React.memo(({ person }) => {
+  const { personSlug } = useParams();
+
+  const linkTo = person.slug === personSlug
+    ? '/people'
+    : `/people/${person.slug}`;
+
   return (
     <Link
-      to={`/people/${person.slug}`}
+      to={linkTo}
       className={cn(
         { 'has-text-danger': person.sex === 'f' },
       )}
--- src/pages/PageNotFound.tsx
+++
@@ -1,5 +0,0 @@
-export const PageNotFound = () => {
-  return (
-    <h1 className="title">Page not found</h1>
-  );
-};
