b3f60977cdd33b0f091a782a1a7025928485b3bc
olek-lytvynenko <olek.lytvynenko@gmail.com>
2023-02-22 14:22:38 +0200

    Solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import { PeoplePage } from './components/PeoplePage';
-import { Navbar } from './components/Navbar';
-
 import './App.scss';
+import {
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { PeoplePage } from './pages/PeoplePage';
+import { NavBar } from './components/Navbar';
 
 export const App = () => {
   return (
     <div data-cy="app">
-      <Navbar />
 
-      <div className="section">
-        <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
-        </div>
-      </div>
+      <NavBar />
+
+      <main className="section">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="home" element={<Navigate to="/" replace />} />
+          <Route path="/people" element={<PeoplePage />}>
+            <Route path=":slug" element={<PeoplePage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
     </div>
   );
 };
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const NotFoundPage: React.FC = () => {
+  return (
+    <div className="section">
+      <div className="container">
+        <p>Page not found</p>
+      </div>
+    </div>
+  );
+};
