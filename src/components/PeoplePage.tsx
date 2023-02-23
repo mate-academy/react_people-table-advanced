5b38bb26243ce8a59a8e81339e067c86077c49d5
Saleh BUD <saleh.7734@gmail.com>
2023-02-23 08:12:34 +0200

    Loading Data from api - table, loading,mother and father are complete

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,31 +1,99 @@
+import { useEffect, useState } from 'react';
+
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
 
+import { Person } from '../types';
+import { getPeople } from '../api';
+
+type PageState =
+  | 'loading'
+  | 'error'
+  | 'nothingFound'
+  | 'changeFilter'
+  | 'showTable';
+
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>();
+  const [pageState, setPageState] = useState<PageState>('loading');
+
+  // Here it'll take the response from API and add father and mother to
+  // each person, if they exist...
+  const addMotherAndFather = (response: Person[]): Person[] => {
+    const peopleList = response.map((person) => {
+      const findMother = person.motherName
+        ? response.filter((p) => p.name === person.motherName)[0]
+        : null;
+
+      const findFather = person.fatherName
+        ? response.filter((p) => p.name === person.fatherName)[0]
+        : null;
+
+      return {
+        ...person,
+        ...(findFather && { father: findFather }),
+        ...(findMother && { mother: findMother }),
+      };
+    });
+
+    return peopleList;
+  };
+
+  useEffect(() => {
+    getPeople()
+      .then((response) => response)
+      .then((result) => {
+        const improvedList = addMotherAndFather(result);
+
+        setPeople(improvedList);
+        setPageState('showTable');
+      })
+      .catch(() => setPageState('error'));
+  }, []);
+
   return (
     <>
       <h1 className="title">People Page</h1>
 
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
-          <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
-          </div>
+          {pageState === 'showTable' && (
+            <div className="column is-7-tablet is-narrow-desktop">
+              <PeopleFilters />
+            </div>
+          )}
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
+              {(() => {
+                switch (pageState) {
+                  case 'loading':
+                    return <Loader />;
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+                  case 'error':
+                    return (
+                      <p data-cy="peopleLoadingError">Something went wrong</p>
+                    );
 
-              <p data-cy="noPeopleMessage">
-                There are no people on the server
-              </p>
+                  case 'nothingFound':
+                    return (
+                      <p data-cy="noPeopleMessage">
+                        There are no people on the server
+                      </p>
+                    );
 
-              <p>There are no people matching the current search criteria</p>
+                  case 'changeFilter':
+                    return (
+                      <p>
+                        There are no people matching the current search criteria
+                      </p>
+                    );
 
-              <PeopleTable />
+                  default:
+                    return <PeopleTable people={people} />;
+                }
+              })()}
             </div>
           </div>
         </div>
