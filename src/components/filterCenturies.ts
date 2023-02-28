03a52558fdbadd847a622732316e0971b08b344a
svitjojo <kornotavr@gmail.com>
2023-02-28 15:15:17 +0200

    People table with filter was implemented

---
+++ src/components/filterCenturies.ts
@@ -0,0 +1,1 @@
+export const filterCenturies = ['16', '17', '18', '19', '20'];
---
+++ src/utils/getFilteredPeople.ts
@@ -0,0 +1,70 @@
+import { Person } from '../types';
+import { SexFilterType } from '../types/SexFilterType';
+import { SortType } from '../types/SortType';
+
+export const getFilteredPeople = (
+  sex: string,
+  centuries: string[],
+  query: string,
+  people: Person[],
+  sort: string | null,
+  order: string | null,
+) => {
+  let filteredPeople = [...people];
+
+  if (sort) {
+    filteredPeople.sort((p1, p2) => {
+      switch (sort) {
+        case SortType.Name:
+        case SortType.Sex:
+          return p1[sort].localeCompare(p2[sort]);
+
+        case SortType.Born:
+        case SortType.Died:
+          return p1[sort] - p2[sort];
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (order) {
+    filteredPeople.reverse();
+  }
+
+  if (sex) {
+    filteredPeople = filteredPeople.filter(person => {
+      switch (sex) {
+        case SexFilterType.Female:
+          return person.sex === 'f';
+
+        case SexFilterType.Male:
+          return person.sex === 'm';
+
+        default:
+          return true;
+      }
+    });
+  }
+
+  if (centuries.length > 0) {
+    filteredPeople = filteredPeople.filter(
+      person => centuries.includes(String(Math.ceil(person.born / 100))),
+    );
+  }
+
+  if (query) {
+    const lowerQuery = query.toLowerCase();
+
+    filteredPeople = filteredPeople.filter(
+      ({ name, fatherName, motherName }) => (
+        name.toLowerCase().includes(lowerQuery)
+        || fatherName?.toLowerCase().includes(lowerQuery)
+        || motherName?.toLowerCase().includes(lowerQuery)
+      ),
+    );
+  }
+
+  return filteredPeople;
+};
