045871b222ae53319db2629a3307727950b5df1b
Vlad Horovyi <vlad78292@gmail.com>
2023-03-01 20:38:38 +0200

    solution. not full yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { PeoplePage } from './components/PeoplePage';
 import { Navbar } from './components/Navbar';
+import { HomePage } from './components/HomePage';
 
 import './App.scss';
+// eslint-disable-next-line import/order
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { NotFoundPage } from './components/NotFoundPage';
 
 export const App = () => {
+  const location = useLocation();
+
   return (
     <div data-cy="app">
       <Navbar />
 
       <div className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <h1 className="title">{location.pathname}</h1>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+            <Route path="people">
+              <Route index element={<PeoplePage />} />
+              <Route path=":slug" element={<PeoplePage />} />
+            </Route>
+          </Routes>
         </div>
       </div>
     </div>
--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,31 +1,107 @@
-import { PeopleFilters } from './PeopleFilters';
+import { useEffect, useState } from 'react';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { getPeople } from '../api';
+import { Person } from '../types';
 import { Loader } from './Loader';
+import { PeopleFilters } from './PeopleFilters';
 import { PeopleTable } from './PeopleTable';
 
 export const PeoplePage = () => {
+  const [isLoader, setIsLoader] = useState(false);
+  const [searchParams] = useSearchParams();
+  const [people, setPeople] = useState<Person[]>([]);
+  const { slug = '' } = useParams();
+  const query = searchParams.get('query');
+  const sex = searchParams.get('sex');
+  const [isError, setIsError] = useState(false);
+  const centuries = searchParams.getAll('centuries');
+
+  const getPeopleFromServer = async () => {
+    setIsLoader(true);
+    try {
+      const peopleFromServer = await getPeople();
+
+      setPeople(peopleFromServer);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoader(false);
+    }
+  };
+
+  useEffect(() => {
+    getPeopleFromServer();
+  }, []);
+
+  const getVisiblePeople = () => {
+    let visiblePeople = [...people];
+
+    if (query) {
+      const correctQuery = query.toLowerCase();
+
+      visiblePeople = visiblePeople
+        .filter(person => person.name.toLowerCase().includes(correctQuery)
+          || person.fatherName?.toLowerCase().includes(correctQuery)
+          || person.motherName?.toLowerCase().includes(correctQuery));
+    }
+
+    if (sex) {
+      switch (sex) {
+        case 'Male':
+          visiblePeople = visiblePeople.filter(person => person.sex === 'm');
+          break;
+        case 'Female':
+          visiblePeople = visiblePeople.filter(person => person.sex === 'f');
+          break;
+        default:
+          break;
+      }
+    }
+
+    if (centuries.length) {
+      visiblePeople = visiblePeople.filter(person => {
+        const bornCentury = Math.ceil(person.born / 100).toString();
+
+        return centuries.includes(bornCentury);
+      });
+    }
+
+    return visiblePeople;
+  };
+
+  const filteredPeople = getVisiblePeople();
+
+  useEffect(() => {
+    getVisiblePeople();
+  }, [query, sex]);
+
+  if (isError) {
+    return (
+      <p data-cy="peopleLoadingError" className="has-text-danger">
+        Something went wrong
+      </p>
+    );
+  }
+
   return (
     <>
       <h1 className="title">People Page</h1>
-
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
-          <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
-          </div>
-
+          {!isLoader && (
+            <div className="column is-7-tablet is-narrow-desktop">
+              <PeopleFilters />
+            </div>
+          )}
           <div className="column">
             <div className="box table-container">
-              <Loader />
-
-              <p data-cy="peopleLoadingError">Something went wrong</p>
-
-              <p data-cy="noPeopleMessage">
-                There are no people on the server
-              </p>
-
-              <p>There are no people matching the current search criteria</p>
-
-              <PeopleTable />
+              {isLoader
+                ? (
+                  <Loader />
+                )
+                : (
+                  <PeopleTable slug={slug} people={filteredPeople} />
+                )}
             </div>
           </div>
         </div>
