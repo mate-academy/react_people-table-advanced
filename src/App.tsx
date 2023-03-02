b7c5340952679a407f450ae9afc57e0c87147494
Anton Vasyliev <magisterol963@gmail.com>
2023-03-02 23:22:44 +0200

    NotFoundPage was added

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import {
 import { TablePage } from './Pages/TablePage';
 import { HomePage } from './Pages/HomePage';
 import { NavBar } from './components/NavBar/NavBar';
+import { NotFoundPage } from './Pages/NotFoundPage';
 
 export const App: React.FC = () => {
   return (
     <div data-cy="app">
       <NavBar />
       <main className="section">
+        <Route path="*" element={<NotFoundPage />} />
+
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/home" element={<Navigate to="/" replace />} />
