d0687c53c35acb6398d32d2932641a71fc811593
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-04-19 15:12:55 +0300

    people-table-advanced was implement

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,43 @@
-import { PeopleFilters } from './PeopleFilters';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { getPeople } from '../api';
+import { Person } from '../types';
+import { getFilteredPeople, getPersonParents } from '../helpers';
+import { PeopleFilters } from './PeopleFilters';
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  const [serachParams, setSearchParams] = useSearchParams();
+  const query = serachParams.get('query') || '';
+  const sex = serachParams.get('sex') || '';
+  const centuries = serachParams.getAll('centuries') || [];
+
+  const loadPeopleFromServer = async () => {
+    try {
+      const peopleFromServer = await getPeople();
+      const peopleWithParents: Person[] = peopleFromServer.map(
+        person => getPersonParents(person, peopleFromServer),
+      );
+
+      setPeople(peopleWithParents);
+    } catch {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadPeopleFromServer();
+  }, []);
+
+  const filteredPeople = getFilteredPeople(people, query, sex, centuries);
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -10,22 +45,36 @@ export const PeoplePage = () => {
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
           <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
+            <PeopleFilters
+              searchParams={serachParams}
+              onSetSearchParams={setSearchParams}
+              query={query}
+              sex={sex}
+              centuries={centuries}
+            />
           </div>
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
+              {isLoading && <Loader />}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+              {(!isLoading && hasError) && (
+                <p data-cy="peopleLoadingError">Something went wrong</p>
+              )}
 
-              <p data-cy="noPeopleMessage">
-                There are no people on the server
-              </p>
+              {(!isLoading && !hasError && people.length === 0) && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
 
-              <p>There are no people matching the current search criteria</p>
+              {(!isLoading && !hasError && filteredPeople.length === 0) && (
+                <p>There are no people matching the current search criteria</p>
+              )}
 
-              <PeopleTable />
+              {(!isLoading && !hasError && filteredPeople.length > 0) && (
+                <PeopleTable people={filteredPeople} />
+              )}
             </div>
           </div>
         </div>
---
+++ src/helpers.ts
@@ -0,0 +1,85 @@
+import { Person } from './types';
+import { SortType } from './types/SortType';
+
+export const getPersonParents = (person: Person, people: Person[]): Person => {
+  const { fatherName, motherName } = person;
+
+  const personFather = people.find(
+    parent => parent.name === fatherName,
+  );
+
+  const personMother = people.find(
+    parent => parent.name === motherName,
+  );
+
+  return {
+    ...person,
+    father: personFather,
+    mother: personMother,
+  };
+};
+
+export const getFilteredPeople = (
+  people: Person[],
+  query: string,
+  sex: string,
+  centuries: string[],
+): Person[] => {
+  let tempCopyOfPeople = people;
+  const validQuery = query.toLowerCase().trim();
+
+  if (query) {
+    tempCopyOfPeople = tempCopyOfPeople.filter(person => (
+      person.name.toLowerCase().includes(validQuery)
+    ));
+  }
+
+  if (centuries.length > 0) {
+    tempCopyOfPeople = tempCopyOfPeople.filter(person => {
+      const personCentury = Math.ceil(person.born / 100).toString();
+
+      if (centuries.includes(personCentury)) {
+        return true;
+      }
+
+      return false;
+    });
+  }
+
+  if (sex) {
+    tempCopyOfPeople = tempCopyOfPeople.filter(person => person.sex === sex);
+  }
+
+  return tempCopyOfPeople;
+};
+
+export const getSortedPeople = (
+  people: Person[],
+  sort: SortType,
+  order: string | null,
+) => {
+  const tempCopyOfPeople = [...people];
+
+  switch (sort) {
+    case SortType.BORN:
+    case SortType.DIED:
+      tempCopyOfPeople.sort((person1, person2) => (
+        person1[sort] - person2[sort]
+      ));
+      break;
+
+    case SortType.NAME:
+    case SortType.SEX:
+      tempCopyOfPeople.sort((person1, person2) => (
+        person1[sort].localeCompare(person2[sort])
+      ));
+      break;
+
+    default:
+      break;
+  }
+
+  return order === 'desc'
+    ? tempCopyOfPeople.reverse()
+    : tempCopyOfPeople;
+};
