4580483c5ed6ae4a1caf2536dfd0bf1f3b088188
Serhii Nikolin <sergeynikolin@gmail.com>
2023-05-16 11:53:40 +0300

    Code edited 1

--- src/components/Parent.tsx
+++ src/components/Parent.tsx
@@ -11,7 +11,7 @@ export const Parent: React.FC<Props> = ({ parent, parentName }) => {
     <>
       {!!parent && <PersonLink person={parent} />}
       {!!parentName && !parent && `${parentName}`}
-      {!parentName && '-'}
+      {!parentName && (<span>-</span>)}
     </>
   );
 };
