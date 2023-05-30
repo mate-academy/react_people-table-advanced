7d80338afb5c9e25b30d69e42b3ca6dc8a90f812
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-05-30 10:59:27 +0300

    Components for home and error pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { PeoplePage } from './components/PeoplePage';
 import { Navbar } from './components/Navbar';
 import './App.scss';
+import { HomePage } from './components/HomePage';
+import { ErrorPage } from './components/ErrorPage';
 
 export const App = () => {
   return (
@@ -16,12 +18,9 @@ export const App = () => {
               <Route path=":personSlug" element={<PeoplePage />} />
             </Route>
 
-            <Route path="/" element={<h1 className="title">Home Page</h1>} />
+            <Route path="/" element={<HomePage />} />
             <Route path="home" element={<Navigate to="/" replace />} />
-            <Route
-              path="*"
-              element={<h1 className="title">Page not found</h1>}
-            />
+            <Route path="*" element={<ErrorPage />} />
           </Routes>
         </div>
       </div>
