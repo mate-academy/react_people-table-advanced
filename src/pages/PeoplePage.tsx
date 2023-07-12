ac41caf578cffce5d495c07256bef0320a40bbeb
Ivan Marchuk <imarchuk90@gmail.com>
2023-07-12 18:08:36 +0300

    Corrections

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -38,7 +38,7 @@ export const PeoplePage: FC = () => {
               {hasError
                 && <p data-cy="peopleLoadingError">Something went wrong</p>}
 
-              {!people.length && (
+              {!people.length && !isLoading && (
                 <p data-cy="noPeopleMessage">
                   There are no people on the server
                 </p>
