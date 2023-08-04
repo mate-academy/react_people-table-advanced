3ba6b7943a85d8a2b7aebbbc72acfbf8900dcb76
Eugene Kulish <genekulish13@gmail.com>
2023-08-04 10:33:00 +0300

    add router

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,10 @@
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+
+const getLinkClassName = ({ isActive }: { isActive: boolean }) => {
+  return classNames('navbar-item', { 'has-background-grey-lighter': isActive });
+};
+
 export const Navbar = () => {
   return (
     <nav
@@ -8,15 +15,16 @@ export const Navbar = () => {
     >
       <div className="container">
         <div className="navbar-brand">
-          <a className="navbar-item" href="#/">Home</a>
+          <NavLink className={getLinkClassName} to="/">
+            Home
+          </NavLink>
 
-          <a
-            aria-current="page"
-            className="navbar-item has-background-grey-lighter"
-            href="#/people"
+          <NavLink
+            className={getLinkClassName}
+            to="/people"
           >
             People
-          </a>
+          </NavLink>
         </div>
       </div>
     </nav>
---
+++ src/pages/People.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { Loader } from '../components/Loader';
+import { getPeople } from '../api';
+import { Person } from '../types';
+import { PeopleTable } from '../components/PeopleTable';
+import { PeopleFilters } from '../components/PeopleFilters';
+
+export const People: React.FC = () => {
+  const [people, setPeople] = useState<Person[] | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setIsError(false);
+
+    getPeople()
+      .then(setPeople)
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="columns is-desktop is-flex-direction-row-reverse">
+          <div className="column is-7-tablet is-narrow-desktop">
+            <PeopleFilters />
+          </div>
+
+          <div className="column">
+            <div className="box table-container">
+              {isLoading && (
+                <Loader />
+              )}
+
+              {isError && (
+                <p data-cy="peopleLoadingError" className="has-text-danger">
+                  Something went wrong
+                </p>
+              )}
+
+              <p>There are no people matching the current search criteria</p>
+
+              {!isLoading && !isError && (!people?.length
+                ? (
+                  <p data-cy="noPeopleMessage">
+                    There are no people on the server
+                  </p>
+                )
+                : (
+                  <PeopleTable people={people} />
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
