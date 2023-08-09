df121356623f73c2a634447f17cdb89ccc7435aa
Yana <yanaklymenko.od@gmail.com>
2023-08-10 00:24:57 +0300

    add task solution

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,29 @@
-export const Navbar = () => {
-  return (
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="#/">Home</a>
+import { NavLink } from 'react-router-dom';
+import cn from 'classnames';
 
-          <a
-            aria-current="page"
-            className="navbar-item has-background-grey-lighter"
-            href="#/people"
-          >
-            People
-          </a>
-        </div>
+const getLinkClass = ({ isActive }: { isActive: boolean }) => cn('navbar-item',
+  { 'has-background-grey-lighter': isActive });
+
+export const Navbar: React.FC = () => (
+  <nav
+    data-cy="nav"
+    className="navbar is-fixed-top has-shadow"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div className="container">
+      <div className="navbar-brand">
+        <NavLink className={getLinkClass} to="/">
+          Home
+        </NavLink>
+
+        <NavLink
+          className={getLinkClass}
+          to="/people"
+        >
+          People
+        </NavLink>
       </div>
-    </nav>
-  );
-};
+    </div>
+  </nav>
+);
