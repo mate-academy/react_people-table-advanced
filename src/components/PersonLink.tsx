680317c2a5de9415e5a030933d4c8b152d5600b5
Vlad Makohon <98763083+vladmakohon@users.noreply.github.com>
2023-08-09 09:38:06 +0300

    minor bug fix

--- src/components/PersonLink.tsx
+++ src/components/PersonLink.tsx
@@ -1,12 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import classNames from 'classnames';
 
 import { Person } from '../types';
 
 export const PersonLink = ({ person }: { person: Person }) => {
+  const [searchParams] = useSearchParams();
+
   return (
     <Link
-      to={`../${person.slug}`}
+      to={{ pathname: `../${person.slug}`, search: searchParams.toString() }}
       className={classNames({
         'has-text-danger': person.sex === 'f',
       })}
