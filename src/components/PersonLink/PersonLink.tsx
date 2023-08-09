86b1187900b0a87d5bc72b6f001275ca6ff621ce
Olena <pampam32121@gmail.com>
2023-08-09 07:44:45 +0300

    fix PersonLink

--- src/components/PersonLink/PersonLink.tsx
+++ src/components/PersonLink/PersonLink.tsx
@@ -15,7 +15,7 @@ export const PersonLink: React.FC<Props> = ({ person }) => {
       className={classNames({
         'has-text-danger': person.sex === 'f',
       })}
-      to={`../${person.slug}${search}`}
+      to={`/people/${person.slug}${search}`}
     >
       {person.name}
     </Link>
