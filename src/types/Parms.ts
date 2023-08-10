57e0b8c0ef2d55b8389a777df8ab97dd9d7f0fab
Daniil Postoiuk <daniil.postoyuk@gmail.com>
2023-08-10 20:09:51 +0300

    add task solution

---
+++ src/types/Parms.ts
@@ -0,0 +1,9 @@
+import { SortField } from './SortField';
+
+export type Parms = {
+  sex: string,
+  query: string,
+  centuries: string[],
+  sortField: SortField,
+  sortOrder: string,
+};
---
+++ src/utils/prepareFilteredAndSortedPeople.ts
@@ -0,0 +1,54 @@
+import { Parms, Person } from '../types';
+import { SortField } from '../types/SortField';
+
+export const prepareFilteredAndSortedPeople = (
+  people: Person[],
+  params: Parms,
+) => {
+  let filteredPeople = people;
+
+  if (params.sex) {
+    filteredPeople = people.filter(person => person.sex === params.sex);
+  }
+
+  if (params.query) {
+    const normalizeQuery = params.query.toLowerCase();
+
+    filteredPeople = people
+      .filter(({ name, motherName, fatherName }) => {
+        const byName = name.toLowerCase().includes(normalizeQuery);
+        const byMotherName = motherName?.toLowerCase().includes(normalizeQuery);
+        const byFatherName = fatherName?.toLowerCase().includes(normalizeQuery);
+
+        return byName || byMotherName || byFatherName;
+      });
+  }
+
+  if (params.centuries.length > 0) {
+    filteredPeople = people.filter(person => {
+      const century = Math.ceil(person.born / 100).toString();
+
+      return params.centuries.includes(century);
+    });
+  }
+
+  if (params.sortField) {
+    const order = params.sortOrder ? -1 : 1;
+
+    filteredPeople = [...people].sort((p1, p2) => {
+      switch (params.sortField) {
+        case SortField.Born:
+        case SortField.Died:
+          return (p1[params.sortField] - p2[params.sortField]) * order;
+        case SortField.Name:
+        case SortField.Sex:
+          return p1[params.sortField]
+            .localeCompare(p2[params.sortField]) * order;
+        default:
+          return 0;
+      }
+    });
+  }
+
+  return filteredPeople;
+};
