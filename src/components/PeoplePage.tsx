7b9a4ec2060a46e5ac9117dcd316510923109e8f
Oleh Vynarchuk <olegahmetshin54@gmail.com>
2023-08-10 13:07:59 +0300

    solution

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useMemo, useState } from 'react';
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { Person } from '../types';
+import { getPeople } from '../api';
+import { ErrorLoading } from './ErrorLoading';
+import { NoPeople } from './NoPeople';
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getPeople()
+      .then(setPeople)
+      .catch((newError) => {
+        setError(true);
+        throw newError;
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const allPeople = useMemo(() => (
+    people.map(person => {
+      const mother = people.find(findPerson => (
+        findPerson.name === person.motherName
+      ));
+
+      const father = people.find(findPerson => (
+        findPerson.name === person.fatherName
+      ));
+
+      return { ...person, mother, father };
+    })
+  ), [people]);
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -15,17 +49,14 @@ export const PeoplePage = () => {
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
+              {loading && <Loader />}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+              {error && <ErrorLoading />}
 
-              <p data-cy="noPeopleMessage">
-                There are no people on the server
-              </p>
+              {!error && !loading && people.length === 0 && <NoPeople />}
 
-              <p>There are no people matching the current search criteria</p>
-
-              <PeopleTable />
+              {people.length !== 0 && !error
+                  && <PeopleTable allPeople={allPeople} />}
             </div>
           </div>
         </div>
