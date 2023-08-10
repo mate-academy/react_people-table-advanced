0c43a934f7f878cff16dd6115a7bc62927087283
Mac <mac@MacBook-Pro-Mac.local>
2023-08-10 10:12:35 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { PeoplePage } from './components/PeoplePage';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 
 import './App.scss';
@@ -6,15 +7,12 @@ import './App.scss';
 export const App = () => {
   return (
     <div data-cy="app">
-      <Navbar />
-
-      <div className="section">
+      <main className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <Navbar />
+          <Outlet />
         </div>
-      </div>
+      </main>
     </div>
   );
 };
