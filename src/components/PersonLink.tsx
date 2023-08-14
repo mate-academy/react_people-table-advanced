1996dc221f2d73ab11fdf24b947b2009d85a2c12
Artem Andrushchenko <artem.abdrushenko213654@gmail.com>
2023-08-14 09:05:05 +0300

    add task solution

---
+++ src/components/PersonLink.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import cn from 'classnames';
+import { Person } from '../types';
+
+type Props = {
+  person: Person;
+};
+
+export const PersonLink: React.FC<Props> = ({ person }) => (
+  <Link
+    to={`/people/${person.slug}`}
+    className={cn({
+      'has-text-danger': person.sex === 'f',
+    })}
+  >
+    {person.name}
+  </Link>
+);
---
+++ src/utils/getFilteredPeople.tsx
@@ -0,0 +1,55 @@
+import { Person } from '../types';
+import { FilterParams, SortType, Order } from '../types/FilterParams';
+
+export const getFilteredPeople = (people: Person[], {
+  query,
+  centuries,
+  sex,
+  sort,
+  order,
+}: FilterParams): Person[] => {
+  let filteredPeople = [...people];
+
+  if (query.trim() !== '') {
+    const normalizedQuery = query.toLowerCase().trim();
+
+    filteredPeople = filteredPeople.filter(
+      person => person.name.toLowerCase().includes(normalizedQuery)
+        || person.fatherName?.toLowerCase().includes(normalizedQuery)
+        || person.motherName?.toLowerCase().includes(normalizedQuery),
+    );
+  }
+
+  if (centuries.length > 0) {
+    filteredPeople = filteredPeople.filter((p) => {
+      return centuries.includes(Math.ceil(p.born / 100).toString());
+    });
+  }
+
+  if (sex) {
+    filteredPeople = filteredPeople.filter((person) => person.sex === sex);
+  }
+
+  if (sort) {
+    filteredPeople.sort((p1: Person, p2: Person) => {
+      switch (sort) {
+        case SortType.Name:
+        case SortType.Sex:
+          return p1[sort].localeCompare(p2[sort]);
+
+        case SortType.Born:
+        case SortType.Died:
+          return p1[sort] - p2[sort];
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (order === Order.Desc) {
+    filteredPeople.reverse();
+  }
+
+  return filteredPeople;
+};
