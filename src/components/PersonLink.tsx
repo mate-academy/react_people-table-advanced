ff5fd6e2f04f8f75341ca493dac292162b1c398d
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-08-24 13:26:44 +0300

    people table with filter was implement

---
+++ src/components/PersonLink.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import cn from 'classnames';
+
+import { Link, useSearchParams } from 'react-router-dom';
+import { Person } from '../types';
+
+type Props = {
+  person: Person;
+};
+
+export const PersonLink: React.FC<Props> = ({ person }) => {
+  const [searchParams] = useSearchParams();
+
+  return (
+    <Link
+      to={{
+        pathname: `/people/${person.slug}`,
+        search: searchParams.toString(),
+      }}
+      className={cn({ 'has-text-danger': person.sex === 'f' })}
+    >
+      {person.name || '-'}
+    </Link>
+  );
+};
