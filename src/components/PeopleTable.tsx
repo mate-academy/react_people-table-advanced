e5760f5c84c97014bff1c0f11262b039c5635936
zipic <zpc23041988@gmail.com>
2023-08-29 12:29:19 +0300

    fix-data-cy

--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -52,7 +52,7 @@ export const PeopleTable:React.FC<Props> = ({ person, isLoading }) => {
   };
 
   if (filteredPersons.length === 0 && !isLoading) {
-    return <p>There are no people matching the current search criteria</p>;
+    return <p data-cy="noPeopleMessage">There are no people on the server</p>;
   }
 
   if (isLoading) {
