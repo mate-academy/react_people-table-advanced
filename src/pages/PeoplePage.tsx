52bcca3833079968781d5bc81a812baa72387521
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-09-03 16:33:03 +0300

    show filters after people list was loaded

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -37,7 +37,9 @@ export const PeoplePage: React.FC = () => {
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
           <div className="column is-7-tablet is-narrow-desktop">
-            <TableSideFilters />
+            {!hasError && !isLoading && (
+              <TableSideFilters />
+            )}
           </div>
           <div className="column">
             <div className="box table-container">
