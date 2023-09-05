850e3729c9c77724eb394a9919addaafbee47055
Anastasia <radchenckooo@gmail.com>
2023-09-05 21:07:52 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { PeoplePage } from './components/PeoplePage';
 import { Navbar } from './components/Navbar';
+import { HomePage } from './components/HomePage';
+import { PageNotFound } from './components/PageNotFound';
 
 import './App.scss';
 
@@ -10,9 +13,15 @@ export const App = () => {
 
       <div className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="home" element={<Navigate to="/" />} />
+            <Route path="people">
+              <Route index element={<PeoplePage />} />
+              <Route path=":slug" element={<PeoplePage />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
