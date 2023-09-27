377c383243be823cc082dac91547c72912ccbec7
Hania Nassalska <hanna.nassalska@gmail.com>
2023-09-27 23:31:40 +0200

    PageTitle

--- src/featurs/HomePage/index.tsx
+++ src/featurs/HomePage/index.tsx
@@ -1,7 +1,10 @@
+import { PageTitle } from '../../common/PageTitle';
+
 export const HomePage = () => (
   <main className="section">
     <div className="container">
-      <h1 className="title">Home Page</h1>
+      <PageTitle title="Home Page" />
     </div>
+
   </main>
 );
--- src/featurs/NotFoundPage/index.tsx
+++ src/featurs/NotFoundPage/index.tsx
@@ -1,7 +1,9 @@
+import { PageTitle } from '../../common/PageTitle';
+
 export const NotFoundPage = () => (
   <main className="section">
     <div className="container">
-      <h1 className="title">Page not found</h1>
+      <PageTitle title="Page not found" />
     </div>
   </main>
 );
