25b0bc5fd4fce48a9b3ce6d6f007bd722b9a3d91
Andrew <deyneka2003@gmail.com>
2023-10-02 17:05:22 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { Navbar } from './components/Navbar';
 
 import './App.scss';
+import { HomePage } from './pages/HomePage';
+import { PeoplePage } from './components/PeoplePage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 export const App = () => {
   return (
@@ -11,7 +14,17 @@ export const App = () => {
 
       <main className="section">
         <div className="container">
-          <Outlet />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+
+            <Route path="people">
+              <Route index element={<PeoplePage />} />
+              <Route path=":personSlug?" element={<PeoplePage />} />
+            </Route>
+
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
         </div>
       </main>
     </div>
