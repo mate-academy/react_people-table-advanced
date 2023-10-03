4988628a6cdeff06b105cf3337bb3f620f18dc5d
neovendano <kolesnyk.vik@gmail.com>
2023-10-03 16:57:08 +0300

    separate person table row as component

--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -1,8 +1,6 @@
-import classNames from 'classnames';
 import { useParams } from 'react-router-dom';
 import { Person } from '../../types';
-import { EMPTY } from '../../utils/consts';
-import { PersonLink } from '../PersonLink/PersonLink';
+import { PersonRow } from './PersonRow';
 
 interface Props {
   people: Person[],
@@ -67,41 +65,11 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
 
         <tbody>
           {people.map((person) => (
-            <tr
-              data-cy="person"
-              className={
-                classNames({
-                  'has-background-warning': personId === person.slug,
-                })
-              }
+            <PersonRow
+              person={person}
+              selectedPersonId={personId}
               key={person.slug}
-            >
-              <td>
-                <PersonLink person={person} />
-              </td>
-
-              <td>{person.sex}</td>
-              <td>{person.born}</td>
-              <td>{person.died}</td>
-              <td>
-                {person.mother
-                  ? (
-                    <PersonLink person={person.mother} />
-                  )
-                  : (
-                    person.motherName || EMPTY
-                  )}
-              </td>
-              <td>
-                {person.father
-                  ? (
-                    <PersonLink person={person.father} />
-                  )
-                  : (
-                    person.fatherName || EMPTY
-                  )}
-              </td>
-            </tr>
+            />
           ))}
         </tbody>
       </thead>
