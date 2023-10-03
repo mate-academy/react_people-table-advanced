56ae81007498835515aa67d02d889b2e91d39f61
Олег Кулик <olegkulik@MacBook-Pro-Oleg.local>
2023-10-03 20:55:32 +0300

    Added filtration, implemented previous project in this one, completed task

---
+++ src/types/FilterParam.ts
@@ -0,0 +1,7 @@
+export enum FilterParam {
+  Centuries = 'centuries',
+  Query = 'query',
+  Sex = 'sex',
+  Sort = 'sort',
+  Order = 'order',
+}
---
+++ src/types/SortFields.ts
@@ -0,0 +1,8 @@
+export enum SortField {
+  Name = 'name',
+  Sex = 'sex',
+  Born = 'born',
+  Died = 'died',
+  Mother = 'mother',
+  Father = 'father',
+}
