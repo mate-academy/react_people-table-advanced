43dd454f60fff4c1a9a54913662f4d8e9c179961
Olha Momot <helgammt@gmail.com>
2023-10-04 14:04:04 +0300

    tried to fix Navbar test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,31 +6,29 @@ const getLinkClass = ({ isActive }: { isActive: boolean }) => classNames(
   { 'has-background-grey-lighter': isActive },
 );
 
-export const NavBar = () => {
-  return (
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink
-            className={getLinkClass}
-            to="/"
-          >
-            Home
-          </NavLink>
+export const Navbar = () => (
+  <nav
+    data-cy="nav"
+    className="navbar is-fixed-top has-shadow"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div className="container">
+      <div className="navbar-brand">
+        <NavLink
+          className={getLinkClass}
+          to="/"
+        >
+          Home
+        </NavLink>
 
-          <NavLink
-            className={getLinkClass}
-            to="/people"
-          >
-            People
-          </NavLink>
-        </div>
+        <NavLink
+          className={getLinkClass}
+          to="/people"
+        >
+          People
+        </NavLink>
       </div>
-    </nav>
-  );
-};
+    </div>
+  </nav>
+);
