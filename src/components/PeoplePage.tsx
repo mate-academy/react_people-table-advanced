066f1903f9a6d151ca837f037ab0490f63326ed0
AndrzejPec <pimatstudio@gmail.com>
2023-10-06 21:59:14 +0200

    table rendering fine

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,31 +1,57 @@
+import { useEffect, useState } from 'react';
+import { getPeople } from '../api';
+import { Person } from '../types';
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    getPeople()
+      .then((data) => {
+        setPeople(data);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+        setHasError(true);
+      });
+  }, []);
+
+  const renderContent = () => {
+    switch (true) {
+
+      case hasError:
+        return <p data-cy="peopleLoadingError">Something went wrong</p>;
+
+      case isLoading:
+        return <Loader />;
+
+      case people.length === 0:
+        return (
+          <p data-cy="noPeopleMessage">There are no people on the server</p>
+        );
+
+      default:
+        return <PeopleTable people={people} />;
+    }
+  };
+
   return (
     <>
       <h1 className="title">People Page</h1>
-
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
           <div className="column is-7-tablet is-narrow-desktop">
             <PeopleFilters />
           </div>
-
           <div className="column">
             <div className="box table-container">
-              <Loader />
-
-              <p data-cy="peopleLoadingError">Something went wrong</p>
-
-              <p data-cy="noPeopleMessage">
-                There are no people on the server
-              </p>
-
-              <p>There are no people matching the current search criteria</p>
-
-              <PeopleTable />
+              {renderContent()}
             </div>
           </div>
         </div>
