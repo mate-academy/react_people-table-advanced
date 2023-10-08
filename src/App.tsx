12f631e7066e85446e2d1471c913c9f6de0bdaa2
Paweł Przybylak <pawel.przybylak21@gmail.com>
2023-10-09 01:50:14 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,52 @@
-import { PeoplePage } from './components/PeoplePage';
-import { Navbar } from './components/Navbar';
-
+import {
+  Routes, Route, Navigate, NavLink,
+} from 'react-router-dom';
+import { PeoplePage } from './components/Pages/PeoplePage';
 import './App.scss';
+import { HomePage } from './components/Pages/Home';
+import { PageNotFound } from './components/Pages/PageNotFound';
 
 export const App = () => {
   return (
     <div data-cy="app">
-      <Navbar />
+      <nav
+        data-cy="nav"
+        className="navbar is-fixed-top has-shadow"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive
+                ? 'navbar-item has-background-grey-lighter'
+                : 'navbar-item')}
+            >
+              Home
+            </NavLink>
+
+            <NavLink
+              to="/people"
+              className={({ isActive }) => (isActive
+                ? 'navbar-item has-background-grey-lighter'
+                : 'navbar-item')}
+            >
+              People
+            </NavLink>
+          </div>
+        </div>
+      </nav>
 
       <div className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+            <Route path="/people" element={<PeoplePage />} />
+            <Route path="/people/:slug" element={<PeoplePage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
