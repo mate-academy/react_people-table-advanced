78176efd76f841e9aa28f1415f6958eb8237d2e1
haduigon <haduigon@gmail.com>
2023-10-15 18:00:08 +0300

    first commit, top sorting is done

--- src/components/SearchLink.tsx
+++ src/components/SearchLink.tsx
@@ -1,4 +1,4 @@
-import { Link, LinkProps, useSearchParams } from 'react-router-dom';
+ import { Link, LinkProps, useSearchParams } from 'react-router-dom';
 import { getSearchWith, SearchParams } from '../utils/searchHelper';
 
 /**
