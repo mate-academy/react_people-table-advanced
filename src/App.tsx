bc266f811e1e94c498b962618654c82248451957
Dmytro Fefelov <reseneweb@gmail.com>
2023-11-08 23:40:04 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import { PeoplePage } from './components/PeoplePage';
-import { Navbar } from './components/Navbar';
+import { Outlet } from 'react-router-dom';
+import { NavBar } from './components/NavBar/NavBar';
 
 import './App.scss';
 
-export const App = () => {
-  return (
-    <div data-cy="app">
-      <Navbar />
+export const App = () => (
+  <div data-cy="app">
+    <NavBar />
 
-      <div className="section">
-        <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
-        </div>
+    <main className="section">
+      <div className="container">
+        <Outlet />
       </div>
-    </div>
-  );
-};
+    </main>
+  </div>
+);
