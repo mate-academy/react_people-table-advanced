d55ef0596eb3c58cce358fe22866bb4911e51914
Ivanna Zaiets <iva.zaiets@gmail.com>
2023-11-17 17:20:48 +0200

    solution react_people-table-advanced task

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,35 +1,196 @@
+import { useEffect, useState } from 'react';
+import { useParams, useSearchParams } from 'react-router-dom';
+
+import cn from 'classnames';
+import { Person } from '../types';
+
+import { getPeople } from '../api';
+
 import { PeopleFilters } from './PeopleFilters';
+import { TableRow } from './TableRow';
 import { Loader } from './Loader';
-import { PeopleTable } from './PeopleTable';
+import { SearchLink } from './SearchLink';
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [loader, setLoader] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const query = searchParams.get('query') || '';
+  const sex = searchParams.get('sex') || '';
+  const centuries = searchParams.getAll('centuries') || [];
+  const sort = searchParams.get('sort') || '';
+  const order = searchParams.get('order') || '';
+
+  useEffect(() => {
+    setLoader(true);
+    getPeople()
+      .then(setPeople)
+      .catch((error) => {
+        setLoader(false);
+        setErrorMessage('Something went wrong');
+        throw error;
+      })
+      .finally(() => setLoader(false));
+  }, []);
+
+  const peopleFromServer = people.map(person => ({
+    ...person,
+    mother: people.find(p => p.name === person.motherName),
+    father: people.find(p => p.name === person.fatherName),
+    century: String(Number(person.born.toString().slice(0, 2)) + 1),
+  }));
+
+  const { slug } = useParams();
+
+  const normalizedQuery = query.toLowerCase().trim();
+
+  let filteredPeople = peopleFromServer.filter(p => (
+    p.name.toLowerCase().includes(normalizedQuery)
+    || p.motherName?.toLowerCase().includes(normalizedQuery)
+    || p.fatherName?.toLowerCase().includes(normalizedQuery)
+  ));
+
+  if (sex) {
+    filteredPeople = filteredPeople.filter(p => p.sex === sex);
+  }
+
+  if (centuries.length > 0) {
+    filteredPeople = filteredPeople.filter(p => (
+      centuries.includes(p.century)
+    ));
+  }
+
+  switch (sort) {
+    case 'name':
+      filteredPeople.sort((value1, value2) => {
+        return value1.name.localeCompare(value2.name);
+      });
+      break;
+
+    case 'sex':
+      filteredPeople.sort((value1, value2) => {
+        return value1.sex.localeCompare(value2.sex);
+      });
+      break;
+
+    case 'born':
+      filteredPeople.sort((value1, value2) => {
+        return value1.born - value2.born;
+      });
+      break;
+
+    case 'died':
+      filteredPeople.sort((value1, value2) => {
+        return value1.born - value2.born;
+      });
+      break;
+
+    default:
+      return <PeoplePage />;
+  }
+
+  switch (order) {
+    case 'desc':
+      filteredPeople = filteredPeople.reverse();
+      break;
+    default:
+      return <PeoplePage />;
+  }
+
+  const secondClick = sort && order !== 'desc';
+  const thirdClick = sort && order === 'desc';
+
   return (
     <>
       <h1 className="title">People Page</h1>
 
-      <div className="block">
-        <div className="columns is-desktop is-flex-direction-row-reverse">
-          <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
-          </div>
+      {people.length !== 0 ? (
+        <div className="block">
+          <div className="columns is-desktop is-flex-direction-row-reverse">
+            <div className="column is-7-tablet is-narrow-desktop">
+              <PeopleFilters
+                setSearchParams={setSearchParams}
+                searchParams={searchParams}
+                centuries={centuries}
+                query={query}
+                sex={sex}
+              />
+            </div>
 
-          <div className="column">
-            <div className="box table-container">
-              <Loader />
+            <div className="column">
+              <div className="box table-container">
+                <table
+                  data-cy="peopleTable"
+                  className="table is-striped
+                  is-hoverable is-narrow is-fullwidth"
+                >
+                  <thead>
+                    <tr>
+                      {['Name', 'Sex', 'Born', 'Died'].map(name => (
+                        <th>
+                          <span className="is-flex is-flex-wrap-nowrap">
+                            {name}
+                            <SearchLink
+                              params={{
+                                sort: thirdClick ? null : name,
+                                order: secondClick ? 'desc' : null,
+                              }}
+                            >
+                              <span className="icon">
+                                <i className={cn('fas', {
+                                  'fa-sort': sort !== name,
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+                                  'fa-sort-up': sort === name
+                                  && order !== 'desc',
 
-              <p data-cy="noPeopleMessage">
-                There are no people on the server
-              </p>
+                                  'fa-sort-down': sort === name
+                                  && order === 'desc',
+                                })}
+                                />
+                              </span>
+                            </SearchLink>
+                          </span>
+                        </th>
+                      ))}
+                      <th>Mother</th>
+                      <th>Father</th>
+                    </tr>
+                  </thead>
 
-              <p>There are no people matching the current search criteria</p>
-
-              <PeopleTable />
+                  <tbody>
+                    {filteredPeople.map(person => (
+                      <TableRow
+                        key={person.slug}
+                        person={person}
+                        slug={slug}
+                      />
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="box table-container">
+          {errorMessage && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              {errorMessage}
+            </p>
+          )}
+
+          {!loader && people.length === 0 && !errorMessage && (
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          )}
+
+          {loader && <Loader />}
+        </div>
+      )}
     </>
   );
 };
---
+++ src/components/TableRow.tsx
@@ -0,0 +1,56 @@
+import { Link } from 'react-router-dom';
+
+import classNames from 'classnames';
+import { Person } from '../types';
+
+import { PersonLink } from './PersonLink';
+
+type Props = {
+  person: Person,
+  slug: string | undefined,
+};
+
+export const TableRow: React.FC<Props> = ({ person, slug }) => {
+  return (
+    <>
+      <tr
+        data-cy="person"
+        className={classNames({
+          'has-background-warning': slug === person.slug,
+        })}
+      >
+        <td>
+          <Link
+            to={`../${person.slug}`}
+            className={classNames({
+              'has-text-link': person.sex === 'm',
+              'has-text-danger': person.sex === 'f',
+            })}
+          >
+            {person.name}
+          </Link>
+        </td>
+
+        <td>{person.sex}</td>
+        <td>{person.born}</td>
+        <td>{person.died}</td>
+
+        <td>
+          {person.mother ? (
+            <PersonLink person={person.mother} />
+          ) : (
+            person.motherName || '-'
+          )}
+        </td>
+
+        <td>
+          {person.father ? (
+            <PersonLink person={person.father} />
+          ) : (
+            person.fatherName || '-'
+          )}
+        </td>
+      </tr>
+    </>
+  );
+};
