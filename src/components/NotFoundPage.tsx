8659bccac1906547e045fa967b38ad3c268280e8
Hryhorii Starykov <hr.starykov@gmail.com>
2023-11-21 09:04:38 +0200

    Solution

---
+++ src/components/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const NotFoundPage = () => {
+  return (
+    <main className="section">
+      <div className="container">
+        <h1 className="title">Page not found</h1>
+      </div>
+    </main>
+  );
+};
---
+++ src/components/PersonLink.tsx
@@ -0,0 +1,27 @@
+import cn from 'classnames';
+import React from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+
+type Props = {
+  name: string | null;
+  slug: string;
+  sex: string;
+};
+
+export const PersonLink: React.FC<Props> = ({ name, slug, sex }) => {
+  const [searchParams] = useSearchParams();
+
+  return (
+    <Link
+      to={{
+        pathname: `/people/${slug}`,
+        search: searchParams.toString(),
+      }}
+      className={cn({
+        'has-text-danger': sex === 'f',
+      })}
+    >
+      {name}
+    </Link>
+  );
+};
