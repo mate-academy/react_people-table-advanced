6f61f2f57e2cc3c343cedbf6898bacd931c633bb
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-11-25 13:36:28 +0200

    colums mother and father added!

--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,48 +1,47 @@
-import React, { useMemo } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import classNames from 'classnames';
-import { Person } from '../types';
-import { TableItem } from './TableItem';
-import { SearchLink } from './SearchLink';
-// import { SortFields } from '../types/SortFields';
+import React, { useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+import classNames from "classnames";
+import { Person } from "../types";
+import { TableItem } from "./TableItem";
+import { SearchLink } from "./SearchLink";
+import { SortFields } from "../types/SortFields";
 
 type Props = {
   people: Person[];
 };
 
 export const PeopleTable: React.FC<Props> = ({ people }) => {
-  enum SortFields {
-    Name = 'name',
-    Sex = 'sex',
-    Born = 'born',
-    Died = 'died',
-  }
-
-  function isQueryIncluded(str = '', person: Person) {
-    return person.name.toLowerCase().includes(str)
-      || person.fatherName?.toLowerCase().includes(str)
-      || person.motherName?.toLowerCase().includes(str);
+  function isQueryIncluded(str = "", person: Person) {
+    return (
+      person.name.toLowerCase().includes(str) ||
+      person.fatherName?.toLowerCase().includes(str) ||
+      person.motherName?.toLowerCase().includes(str)
+    );
   }
 
   function isBornInCentury(centuries: string[] = [], person: Person) {
-    return !centuries.length
-      || centuries.includes(Math.ceil(person.born / 100).toString());
+    return (
+      !centuries.length ||
+      centuries.includes(Math.ceil(person.born / 100).toString())
+    );
   }
 
   const [searchParams] = useSearchParams();
-  const sex = searchParams.get('sex');
-  const query = searchParams.get('query');
-  const centuries = searchParams.getAll('centuries') || [];
-  const sort = searchParams.get('sort') || '';
-  const order = searchParams.get('order') || '';
+  const sex = searchParams.get("sex");
+  const query = searchParams.get("query");
+  const centuries = searchParams.getAll("centuries") || [];
+  const sort = searchParams.get("sort") || "";
+  const order = searchParams.get("order") || "";
   const preparedPeople = useMemo(() => {
     const filtredAndSortedPeople = people
-      .filter(person => {
+      .filter((person) => {
         const lowerQuery = query?.toLowerCase();
 
-        return (sex ? person.sex === sex : person)
-          && isQueryIncluded(lowerQuery, person)
-          && isBornInCentury(centuries, person);
+        return (
+          (sex ? person.sex === sex : person) &&
+          isQueryIncluded(lowerQuery, person) &&
+          isBornInCentury(centuries, person)
+        );
       })
       .sort((person1, person2) => {
         switch (sort) {
@@ -59,7 +58,7 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
         }
       });
 
-    if (order === 'desc') {
+    if (order === "desc") {
       filtredAndSortedPeople.reverse();
     }
 
@@ -78,18 +77,15 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
               {String(field)}
               <SearchLink
                 params={{
-                  sort:
-                    order === 'desc' && sort === field
-                      ? null
-                      : field,
-                  order: sort === field && !order ? 'desc' : null,
+                  sort: order === "desc" && sort === field ? null : field,
+                  order: sort === field && !order ? "desc" : null,
                 }}
               >
                 <span className="icon">
                   <i
-                    className={classNames('fas', 'fa-sort', {
-                      'fa-sort-up': sort === field && !order,
-                      'fa-sort-down': sort === field && order,
+                    className={classNames("fas", "fa-sort", {
+                      "fa-sort-up": sort === field && !order,
+                      "fa-sort-down": sort === field && order,
                     })}
                   />
                 </span>
@@ -97,10 +93,12 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
             </span>
           </th>
         ))}
+        <th>Mother</th>
+        <th>Father</th>
       </thead>
 
       <tbody>
-        {preparedPeople.map(person => (
+        {preparedPeople.map((person) => (
           <TableItem
             key={person.name}
             person={person}
