be076d40d8c6520acf3d6263a93ad09340979b0a
Semenuik Maxim <xovisage@gmail.com>
2023-12-04 09:57:17 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import { Outlet } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
+
 export const App = () => {
   return (
     <div data-cy="app">
--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,24 +1,58 @@
-import { useParams } from 'react-router-dom';
-import cn from 'classnames';
-import { Person } from '../types';
+import React from 'react';
+import classNames from 'classnames';
+import { useParams, Link } from 'react-router-dom';
 import { PersonLink } from './PersonLink';
+import { Person } from '../types';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bulma/css/bulma.css';
 import { useSearchParams } from '../utils/useSearchParams';
+import { getSearchWith } from '../utils/searchHelper';
 
 type Props = {
   people: Person[];
 };
 
+export enum SortBy {
+  Name = 'name',
+  Sex = 'sex',
+  Born = 'born',
+  Died = 'died',
+}
+
 export const PeopleTable: React.FC<Props> = ({ people }) => {
-  const { id } = useParams();
+  const { slug: id } = useParams();
 
   const {
     sex,
     query,
     centuriesUrl,
+    searchParams,
   } = useSearchParams();
 
-  const filterPeople = () => {
-    let filteredPeople = people;
+  const findParent = (parentName: string | null) => {
+    const parent = people.find((person) => person.name === parentName);
+
+    return parent ? <PersonLink person={parent} /> : parentName;
+  };
+
+  const sort = searchParams.get('sort');
+  const order = searchParams.get('order');
+
+  const sortByColumn = (column: string) => {
+    if (sort !== column) {
+      return { sort: column, order: null };
+    }
+
+    if (!order) {
+      return { sort: column, order: 'desc' };
+    }
+
+    return { sort: null, order: null };
+  };
+
+  const sortPeople = (val: Person[]) => {
+    let filteredPeople = val;
+    const isReversed: number = order ? -1 : 1;
 
     if (sex) {
       filteredPeople = filteredPeople.filter(person => person.sex === sex);
@@ -40,74 +74,77 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
         .includes(Math.ceil(person.born / 100).toString()));
     }
 
-    return filteredPeople;
+    return filteredPeople.sort((a, b) => {
+      switch (sort) {
+        case 'name':
+        case 'sex':
+          return (a[sort] as string).localeCompare(b[sort]) * isReversed;
+        case 'born':
+        case 'died':
+          return (+a[sort] - +b[sort]) * isReversed;
+        default:
+          return 0;
+      }
+    });
   };
 
-  const findParent = (parentName: string) => {
-    const parent = people.find(({ name }) => name === parentName);
-
-    if (parent) {
-      return (
-        <PersonLink person={parent} />
-      );
-    }
-
-    return parentName;
-  };
+  const sortedPeople = sortPeople(people);
 
   return (
     <table
       data-cy="peopleTable"
-      className="
-        table
-        is-striped
-        is-hoverable
-        is-narrow
-        is-fullwidth
-      "
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
     >
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Sex</th>
-          <th>Born</th>
-          <th>Died</th>
+          {Object.keys(SortBy).map((sortName: string) => (
+            <th key={sortName}>
+              <span className="is-flex is-flex-wrap-nowrap">
+                {sortName}
+                <Link
+                  to={{
+                    search: getSearchWith(
+                      searchParams,
+                      sortByColumn(sortName.toLowerCase()),
+                    ),
+                  }}
+                  aria-label={`Sort by ${sortName}`}
+                >
+                  <span className="icon">
+                    <i className={classNames('fas fa-sort', {
+                      'fa-sort-up': sort === sortName.toLowerCase() && !order,
+                      'fa-sort-down': sort === sortName.toLowerCase() && order,
+                    })}
+                    />
+                  </span>
+                </Link>
+              </span>
+            </th>
+          ))}
           <th>Mother</th>
           <th>Father</th>
         </tr>
       </thead>
 
       <tbody>
-        {filterPeople().length
-          ? filterPeople().map((person) => (
-            <tr
-              data-cy="person"
-              key={person.slug}
-              className={cn({
-                'has-background-warning': person.slug === id,
-              })}
-            >
-              <td>
-                <PersonLink person={person} />
-              </td>
-
-              <td>{person.sex}</td>
-              <td>{person.born}</td>
-              <td>{person.died}</td>
-
-              <td>
-                {
-                  person.motherName ? findParent(person.motherName) : '-'
-                }
-              </td>
-
-              <td>
-                {
-                  person.fatherName ? findParent(person.fatherName) : '-'
-                }
-              </td>
-            </tr>
-          )) : 'There are no people matching the current search criteria'}
+        {sortedPeople.map((person) => (
+          <tr
+            data-cy="person"
+            key={person.slug}
+            className={classNames({
+              'has-background-warning': person.slug === id,
+            })}
+          >
+            <td>
+              <PersonLink person={person} />
+            </td>
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            <td>{findParent(person.motherName)}</td>
+            <td>{findParent(person.fatherName)}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
