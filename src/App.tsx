c7cd3ca06d3c8290a2a529c0fa334be654bf1a46
FurArt <iiionerart@gmail.com>
2023-12-21 23:56:21 +0200

    add sorting for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { Outlet, useLocation, useParams } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import './App.scss';
 import { Navigation } from './components/Navigation/Navigation';
 
 export const App = () => {
-  const { slug } = useParams();
-  const { pathname, search, state } = useLocation();
-
   return (
     <div data-cy="app">
       <Navigation />
-      <p className="centre">{`slug ${slug} `}</p>
-      <p className="centre">{`pathname ${pathname} `}</p>
-      <p className="centre">{`search ${search} `}</p>
-      <p className="centre">{`state ${state} `}</p>
       <main className="section">
         <div className="container">
           <Outlet />
