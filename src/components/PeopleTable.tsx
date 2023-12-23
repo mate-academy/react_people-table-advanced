37adf45e549cc7159b9cf08d95fe9976dd10a1ee
Iryna Konovalova <iconovalova19@gmail.com>
2023-12-23 16:19:47 +0100

    implement sorting

--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import { useParams, useSearchParams } from 'react-router-dom';
+import { Link, useParams, useSearchParams } from 'react-router-dom';
 import cn from 'classnames';
 
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { Person } from '../types';
 import { PersonLink } from './PersonLink';
 import { FilterParams } from '../types/FilterParams';
+import { getSearchWith } from '../utils/searchHelper';
+import { SortParams } from '../types/SortParams';
+import { SortType } from '../types/SortType';
 
 type Props = {
   people: Person[],
@@ -20,6 +23,8 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
     const sexSearchValue = searchParams.get(FilterParams.Sex);
     const querySearchValue = searchParams.get(FilterParams.Query);
     const centurySearchValue = searchParams.getAll(FilterParams.Century);
+    const sortNameParams = searchParams.get(SortParams.Sort);
+    const orderParams = searchParams.get(SortParams.Order);
 
     if (sexSearchValue) {
       result = result.filter(p => p.sex === sexSearchValue);
@@ -52,8 +57,74 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
       result = peopleFromCenturies;
     }
 
+    if (!sortNameParams) {
+      return result;
+    }
+
+    switch (sortNameParams) {
+      case SortType.Name:
+        result.sort(
+          (person1, person2) => person1.name.localeCompare(person2.name),
+        );
+        break;
+
+      case SortType.Sex:
+        result.sort(
+          (person1, person2) => person1.sex.localeCompare(person2.sex),
+        );
+        break;
+      case SortType.Born:
+        result.sort(
+          (person1, person2) => person1.born - person2.born,
+        );
+        break;
+      case SortType.Died:
+        result.sort(
+          (person1, person2) => person1.died - person2.died,
+        );
+        break;
+
+      default:
+        break;
+    }
+
+    if (orderParams) {
+      result.reverse();
+    }
+
     return result;
   }, [searchParams, people]);
+
+  const handleSort = useCallback((type: SortType): string => {
+    const sortNameParams = searchParams.get(SortParams.Sort);
+    const orderParams = searchParams.get(SortParams.Order);
+
+    switch (true) {
+      case sortNameParams === type && !orderParams:
+        return getSearchWith(
+          searchParams, { [SortParams.Order]: 'desc' },
+        );
+
+      case sortNameParams !== type:
+        return getSearchWith(
+          searchParams, {
+            [SortParams.Sort]: type,
+            [SortParams.Order]: null,
+          },
+        );
+
+      case (sortNameParams === type && !!orderParams):
+        return getSearchWith(
+          searchParams, {
+            [SortParams.Sort]: null,
+            [SortParams.Order]: null,
+          },
+        );
+
+      default:
+        return '';
+    }
+  }, [searchParams]);
 
   return (
     <table
@@ -65,44 +136,104 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
           <th>
             <span className="is-flex is-flex-wrap-nowrap">
               Name
-              <a href="#/people?sort=name">
+              <Link
+                to={{
+                  search: handleSort(SortType.Name),
+                }}
+              >
                 <span className="icon">
-                  <i className="fas fa-sort" />
+                  <i
+                    className={cn('fas', {
+                      'fa-sort':
+                        searchParams.get(SortParams.Sort) !== SortType.Name,
+                      'fa-sort-up':
+                        searchParams.get(SortParams.Sort) === SortType.Name
+                        && !searchParams.has(SortParams.Order),
+                      'fa-sort-down':
+                        searchParams.get(SortParams.Sort) === SortType.Name
+                        && searchParams.has(SortParams.Order),
+                    })}
+                  />
                 </span>
-              </a>
+              </Link>
             </span>
           </th>
 
           <th>
             <span className="is-flex is-flex-wrap-nowrap">
               Sex
-              <a href="#/people?sort=sex">
+              <Link
+                to={{
+                  search: handleSort(SortType.Sex),
+                }}
+              >
                 <span className="icon">
-                  <i className="fas fa-sort" />
+                  <i
+                    className={cn('fas', {
+                      'fa-sort':
+                        searchParams.get(SortParams.Sort) !== SortType.Sex,
+                      'fa-sort-up':
+                        searchParams.get(SortParams.Sort) === SortType.Sex
+                        && !searchParams.has(SortParams.Order),
+                      'fa-sort-down':
+                        searchParams.get(SortParams.Sort) === SortType.Sex
+                        && searchParams.has(SortParams.Order),
+                    })}
+                  />
                 </span>
-              </a>
+              </Link>
             </span>
           </th>
 
           <th>
             <span className="is-flex is-flex-wrap-nowrap">
               Born
-              <a href="#/people?sort=born&amp;order=desc">
+              <Link
+                to={{
+                  search: handleSort(SortType.Born),
+                }}
+              >
                 <span className="icon">
-                  <i className="fas fa-sort-up" />
+                  <i
+                    className={cn('fas', {
+                      'fa-sort':
+                        searchParams.get(SortParams.Sort) !== SortType.Born,
+                      'fa-sort-up':
+                        searchParams.get(SortParams.Sort) === SortType.Born
+                        && !searchParams.has(SortParams.Order),
+                      'fa-sort-down':
+                        searchParams.get(SortParams.Sort) === SortType.Born
+                        && searchParams.has(SortParams.Order),
+                    })}
+                  />
                 </span>
-              </a>
+              </Link>
             </span>
           </th>
 
           <th>
             <span className="is-flex is-flex-wrap-nowrap">
               Died
-              <a href="#/people?sort=died">
+              <Link
+                to={{
+                  search: handleSort(SortType.Died),
+                }}
+              >
                 <span className="icon">
-                  <i className="fas fa-sort" />
+                  <i
+                    className={cn('fas', {
+                      'fa-sort':
+                       searchParams.get(SortParams.Sort) !== SortType.Born,
+                      'fa-sort-up':
+                        searchParams.get(SortParams.Sort) === SortType.Died
+                        && !searchParams.has(SortParams.Order),
+                      'fa-sort-down':
+                        searchParams.get(SortParams.Sort) === SortType.Died
+                        && searchParams.has(SortParams.Order),
+                    })}
+                  />
                 </span>
-              </a>
+              </Link>
             </span>
           </th>
 
