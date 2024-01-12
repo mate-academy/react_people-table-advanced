a46fdab60939d470f6f3b764ef44b59a9d74aece
Laura Wronska <laura.wronskaa@gmail.com>
2024-01-12 14:14:25 +0100

    add filtering

--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { Person } from '../types';
 import { PersonInfo } from './PersonInfo';
 
@@ -8,6 +9,39 @@ type Props = {
 };
 
 export const PeopleTable: FC<Props> = ({ people }) => {
+  const [searchParams] = useSearchParams();
+  const sexFilter = searchParams.get('sex');
+  const centuryFilter = searchParams.getAll('centuries');
+  const query = searchParams.get('query');
+
+  const visiblePeople = useMemo(() => {
+    let newPeople = [...people];
+
+    if (sexFilter) {
+      newPeople = newPeople.filter(person => person.sex === sexFilter);
+    }
+
+    if (centuryFilter.length) {
+      newPeople = newPeople.filter(person => {
+        const century = Math.ceil(person.died / 100);
+
+        return centuryFilter.includes(String(century));
+      });
+    }
+
+    if (query) {
+      newPeople = newPeople.filter(person => {
+        const lowerQuery = query.toLowerCase();
+
+        return person.name.toLowerCase().includes(lowerQuery)
+          || person.motherName?.toLowerCase().includes(lowerQuery)
+          || person.fatherName?.toLowerCase().includes(lowerQuery);
+      });
+    }
+
+    return newPeople;
+  }, [people, sexFilter, centuryFilter, query]);
+
   return (
     <table
       data-cy="peopleTable"
@@ -15,17 +49,57 @@ export const PeopleTable: FC<Props> = ({ people }) => {
     >
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Sex</th>
-          <th>Born</th>
-          <th>Died</th>
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Name
+              <a href="#/people?sort=name">
+                <span className="icon">
+                  <i className="fas fa-sort" />
+                </span>
+              </a>
+            </span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Sex
+              <a href="#/people?sort=sex">
+                <span className="icon">
+                  <i className="fas fa-sort" />
+                </span>
+              </a>
+            </span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Born
+              <a href="#/people?sort=born&amp;order=desc">
+                <span className="icon">
+                  <i className="fas fa-sort-up" />
+                </span>
+              </a>
+            </span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Died
+              <a href="#/people?sort=died">
+                <span className="icon">
+                  <i className="fas fa-sort" />
+                </span>
+              </a>
+            </span>
+          </th>
+
           <th>Mother</th>
           <th>Father</th>
         </tr>
       </thead>
 
       <tbody>
-        {people?.map(person => (
+        {visiblePeople?.map(person => (
           <PersonInfo key={person.slug} person={person} />
         ))}
       </tbody>
