d2a8d67d5fcf4330067a1bbde23d4fdfe00ce5cb
Oleksandr Kosenko <d1izel@ukr.net>
2024-01-13 22:53:07 +0200

    add react-people-table-advanced

---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,117 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import { getPeople } from '../api';
+import { Person } from '../types';
+import { Loader } from '../components/Loader';
+import { PeopleFilters } from '../components/PeopleFilters';
+import { PeopleTable } from '../components/PeopleTable';
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  const [searchParams] = useSearchParams();
+
+  const sex = searchParams.get('sex');
+  const query = searchParams.get('query');
+  const centuries = searchParams.getAll('centuries');
+  const sort = searchParams.get('sort');
+  const order = searchParams.get('order');
+
+  let filteredPeople = [...people];
+
+  if (sex) {
+    filteredPeople = filteredPeople.filter(person => person.sex === sex);
+  }
+
+  if (query) {
+    const queryNormalize = query.trim().toLowerCase();
+
+    filteredPeople = filteredPeople.filter((person) => {
+      return (
+        person.name.toLowerCase().includes(queryNormalize)
+        || person.motherName?.toLowerCase().includes(queryNormalize)
+        || person.fatherName?.toLowerCase().includes(queryNormalize));
+    });
+  }
+
+  if (centuries.length) {
+    filteredPeople = filteredPeople.filter((person) => {
+      return (
+        centuries.includes(Math.ceil(person.born / 100).toString())
+      );
+    });
+  }
+
+  if (sort) {
+    filteredPeople = filteredPeople.sort((a, b) => {
+      switch (sort) {
+        case 'Name':
+          return a.name.localeCompare(b.name);
+        case 'Sex':
+          return a.sex.localeCompare(b.sex);
+        case 'Born':
+          return a.born - b.born;
+        case 'Died':
+          return a.died - b.died;
+        default:
+          return 0;
+      }
+    });
+
+    if (order === 'desc') {
+      filteredPeople.reverse();
+    }
+  }
+
+  useEffect(() => {
+    setLoading(true);
+
+    getPeople()
+      .then(setPeople)
+      .catch(() => setErrorMessage(true))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="columns is-desktop is-flex-direction-row-reverse">
+          <div className="column is-7-tablet is-narrow-desktop">
+            <PeopleFilters />
+          </div>
+
+          <div className="column">
+            <div className="box table-container">
+              {loading && (
+                <Loader />
+              )}
+
+              {errorMessage && (
+                <p data-cy="peopleLoadingError">Something went wrong</p>
+              )}
+
+              {!loading && !people.length && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
+
+              {!filteredPeople.length && (
+                <p>There are no people matching the current search criteria</p>
+              )}
+
+              {!loading && !!people?.length && (
+                <PeopleTable people={filteredPeople} />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
