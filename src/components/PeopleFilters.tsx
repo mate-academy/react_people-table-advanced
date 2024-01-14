8a4aad3ae8992648af3ae56e5539399dbd599815
Monika Osowiecka <mosowiecka@users.noreply.github.com>
2024-01-14 18:34:26 +0100

    Added params 1/2

--- src/components/PeopleFilters.tsx
+++ src/components/PeopleFilters.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Centuries } from '../types/Centuries';
+import { SearchLink } from './SearchLink';
 
 type Props = {
   handleCenturyButton: (century:string) => void,
@@ -16,120 +17,147 @@ export const PeopleFilters: React.FC<Props> = ({
   filterQuery,
 }) => {
   return (
-    <nav className="panel">
-      <p className="panel-heading">Filters</p>
+    <>
+      <nav className="panel">
+        <p className="panel-heading">Filters</p>
 
-      <p className="panel-tabs" data-cy="SexFilter">
-        <a
-          className={filterSex === 'All' ? 'is-active' : ''}
-          href="#/people"
-          onClick={() => handleSexLink('All')}
-        >
-          All
-        </a>
-        <a
-          className={filterSex === 'm' ? 'is-active' : ''}
-          href="#/people?sex=m"
-          onClick={() => handleSexLink('m')}
-        >
-          Male
-        </a>
-        <a
-          className={filterSex === 'f' ? 'is-active' : ''}
-          href="#/people?sex=f"
-          onClick={() => handleSexLink('f')}
-        >
-          Female
-        </a>
-      </p>
-
-      <div className="panel-block">
-        <p className="control has-icons-left">
-          <input
-            data-cy="NameFilter"
-            type="search"
-            className="input"
-            placeholder="Search"
-            value={filterQuery}
-            onChange={event => handleInput(event.target.value)}
-          />
-
-          <span className="icon is-left">
-            <i className="fas fa-search" aria-hidden="true" />
-          </span>
+        <p className="panel-tabs" data-cy="SexFilter">
+          <SearchLink
+            params={{ sex: null }}
+            className={filterSex === 'All' ? 'is-active' : ''}
+            onClick={() => handleSexLink('All')}
+          >
+            All
+          </SearchLink>
+          <SearchLink
+            params={{ sex: 'm' }}
+            className={filterSex === 'm' ? 'is-active' : ''}
+            onClick={() => handleSexLink('m')}
+          >
+            Male
+          </SearchLink>
+          <SearchLink
+            params={{ sex: 'f' }}
+            className={filterSex === 'f' ? 'is-active' : ''}
+            onClick={() => handleSexLink('f')}
+          >
+            Female
+          </SearchLink>
         </p>
-      </div>
 
-      <div className="panel-block">
-        <div className="level is-flex-grow-1 is-mobile" data-cy="CenturyFilter">
-          <div className="level-left">
-            <a
-              data-cy="century"
-              className={`button mr-1 ${filterCentury.includes('16') ? 'is-info' : ''}`}
-              href="#/people?centuries=16"
-              onClick={() => handleCenturyButton('16')}
-            >
-              16
-            </a>
+        <div className="panel-block">
+          <p className="control has-icons-left">
+            <input
+              data-cy="NameFilter"
+              type="search"
+              className="input"
+              placeholder="Search"
+              value={filterQuery}
+              onChange={event => handleInput(event.target.value)}
+            />
 
-            <a
-              data-cy="century"
-              className={`button mr-1 ${filterCentury.includes('17') ? 'is-info' : ''}`}
-              href="#/people?centuries=17"
-              onClick={() => handleCenturyButton('17')}
-            >
-              17
-            </a>
+            <span className="icon is-left">
+              <i className="fas fa-search" aria-hidden="true" />
+            </span>
+          </p>
+        </div>
 
-            <a
-              data-cy="century"
-              className={`button mr-1 ${filterCentury.includes('18') ? 'is-info' : ''}`}
-              href="#/people?centuries=18"
-              onClick={() => handleCenturyButton('18')}
-            >
-              18
-            </a>
+        <div className="panel-block">
+          <div
+            className="level is-flex-grow-1 is-mobile"
+            data-cy="CenturyFilter"
+          >
+            <div className="level-left">
+              <SearchLink
+                params={{
+                  centuries: filterCentury.includes('16')
+                    ? filterCentury.filter((c) => c !== '16')
+                    : [...filterCentury, '16'],
+                }}
+                data-cy="century"
+                className={`button mr-1 ${filterCentury.includes('16') ? 'is-info' : ''}`}
+                onClick={() => handleCenturyButton('16')}
+              >
+                16
+              </SearchLink>
 
-            <a
-              data-cy="century"
-              className={`button mr-1 ${filterCentury.includes('19') ? 'is-info' : ''}`}
-              onClick={() => handleCenturyButton('19')}
-              href="#/people?centuries=19"
-            >
-              19
-            </a>
+              <SearchLink
+                params={{
+                  centuries: filterCentury.includes('17')
+                    ? filterCentury.filter((c) => c !== '17')
+                    : [...filterCentury, '17'],
+                }}
+                data-cy="century"
+                className={`button mr-1 ${filterCentury.includes('17') ? 'is-info' : ''}`}
+                onClick={() => handleCenturyButton('17')}
+              >
+                17
+              </SearchLink>
 
-            <a
-              data-cy="century"
-              className={`button mr-1 ${filterCentury.includes('20') ? 'is-info' : ''}`}
-              onClick={() => handleCenturyButton('20')}
-              href="#/people?centuries=20"
-            >
-              20
-            </a>
-          </div>
+              <SearchLink
+                params={{
+                  centuries: filterCentury.includes('18')
+                    ? filterCentury.filter((c) => c !== '18')
+                    : [...filterCentury, '18'],
+                }}
+                data-cy="century"
+                className={`button mr-1 ${filterCentury.includes('18') ? 'is-info' : ''}`}
+                onClick={() => handleCenturyButton('18')}
+              >
+                18
+              </SearchLink>
 
-          <div className="level-right ml-4">
-            <a
-              data-cy="centuryALL"
-              className={`button is-success ${filterCentury.length === 0 ? '' : 'is-outlined'}`}
-              href="#/people"
-              onClick={() => handleCenturyButton('All')}
-            >
-              All
-            </a>
+              <SearchLink
+                params={{
+                  centuries: filterCentury.includes('19')
+                    ? filterCentury.filter((c) => c !== '19')
+                    : [...filterCentury, '19'],
+                }}
+                data-cy="century"
+                className={`button mr-1 ${filterCentury.includes('19') ? 'is-info' : ''}`}
+                onClick={() => handleCenturyButton('19')}
+              >
+                19
+              </SearchLink>
+
+              <SearchLink
+                params={{
+                  centuries: filterCentury.includes('20')
+                    ? filterCentury.filter((c) => c !== '20')
+                    : [...filterCentury, '20'],
+                }}
+                data-cy="century"
+                className={`button mr-1 ${filterCentury.includes('20') ? 'is-info' : ''}`}
+                onClick={() => handleCenturyButton('20')}
+              >
+                20
+              </SearchLink>
+            </div>
+
+            <div className="level-right ml-4">
+              <SearchLink
+                params={{
+                  centuries: null,
+                }}
+                data-cy="centuryALL"
+                className={`button is-success ${filterCentury.length === 0 ? '' : 'is-outlined'}`}
+                onClick={() => handleCenturyButton('All')}
+              >
+                All
+              </SearchLink>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="panel-block">
-        <a
-          className="button is-link is-outlined is-fullwidth"
-          href="#/people"
-        >
-          Reset all filters
-        </a>
-      </div>
-    </nav>
+        <div className="panel-block">
+          <a
+            className="button is-link is-outlined is-fullwidth"
+            href="#/people"
+          >
+            Reset all filters
+          </a>
+        </div>
+      </nav>
+    </>
   );
 };
