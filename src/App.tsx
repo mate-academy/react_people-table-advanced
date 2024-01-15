852b647ce245a43a8c27be9004f7f54b44f9a727
Yurii Shkrobut <georgebest2409@gmail.com>
2024-01-15 17:24:17 +0200

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { PeoplePage } from './components/PeoplePage';
 import { Navbar } from './components/Navbar';
 
@@ -8,13 +9,22 @@ export const App = () => {
     <div data-cy="app">
       <Navbar />
 
-      <div className="section">
+      <main className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <Routes>
+            <Route path="/" element={<h1 className="title">Home Page</h1>} />
+            <Route path="home" element={<Navigate to="/" replace />} />
+
+            <Route path="/people/:slug?" element={<PeoplePage />} />
+
+            <Route
+              path="*"
+              element={<h1 className="title">Page not found</h1>}
+            />
+          </Routes>
+
         </div>
-      </div>
+      </main>
     </div>
   );
 };
--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,32 +1,81 @@
+/* eslint-disable no-param-reassign */
+import { useEffect, useState } from 'react';
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { getPeople } from '../api';
+import { Person } from '../types';
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[] | []>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const noErrorAndLoad = !isError && !isLoading;
+  const noErrorAndLoadAndIsPeople = noErrorAndLoad && !!people.length;
+
+  const normalizePeople = people.map(person => {
+    const father = people.find(parent => person.fatherName === parent.name);
+    const mother = people.find(parent => person.motherName === parent.name);
+
+    if (father) {
+      person.father = father;
+    }
+
+    if (mother) {
+      person.mother = mother;
+    }
+
+    return person;
+  });
+
+  useEffect(() => {
+    setIsError(false);
+    setIsLoading(true);
+
+    getPeople()
+      .then(setPeople)
+      .catch(() => {
+        setIsError(true);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <>
       <h1 className="title">People Page</h1>
 
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
-          <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
-          </div>
+          {noErrorAndLoadAndIsPeople && (
+            <div className="column is-7-tablet is-narrow-desktop">
+              <PeopleFilters />
+            </div>
+          )}
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
+              {isLoading && <Loader />}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+              {isError && (
+                <p data-cy="peopleLoadingError" className="has-text-danger">
+                  Something went wrong
+                </p>
+              )}
 
-              <p data-cy="noPeopleMessage">
-                There are no people on the server
-              </p>
+              {noErrorAndLoad && !people.length && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
 
               <p>There are no people matching the current search criteria</p>
 
-              <PeopleTable />
+              {noErrorAndLoadAndIsPeople && (
+                <PeopleTable people={normalizePeople} />
+              )}
             </div>
+
           </div>
         </div>
       </div>
