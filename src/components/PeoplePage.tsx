b37ea9afc02398413c6dd6ed9b3b96b96af1eef8
Yurii Shkrobut <georgebest2409@gmail.com>
2024-01-18 17:46:10 +0200

    Secondary

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
@@ -10,11 +11,12 @@ export const PeoplePage = () => {
   const [people, setPeople] = useState<Person[] | []>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [searchParams] = useSearchParams();
 
-  const noErrorAndLoad = !isError && !isLoading;
-  const noErrorAndLoadAndIsPeople = noErrorAndLoad && !!people.length;
+  const sex = searchParams.get('sex') || '';
+  const query = searchParams.get('query') || '';
 
-  const normalizePeople = people.map(person => {
+  let normalizePeople = people.map(person => {
     const father = people.find(parent => person.fatherName === parent.name);
     const mother = people.find(parent => person.motherName === parent.name);
 
@@ -28,6 +30,30 @@ export const PeoplePage = () => {
 
     return person;
   });
+
+  const filterPeople = () => {
+    if (sex) {
+      normalizePeople = normalizePeople.filter(person => person.sex === sex);
+    }
+
+    if (query) {
+      normalizePeople = normalizePeople.filter(person => {
+        const normQuery = query.toLowerCase();
+
+        return person.name.toLowerCase().includes(normQuery)
+          || person.fatherName?.toLowerCase().includes(normQuery)
+          || person.motherName?.toLowerCase().includes(normQuery);
+      });
+    }
+
+    return normalizePeople;
+  };
+
+  const filteredPeople = filterPeople();
+  const noErrorAndLoad = !isError && !isLoading;
+  const noErrorAndLoadAndIsPeople = noErrorAndLoad && !!people.length;
+  const noCurrentSearchCriteria
+    = noErrorAndLoadAndIsPeople && !filteredPeople.length;
 
   useEffect(() => {
     setIsError(false);
@@ -69,10 +95,12 @@ export const PeoplePage = () => {
                 </p>
               )}
 
-              <p>There are no people matching the current search criteria</p>
+              {noCurrentSearchCriteria && (
+                <p>There are no people matching the current search criteria</p>
+              )}
 
-              {noErrorAndLoadAndIsPeople && (
-                <PeopleTable people={normalizePeople} />
+              {!noCurrentSearchCriteria && (
+                <PeopleTable people={filteredPeople} />
               )}
             </div>
 
--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 import { useSearchParams } from 'react-router-dom';
 import { Person } from '../types';
 import { PersonLink } from './PersonLink';
@@ -8,57 +9,77 @@ import { SearchLink } from './SearchLink';
 /* eslint-disable jsx-a11y/control-has-associated-label */
 
 interface Props {
-  people: Person[]
+  people: Person[];
 }
 
 const SORT_PARAMS = ['Name', 'Sex', 'Born', 'Died'];
 
 export const PeopleTable: React.FC<Props> = ({ people }) => {
   const [searchParams] = useSearchParams();
+
   const sort = searchParams.get('sort');
+  const isReversed = searchParams.get('order') === 'desc';
+
   let sortedPeople = [...people];
 
   sortedPeople = sortedPeople.sort((a, b) => {
     switch (sort) {
       case 'name':
-        return a.name.localeCompare(b.name);
+      case 'sex':
+        return a[sort].localeCompare(b[sort]);
 
       case 'born':
-        return a.born - b.born;
-
       case 'died':
-        return a.died - b.died;
+        return a[sort] - b[sort];
 
       default:
         return 0;
     }
   });
 
+  if (isReversed) {
+    sortedPeople = sortedPeople.reverse();
+  }
+
+  const getSortParams = (param: string) => {
+    return {
+      sort: param === sort && isReversed ? null : param,
+      order: param === sort && !isReversed ? 'desc' : null,
+    };
+  };
+
   return (
     <table
       data-cy="peopleTable"
       className="table is-striped is-hoverable is-narrow is-fullwidth"
     >
-      <thead>
-        <tr>
-          {SORT_PARAMS.map(param => (
-            <th key={param}>
-              <span className="is-flex is-flex-wrap-nowrap">
-                {param}
-                <SearchLink params={{ sort: param.toLowerCase() }}>
-                  <span className="icon">
-                    <i className="fas fa-sort" />
-                  </span>
-                </SearchLink>
+      {!!sortedPeople.length && (
+        <thead>
+          <tr>
+            {SORT_PARAMS.map(it => (
+              <th key={it}>
+                <span className="is-flex is-flex-wrap-nowrap">
+                  {it}
+                  <SearchLink params={getSortParams(it.toLowerCase())}>
+                    <span className="icon">
+                      <i className={cn('fas', {
+                        'fa-sort': it.toLowerCase() !== sort,
+                        'fa-sort-up': it.toLowerCase() === sort && !isReversed,
+                        'fa-sort-down': it.toLowerCase() === sort && isReversed,
+                      })}
+                      />
+                    </span>
+                  </SearchLink>
 
-              </span>
-            </th>
-          ))}
+                </span>
+              </th>
+            ))}
 
-          <th>Mother</th>
-          <th>Father</th>
-        </tr>
-      </thead>
+            <th>Mother</th>
+            <th>Father</th>
+          </tr>
+        </thead>
+      )}
 
       <tbody>
         {sortedPeople.map(person => (
