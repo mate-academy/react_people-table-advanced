2c1382b10025b9ae58e9168cfb82d7bbbdb6fa42
unknown <maksoleksyuk@gmail.com>
2024-01-21 13:41:38 +0200

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
+import {
+  useEffect, useState,
+  createContext,
+} from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { PeoplePage } from './components/PeoplePage';
 import { Navbar } from './components/Navbar';
-
+import { getPeople } from './api';
 import './App.scss';
 
+export const PeopleContext = createContext([]);
+
 export const App = () => {
+  const [people, setPeople] = useState();
+  const fetchData = async () => {
+    const peopleData = await getPeople();
+
+    setPeople(peopleData);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div data-cy="app">
       <Navbar />
 
       <div className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <PeopleContext.Provider value={people}>
+            <Routes>
+              <Route path="/" element={<h1 className="title">Home Page</h1>} />
+              <Route path="/people" element={<PeoplePage />} />
+              <Route
+                path="*"
+                element={
+                  <h1 className="title">Page not found</h1>
+                }
+              />
+            </Routes>
+          </PeopleContext.Provider>
         </div>
       </div>
     </div>
