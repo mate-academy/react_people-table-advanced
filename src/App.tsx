baa8046396a563ff5aa907dd2d8086d45958a685
Alla Serhiienko <allasergienko706@gmail.com>
2024-01-31 21:14:49 +0100

    linter error was edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import './App.scss';
-import { NavBar } from './components/Navbar';
+import { NavBar } from './components/NavBar';
 
 export const App = () => {
   return (
