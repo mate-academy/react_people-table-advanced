dce06a0ea17ce5d78441937dc59fd5f05e80af26
Artem <artemmalikov742@gmail.com>
2024-02-11 20:43:12 +0200

    add fixes

--- src/components/PeopleFilters.tsx
+++ src/components/PeopleFilters.tsx
@@ -9,6 +9,7 @@ export const PeopleFilters = () => {
   const [query, setQuery] = useState(searchParams.get('query') || '');
   const centuries = searchParams.getAll('centuries') || [];
   const sex = searchParams.get('sex') || '';
+  const centuriesForFilter = [16, 17, 18, 19, 20];
 
   const handleQueryChanged = (newQuery: string) => {
     const normalizedQuery = newQuery.trim().toLowerCase();
@@ -78,55 +79,17 @@ export const PeopleFilters = () => {
       <div className="panel-block">
         <div className="level is-flex-grow-1 is-mobile" data-cy="CenturyFilter">
           <div className="level-left">
-            <SearchLink
-              data-cy="century"
-              className={cn('button mr-1', {
-                'is-info': centuries.includes('16'),
-              })}
-              params={{ centuries: getNewCenturySearchParams('16') }}
-            >
-              16
-            </SearchLink>
-
-            <SearchLink
-              data-cy="century"
-              className={cn('button mr-1', {
-                'is-info': centuries.includes('17'),
-              })}
-              params={{ centuries: getNewCenturySearchParams('17') }}
-            >
-              17
-            </SearchLink>
-
-            <SearchLink
-              data-cy="century"
-              className={cn('button mr-1', {
-                'is-info': centuries.includes('18'),
-              })}
-              params={{ centuries: getNewCenturySearchParams('18') }}
-            >
-              18
-            </SearchLink>
-
-            <SearchLink
-              data-cy="century"
-              className={cn('button mr-1', {
-                'is-info': centuries.includes('19'),
-              })}
-              params={{ centuries: getNewCenturySearchParams('19') }}
-            >
-              19
-            </SearchLink>
-
-            <SearchLink
-              data-cy="century"
-              className={cn('button mr-1', {
-                'is-info': centuries.includes('20'),
-              })}
-              params={{ centuries: getNewCenturySearchParams('20') }}
-            >
-              20
-            </SearchLink>
+            {centuriesForFilter.map(century => (
+              <SearchLink
+                data-cy="century"
+                className={cn('button mr-1', {
+                  'is-info': centuries.includes(`${century}`),
+                })}
+                params={{ centuries: getNewCenturySearchParams(`${century}`) }}
+              >
+                {century}
+              </SearchLink>
+            ))}
           </div>
 
           <div className="level-right ml-4">
