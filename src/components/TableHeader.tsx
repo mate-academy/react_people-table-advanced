134a74fb353470cf309c2cd5ecb759d56645930d
Oleksandr Boiko <iccc97@gmail.com>
2024-02-13 14:44:29 +0200

    changed `TableHeader`

--- src/components/TableHeader.tsx
+++ src/components/TableHeader.tsx
@@ -8,43 +8,41 @@ interface Props {
   order: string;
 }
 
-export const TableHeader: React.FC<Props> = ({ sort, order }) => {
-  return (
-    <thead>
-      <tr>
-        {cells.map(cellName => {
-          return (
-            <th key={cellName}>
-              <span className="is-flex is-flex-wrap-nowrap is-capitalized">
-                {cellName}
-                <SearchLink
-                  params={{
-                    sort: sort === cellName && order
-                      ? null
-                      : cellName,
-                    order: sort === cellName && !order
-                      ? 'desc'
-                      : null,
-                  }}
-                >
-                  <span className="icon">
-                    <i
-                      className={classNames('fas', {
-                        'fa-sort': sort !== cellName,
-                        'fa-sort-up': sort === cellName && !order,
-                        'fa-sort-down': sort === cellName && order,
-                      })}
-                    />
-                  </span>
-                </SearchLink>
-              </span>
-            </th>
-          );
-        })}
+export const TableHeader: React.FC<Props> = ({ sort, order }) => (
+  <thead>
+    <tr>
+      {cells.map(cellName => {
+        return (
+          <th key={cellName}>
+            <span className="is-flex is-flex-wrap-nowrap is-capitalized">
+              {cellName}
+              <SearchLink
+                params={{
+                  sort: sort === cellName && order
+                    ? null
+                    : cellName,
+                  order: sort === cellName && !order
+                    ? 'desc'
+                    : null,
+                }}
+              >
+                <span className="icon">
+                  <i
+                    className={classNames('fas', {
+                      'fa-sort': sort !== cellName,
+                      'fa-sort-up': sort === cellName && !order,
+                      'fa-sort-down': sort === cellName && order,
+                    })}
+                  />
+                </span>
+              </SearchLink>
+            </span>
+          </th>
+        );
+      })}
 
-        <th>Mother</th>
-        <th>Father</th>
-      </tr>
-    </thead>
-  );
-};
+      <th>Mother</th>
+      <th>Father</th>
+    </tr>
+  </thead>
+);
