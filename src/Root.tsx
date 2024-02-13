b90420b9701ffb66cc0def15589822983e9d93b1
Vlad Pashko <vladpavloggg@gmail.com>
2024-02-13 22:44:44 +0200

    fix constant naming and repeated use peoplePage component

--- src/Root.tsx
+++ src/Root.tsx
@@ -15,7 +15,7 @@ export const Root = () => (
       <Route path="/" element={<App />}>
         <Route index element={<HomePage />} />
         <Route path="/home" element={<Navigate to="/" replace />} />
-        <Route path="/people" element={<PeoplePage />}>
+        <Route path="/people">
           <Route path=":slug?" element={<PeoplePage />} />
         </Route>
         <Route path="*" element={<h1 className="title">Page not found</h1>} />
