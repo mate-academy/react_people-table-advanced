e65e05b406f2f5923a0d281e75d69f355d533f1e
Ivan <your@mail.com>
2024-02-19 19:52:18 +0200

    sorting truble

--- src/components/PeopleFilters.tsx
+++ src/components/PeopleFilters.tsx
@@ -1,4 +1,11 @@
+import { useSearchParams } from "react-router-dom";
+
 export const PeopleFilters = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') || '';
+  const sex = searchParams.get('sex') || '';
+  const centuries = searchParams.getAll('centuries') || [];
+
   return (
     <nav className="panel">
       <p className="panel-heading">Filters</p>
--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   Link,
   useLocation,
@@ -15,100 +15,55 @@ type Props = {
   people: Person[];
 };
 
-const makeGoodList = (people: Person[], order: string, cell: string) => {
+const makeGoodList = (people: Person[], part?: string, order?: string) => {
   const correctPeopleList = people;
 
-  switch (cell) {
-    case 'Name':
-      switch (order) {
-        case 'increase':
-          correctPeopleList.sort((a: Person, b: Person) =>
-            a.name.localeCompare(b.name),
-          );
+  if (part && order) {
+    switch (part) {
+      case 'name':
+      case 'sex':
+        correctPeopleList.sort((a: Person, b: Person) =>
+          b[`${part}`].localeCompare(a[`${part}`]),
+        );
 
-          return correctPeopleList;
+        return correctPeopleList;
 
-        case 'decrease':
-          correctPeopleList.sort((a: Person, b: Person) =>
-            b.name.localeCompare(a.name),
-          );
+      case 'born':
+      case 'died':
+        correctPeopleList.sort((a, b) => b[`${part}`] - a[`${part}`]);
 
-          return correctPeopleList;
+        return correctPeopleList;
 
-        default:
-          return correctPeopleList;
-      }
-
-      break;
-
-    case 'Sex':
-      switch (order) {
-        case 'increase':
-          correctPeopleList.sort((a: Person, b: Person) =>
-            a.sex.localeCompare(b.sex),
-          );
-
-          return correctPeopleList;
-
-        case 'decrease':
-          correctPeopleList.sort((a: Person, b: Person) =>
-            b.sex.localeCompare(a.sex),
-          );
-
-          return correctPeopleList;
-
-        default:
-          return correctPeopleList;
-      }
-
-      break;
-
-    case 'Born':
-      switch (order) {
-        case 'increase':
-          correctPeopleList.sort((a, b) => a.born - b.born);
-
-          return correctPeopleList;
-
-        case 'decrease':
-          correctPeopleList.sort((a, b) => b.born - a.born);
-
-          return correctPeopleList;
-
-        default:
-          return correctPeopleList;
-      }
-
-      break;
-
-    case 'Died':
-      switch (order) {
-        case 'increase':
-          correctPeopleList.sort((a, b) => a.died - b.died);
-
-          return correctPeopleList;
-
-        case 'decrease':
-          correctPeopleList.sort((a, b) => b.died - a.died);
-
-          return correctPeopleList;
-
-        default:
-          return correctPeopleList;
-      }
-
-      break;
-
-    default:
-      return correctPeopleList;
+      default:
+        return correctPeopleList;
+    }
   }
+
+  if (part && !order) {
+    switch (part) {
+      case 'name':
+      case 'sex':
+        correctPeopleList.sort((a: Person, b: Person) =>
+          a[`${part}`].localeCompare(b[`${part}`]),
+        );
+
+        return correctPeopleList;
+
+      case 'born':
+      case 'died':
+        correctPeopleList.sort((a, b) => a[`${part}`] - b[`${part}`]);
+
+        return correctPeopleList;
+
+      default:
+        return correctPeopleList;
+    }
+  }
+
+  return correctPeopleList;
 };
 
 export const PeopleTable: React.FC<Props> = ({ people }) => {
-  const [order, setOrder] = useState('');
-  const [comparePart, setComparePart] = useState('');
-  const [activeCell, setActiveCell] = useState('');
-
   const findParent = (parentName: string | null) => {
     const parent = people.find(p => p.name === parentName);
 
@@ -117,34 +72,31 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
 
   const { pathname } = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
+  const sort = searchParams.get('sort') || '';
+  const order = searchParams.get('order') || '';
 
   const { slug } = useParams();
 
   const selectedPerson = slug;
 
-  useEffect(() => {
-    if (comparePart === '') {
-      setOrder('increase');
-      setComparePart(`${activeCell}`);
-    } else if (comparePart !== `${activeCell}`) {
-      setOrder('increase');
-      setComparePart(`${activeCell}`);
-    } else if (comparePart === `${activeCell}` && order !== 'decrease') {
-      setOrder('decrease');
-    } else if (comparePart === `${activeCell}` && order === 'decrease') {
-      setOrder('');
-      setComparePart('');
-    }
-  }, [activeCell, comparePart, order]);
-
-  const sort = () => {};
-
   const handlePartChange = (field: string) => {
     const params = new URLSearchParams(searchParams);
 
-    params.set(`${field}`, order);
+    if (sort && order) {
+      params.delete(order);
+      params.delete(sort);
+    }
+
+    if (!sort) {
+      params.set(sort, field);
+    }
+
+    if (sort && !order) {
+      params.set(order, 'desc');
+    }
 
     setSearchParams(params);
+    makeGoodList(people, sort, order);
   };
 
   return (
@@ -153,7 +105,7 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
       className="table is-striped is-hoverable is-narrow is-fullwidth"
     >
       <p> {pathname} </p>
-      <p> {searchParams} </p>
+      <p> {searchParams.get('sort')} </p>
       <thead>
         <tr>
           <th>
@@ -163,8 +115,6 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
                 to="?sort=name"
                 onClick={() => {
                   handlePartChange('name');
-                  setActiveCell('Name');
-                  makeGoodList(people, order, activeCell);
                 }}
               >
                 <span className="icon">
@@ -177,7 +127,12 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
           <th>
             <span className="is-flex is-flex-wrap-nowrap">
               Sex
-              <Link to="#/people?sort=sex" onClick={() => sort()}>
+              <Link
+                to="#/people?sort=sex"
+                onClick={() => {
+                  handlePartChange('sex');
+                }}
+              >
                 <span className="icon">
                   <i className="fas fa-sort" />
                 </span>
@@ -190,7 +145,9 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
               Born
               <Link
                 to="#/people?sort=born&amp;order=desc"
-                onClick={() => sort()}
+                onClick={() => {
+                  handlePartChange('born');
+                }}
               >
                 <span className="icon">
                   <i className="fas fa-sort-up" />
@@ -202,7 +159,12 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
           <th>
             <span className="is-flex is-flex-wrap-nowrap">
               Died
-              <Link to="#/people?sort=died" onClick={() => sort()}>
+              <Link
+                to="#/people?sort=died"
+                onClick={() => {
+                  handlePartChange('died');
+                }}
+              >
                 <span className="icon">
                   <i className="fas fa-sort" />
                 </span>
