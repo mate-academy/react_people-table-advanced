f35b4a260d69e0e1456c3e2e4bcd85dd91bb4899
Ivan <your@mail.com>
2024-02-19 10:39:36 +0200

    problem with sorting

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,33 +1,76 @@
+import React, { useState, useEffect } from 'react';
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
+import { getPeople } from '../api';
 import { PeopleTable } from './PeopleTable';
+import { Person } from '../types';
 
-export const PeoplePage = () => {
+export const PeoplePage: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasNoPeople, setHasNoPeople] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getPeople()
+      .then(response => {
+        if (!response.length) {
+          setHasNoPeople(true);
+        }
+
+        setPeople(response);
+      })
+      .catch(error => {
+        setHasError(true);
+        setHasNoPeople(false);
+        throw error;
+      })
+      .finally(() => {
+        setIsLoading(false);
+
+        setTimeout(() => {
+          setHasError(false);
+        }, 2000);
+      });
+  }, []);
+
   return (
     <>
       <h1 className="title">People Page</h1>
 
-      <div className="block">
-        <div className="columns is-desktop is-flex-direction-row-reverse">
-          <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
-          </div>
+      {isLoading && <Loader />}
 
-          <div className="column">
-            <div className="box table-container">
-              <Loader />
+      {!isLoading && (
+        <div className="block">
+          <div className="columns is-desktop is-flex-direction-row-reverse">
+            <div className="column is-7-tablet is-narrow-desktop">
+              <PeopleFilters />
+            </div>
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+            <div className="column">
+              <div className="box table-container">
+                {hasError && (
+                  <p data-cy="peopleLoadingError" className="has-text-danger">
+                    Something went wrong
+                  </p>
+                )}
 
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
+                {hasNoPeople && (
+                  <p data-cy="noPeopleMessage">
+                    There are no people on the server
+                  </p>
+                )}
 
-              <p>There are no people matching the current search criteria</p>
+                <p>There are no people matching the current search criteria</p>
 
-              <PeopleTable />
+                {!hasNoPeople && !hasError && <PeopleTable people={people} />}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
---
+++ src/components/PersonLink.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import cn from 'classnames';
+import { Person } from '../types';
+
+type Props = {
+  person: Person;
+};
+
+export const PersonLink: React.FC<Props> = ({ person }) => {
+  return (
+    <Link
+      to={`../${person.slug}`}
+      className={cn({
+        'has-text-danger': person.sex === 'f',
+      })}
+    >
+      {person.name || '-'}
+    </Link>
+  );
+};
