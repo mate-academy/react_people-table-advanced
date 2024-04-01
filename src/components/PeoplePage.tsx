4b22ee31d976b51a6bf6cb31628c6f577cf70bf2
Eater228 <543145yura@gmail.com>
2024-04-01 15:39:20 +0300

    Failed test

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,47 @@
+/* eslint-disable max-len */
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { getPeople } from '../api';
+import { useEffect, useMemo, useState } from 'react';
+import { Person } from '../types';
+import { useSearchParams } from 'react-router-dom';
+import { getPreparedPeople } from '../utils/filterPeople';
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [searchParams] = useSearchParams();
+
+  const visiblePeople = useMemo(() => {
+    const options = {
+      sex: searchParams.get('sex'),
+      query: searchParams.get('query'),
+      centuries: searchParams.getAll('centuries'),
+      sort: searchParams.get('sort'),
+      order: searchParams.get('order'),
+    };
+
+    return getPreparedPeople(people, options);
+  }, [people, searchParams]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getPeople()
+      .then(setPeople)
+      .catch(() => setError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const hasSearchMatch =
+    !!people.length && !visiblePeople.length && !error && !isLoading;
+
+  const isPeopleTable =
+    !!people.length && !isLoading && !error && !hasSearchMatch;
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -10,20 +49,36 @@ export const PeoplePage = () => {
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
           <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
+            {people.length !== 0 && <PeopleFilters />}
           </div>
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
+              {isLoading && <Loader />}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+              {error && (
+                <p data-cy="peopleLoadingError">Something went wrong</p>
+              )}
+
+              {!people.length && !isLoading && !error && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
+
+              {/* <p data-cy="peopleLoadingError">Something went wrong</p>
 
               <p data-cy="noPeopleMessage">There are no people on the server</p>
 
-              <p>There are no people matching the current search criteria</p>
+              <p>There are no people matching the current search criteria</p> */}
 
-              <PeopleTable />
+              {hasSearchMatch && (
+                <p>There are no people matching the current search criteria</p>
+              )}
+
+              {isPeopleTable && (
+                <PeopleTable people={people} visiblePeople={visiblePeople} />
+              )}
             </div>
           </div>
         </div>
