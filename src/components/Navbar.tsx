93527e9bf2f8f66a44bbbd54b1b079644eb77b37
Yurii Yepikhov <yuriiyepikhov@gmail.com>
2024-04-05 03:04:40 +0300

    add task solution

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,12 @@
+import { NavLink, useLocation } from 'react-router-dom';
+import classNames from 'classnames';
+
+const addNavLinkClass = ({ isActive }: { isActive: boolean }) =>
+  classNames('navbar-item', { 'has-background-grey-lighter': isActive });
+
 export const Navbar = () => {
+  const { search } = useLocation();
+
   return (
     <nav
       data-cy="nav"
@@ -8,17 +16,16 @@ export const Navbar = () => {
     >
       <div className="container">
         <div className="navbar-brand">
-          <a className="navbar-item" href="#/">
+          <NavLink to="/" className={addNavLinkClass}>
             Home
-          </a>
+          </NavLink>
 
-          <a
-            aria-current="page"
-            className="navbar-item has-background-grey-lighter"
-            href="#/people"
+          <NavLink
+            to={{ pathname: '/people', search }}
+            className={addNavLinkClass}
           >
             People
-          </a>
+          </NavLink>
         </div>
       </div>
     </nav>
