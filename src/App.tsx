814942acda3b630ef8c7c6fec1a4c94420b6670f
Arkadiuszx <stefanski.a00@gmail.com>
2024-04-09 14:35:38 +0200

    Add Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
 import { PeoplePage } from './components/PeoplePage';
-import { Navbar } from './components/Navbar';
 
 import './App.scss';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
 
 export const App = () => {
   return (
-    <div data-cy="app">
-      <Navbar />
-
-      <div className="section">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<h1 className="title">Home Page</h1>} />
-            <Route path="/people" element={<PeoplePage />}>
-              <Route path=":personSlug" element={<PeoplePage />} />
-            </Route>
-            <Route path="/home" element={<Navigate to="/" replace />} />
-            <Route
-              path="*"
-              element={<h1 className="title">Page not found</h1>}
-            />
-          </Routes>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<h1 className="title">Home Page</h1>} />
+        <Route path="/people" element={<PeoplePage />}>
+          <Route path=":personSlug" element={<PeoplePage />} />
+        </Route>
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
   );
 };
