9cca7b2cfdc22ff547af03bfaad386d5e6e1c1fa
Vladyslav Zakharov <vladyslavzakharovdk@gmail.com>
2024-04-10 18:33:37 +0300

    fix show modals

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,14 +1,85 @@
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
 import { getPeople } from '../api';
 import { Person } from '../types';
+import { SearchParams } from '../utils/searchHelper';
 
 export const PeoplePage = () => {
   const [isLoader, setIsLoader] = useState(true);
   const [isError, setIsError] = useState(false);
   const [people, setPeople] = useState<Person[]>([]);
+  const [searchParams] = useSearchParams();
+  const sort = searchParams.get('sort') || '';
+  const order = searchParams.get('order') || '';
+  const query = searchParams.get('query') || '';
+  const centuries = searchParams.getAll('centuries') || [];
+  const sex = searchParams.get('sex') || '';
+
+  const toggleByParams = (param: string): SearchParams => {
+    if (param === sort) {
+      if (order === 'desc') {
+        return { sort: null, order: null };
+      }
+
+      return { sort: param, order: 'desc' };
+    }
+
+    return { sort: param };
+  };
+
+  const getClassParams = (param: string) => {
+    if (param === sort) {
+      if (order === 'desc') {
+        return 'fas fa-sort-up';
+      }
+
+      return 'fas fa-sort-down';
+    }
+
+    return 'fas fa-sort';
+  };
+
+  const getCentury = (year: number) => Math.floor(year / 100) + 1;
+
+  const preparePeopleList = [...people]
+    .sort((p1, p2) => {
+      let res = 0;
+
+      switch (sort) {
+        case 'name':
+          res = p1.name.localeCompare(p2.name);
+          break;
+        case 'sex':
+          res = p1.sex.localeCompare(p2.sex);
+          break;
+
+        case 'born':
+          res = p2.born - p1.born;
+          break;
+
+        case 'died':
+          res = p2.died - p1.died;
+          break;
+
+        default:
+          res = 0;
+          break;
+      }
+
+      return order ? -res : res;
+    })
+    .filter(person => {
+      return (
+        person.name.toLowerCase().includes(query.toLowerCase()) &&
+        person.sex.includes(sex) &&
+        (centuries.length
+          ? centuries.includes(`${getCentury(person.born)}`)
+          : true)
+      );
+    });
 
   useEffect(() => {
     getPeople()
@@ -37,17 +108,23 @@ export const PeoplePage = () => {
                 <p data-cy="peopleLoadingError">Something went wrong</p>
               )}
 
-              {!people.length && !isLoader && (
+              {!people.length && !isLoader && isError && (
                 <p data-cy="noPeopleMessage">
                   There are no people on the server
                 </p>
               )}
 
-              {!people.length && !isLoader && (
+              {!preparePeopleList.length && !isLoader && (
                 <p>There are no people matching the current search criteria</p>
               )}
 
-              {!!people.length && <PeopleTable people={people} />}
+              {!!preparePeopleList.length && (
+                <PeopleTable
+                  preparePeopleList={preparePeopleList}
+                  toggleByParams={toggleByParams}
+                  getClassParams={getClassParams}
+                />
+              )}
             </div>
           </div>
         </div>
--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,86 +1,23 @@
 import React from 'react';
 import { Person } from '../types';
 import { PersonLink } from './PersonLink';
-import { useSearchParams } from 'react-router-dom';
 import { SearchLink } from './SearchLink';
 import { SearchParams } from '../utils/searchHelper';
 
 interface Props {
-  people: Person[];
+  preparePeopleList: Person[];
+  toggleByParams: (param: string) => SearchParams;
+  getClassParams: (
+    param: string,
+  ) => 'fas fa-sort-up' | 'fas fa-sort-down' | 'fas fa-sort';
 }
 
 /* eslint-disable jsx-a11y/control-has-associated-label */
-export const PeopleTable: React.FC<Props> = ({ people }) => {
-  const [searchParams] = useSearchParams();
-  const sort = searchParams.get('sort') || '';
-  const order = searchParams.get('order') || '';
-  const query = searchParams.get('query') || '';
-  const centuries = searchParams.getAll('centuries') || [];
-  const sex = searchParams.get('sex') || '';
-
-  const toggleByParams = (param: string): SearchParams => {
-    if (param === sort) {
-      if (order === 'desc') {
-        return { sort: null, order: null };
-      }
-
-      return { sort: param, order: 'desc' };
-    }
-
-    return { sort: param };
-  };
-
-  const getClassParams = (param: string) => {
-    if (param === sort) {
-      if (order === 'desc') {
-        return 'fas fa-sort-up';
-      }
-
-      return 'fas fa-sort-down';
-    }
-
-    return 'fas fa-sort';
-  };
-
-  const getCentury = (year: number) => Math.floor(year / 100) + 1;
-
-  const preparePeopleList = [...people]
-    .sort((p1, p2) => {
-      let res = 0;
-
-      switch (sort) {
-        case 'name':
-          res = p1.name.localeCompare(p2.name);
-          break;
-        case 'sex':
-          res = p1.sex.localeCompare(p2.sex);
-          break;
-
-        case 'born':
-          res = p2.born - p1.born;
-          break;
-
-        case 'died':
-          res = p2.died - p1.died;
-          break;
-
-        default:
-          res = 0;
-          break;
-      }
-
-      return order ? -res : res;
-    })
-    .filter(person => {
-      return (
-        person.name.toLowerCase().includes(query.toLowerCase()) &&
-        person.sex.includes(sex) &&
-        (centuries.length
-          ? centuries.includes(`${getCentury(person.born)}`)
-          : true)
-      );
-    });
-
+export const PeopleTable: React.FC<Props> = ({
+  preparePeopleList,
+  toggleByParams,
+  getClassParams,
+}) => {
   return (
     <table
       data-cy="peopleTable"
@@ -139,7 +76,11 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
 
       <tbody>
         {preparePeopleList.map(person => (
-          <PersonLink people={people} person={person} key={person.slug} />
+          <PersonLink
+            people={preparePeopleList}
+            person={person}
+            key={person.slug}
+          />
         ))}
       </tbody>
     </table>
