6346903478dab67cc5c279ee68db1f8554d0a323
shikoryak <oksanashykoriak@gmail.com>
2024-04-16 16:08:20 +0300

    fixed mistakes

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -9,7 +9,7 @@ import { useSearchParams } from 'react-router-dom';
 export const PeoplePage = () => {
   const [people, setPeople] = useState<Person[]>([]);
   const [loading, setLoading] = useState(true);
-  const [failed, setFailed] = useState(false);
+  const [hasError, setHasError] = useState(false);
   const [searchParams] = useSearchParams();
   const query = (searchParams.get('query') || '').toLowerCase();
   const centuries = (searchParams.getAll('centuries') || []).map(
@@ -20,24 +20,24 @@ export const PeoplePage = () => {
   useEffect(() => {
     getPeople()
       .then(data => setPeople(data))
-      .catch(() => setFailed(true))
+      .catch(() => setHasError(true))
       .finally(() => {
         setLoading(false);
       });
   }, []);
 
   const visiblePeople = people.filter(person => {
-    return (
-      (query
-        ? person.name.toLowerCase().includes(query) ||
-          person.motherName?.toLowerCase().includes(query) ||
-          person.fatherName?.toLowerCase().includes(query)
-        : true) &&
-      (centuries.length
-        ? centuries.includes(+`${person.born}`.slice(0, 2) + 1)
-        : true) &&
-      (sex ? person.sex === sex : true)
-    );
+    const queryResult = query
+      ? person.name.toLowerCase().includes(query) ||
+        person.motherName?.toLowerCase().includes(query) ||
+        person.fatherName?.toLowerCase().includes(query)
+      : true;
+    const centuriesResult = centuries.length
+      ? centuries.includes(+`${person.born}`.slice(0, 2) + 1)
+      : true;
+    const sexResult = sex ? person.sex === sex : true;
+
+    return queryResult && centuriesResult && sexResult;
   });
 
   return (
@@ -54,7 +54,7 @@ export const PeoplePage = () => {
             <div className="box table-container">
               {loading ? (
                 <Loader />
-              ) : failed ? (
+              ) : hasError ? (
                 <p data-cy="peopleLoadingError" className="has-text-danger">
                   Something went wrong
                 </p>
