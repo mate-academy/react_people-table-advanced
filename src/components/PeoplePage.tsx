1d6a6694017b91f79bb33835aea329f149b1519e
zozulia.kirill <kirillzozulya95@gmail.com>
2024-04-17 13:46:42 +0300

    draft: init

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from 'react';
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { Person } from '../types';
+import { getPeople } from '../api';
 
-export const PeoplePage = () => {
+export const PeoplePage: React.FC = () => {
+  const [persons, setPersons] = useState<Person[] | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState('Something went wrong');
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getPeople()
+      .then(data => {
+        const preparedData = data.map(p => {
+          const mother = data.find(d => d.name === p.motherName);
+          const father = data.find(d => d.name === p.fatherName);
+
+          return {
+            ...p,
+            mother: mother || undefined,
+            father: father || undefined,
+          };
+        });
+
+        setPersons(preparedData);
+      })
+      .catch(err => {
+        setErrorMessage(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+        setErrorMessage('');
+      });
+  }, []);
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -15,15 +49,29 @@ export const PeoplePage = () => {
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
+              {isLoading && <Loader />}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+              {!isLoading && (
+                <>
+                  {errorMessage && (
+                    <p data-cy="peopleLoadingError" className="has-text-danger">
+                      {errorMessage}
+                    </p>
+                  )}
 
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
+                  {persons?.length === 0 && (
+                    <p data-cy="noPeopleMessage">
+                      There are no people on the server
+                    </p>
+                  )}
 
-              <p>There are no people matching the current search criteria</p>
+                  <p>
+                    There are no people matching the current search criteria
+                  </p>
 
-              <PeopleTable />
+                  {persons && <PeopleTable persons={persons} />}
+                </>
+              )}
             </div>
           </div>
         </div>
