357cf04ce2d015d2caeec41764b260258568856a
Yaroslav Horodynskyi <gorodinskiyyar@gmail.com>
2024-04-30 14:12:25 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
+import './App.scss';
 import { PeoplePage } from './components/PeoplePage';
 import { Navbar } from './components/Navbar';
-
-import './App.scss';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div data-cy="app">
       <Navbar />
 
-      <div className="section">
+      <div className="section container">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <Routes>
+            <Route path="/" element={<h1 className="title">Home Page</h1>} />
+            <Route path="/home" element={<Navigate to="/" />} />
+            <Route
+              path="/people/:personSlug?/:?params?"
+              element={<PeoplePage />}
+            />
+
+            <Route
+              path="*"
+              element={<h1 className="title">Page not found</h1>}
+            />
+          </Routes>
         </div>
       </div>
     </div>
