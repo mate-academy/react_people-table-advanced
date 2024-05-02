397ccdaf746096f06c7d4d4b8e82a7c7d166cad4
Lesia <voloshka2912@icloud.com>
2024-05-02 16:07:56 +0300

    add TableHeader, use visiblePeople in PeoplePage

--- src/pages/PeoplePage/PeoplePage.tsx
+++ src/pages/PeoplePage/PeoplePage.tsx
@@ -2,9 +2,25 @@ import { PeopleFilters } from '../../components/PeopleFilters/PeopleFilters';
 import { Loader } from '../../components/Loader';
 import { PeopleTable } from '../../components/PeopleTable/PeopleTable';
 import { usePeople } from '../../hooks/usePeople';
+import { useSearchParams } from 'react-router-dom';
+import { getVisiblePeople } from '../../utils/getVisiblePeople';
 
 export const PeoplePage = () => {
   const { people, error, isLoading } = usePeople();
+  const [searchParams] = useSearchParams();
+  const sort = searchParams.get('sort');
+  const order = searchParams.get('order');
+  const query = searchParams.get('query');
+  const centuries = searchParams.getAll('centuries');
+  const sex = searchParams.get('sex');
+
+  const visiblePeople = getVisiblePeople(people, {
+    query,
+    centuries,
+    sex,
+    sort,
+    order,
+  });
 
   return (
     <>
@@ -26,7 +42,14 @@ export const PeoplePage = () => {
                   There are no people on the server
                 </p>
               )}
-              {!!people.length && <PeopleTable />}
+              {!!people.length &&
+                (visiblePeople.length ? (
+                  <PeopleTable visiblePeople={visiblePeople} />
+                ) : (
+                  <p>
+                    There are no people matching the current search criteria
+                  </p>
+                ))}
             </div>
           </div>
         </div>
--- src/utils/getVisiblePeople.tsx
+++ src/utils/getVisiblePeople.tsx
@@ -2,36 +2,42 @@ import { PersonType } from '../types/PersonType';
 
 export const getVisiblePeople = (
   people: PersonType[],
-  query: string | null,
-  centuries: string[],
-  sex: string | null,
-  sort: string | null,
-  order: string | null,
+  filters: {
+    query: string | null;
+    centuries: string[];
+    sex: string | null;
+    sort: string | null;
+    order: string | null;
+  },
 ) => {
   let filteredPeople: PersonType[] = [...people];
 
-  if (query) {
+  if (filters.query) {
+    const query = filters.query;
+
     filteredPeople = filteredPeople.filter(person =>
       person.name.toLowerCase().includes(query),
     );
   }
 
-  if (sex) {
-    filteredPeople = filteredPeople.filter(person => person.sex === sex);
+  if (filters.sex) {
+    filteredPeople = filteredPeople.filter(
+      person => person.sex === filters.sex,
+    );
   }
 
-  if (centuries.length !== 0) {
+  if (filters.centuries.length !== 0) {
     filteredPeople = filteredPeople.filter(person => {
       const century = Math.ceil(person.born / 100);
 
-      return centuries.includes(century.toString());
+      return filters.centuries.includes(century.toString());
     });
   }
 
-  if (sort) {
+  if (filters.sort) {
     filteredPeople = filteredPeople.sort((person1, person2) => {
-      const valueA = person1[sort as keyof PersonType];
-      const valueB = person2[sort as keyof PersonType];
+      const valueA = person1[filters.sort as keyof PersonType];
+      const valueB = person2[filters.sort as keyof PersonType];
 
       if (typeof valueA === 'string' && typeof valueB === 'string') {
         return valueA.localeCompare(valueB);
@@ -45,7 +51,7 @@ export const getVisiblePeople = (
     });
   }
 
-  if (order) {
+  if (filters.order) {
     filteredPeople.reverse();
   }
 
