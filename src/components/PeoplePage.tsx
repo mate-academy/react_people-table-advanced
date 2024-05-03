170fed8485cae568d25cf55cf9b86e2e1c87987e
SerjDSA <serjdsa7@gmail.com>
2024-05-03 17:24:54 +0300

    fixed last commits

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useMemo, useState } from 'react';
 import { PeopleFilters } from './PeopleFilters';
-import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { Person } from '../types';
+import { getPeople } from '../api';
+import { useSearchParams } from 'react-router-dom';
+import { findParents, preparePeople } from '../utils/peopleHelper';
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [search] = useSearchParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+    setHasError(false);
+
+    getPeople()
+      .then(peopleFromServer => {
+        const peopleWithParents = findParents(peopleFromServer);
+
+        setPeople(peopleWithParents);
+      })
+      .catch(() => setHasError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const preparedPeople = useMemo(
+    () => preparePeople(people, search),
+    [people, search],
+  );
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -10,20 +38,16 @@ export const PeoplePage = () => {
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
           <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
+            {!isLoading && <PeopleFilters />}
           </div>
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
-
-              <p data-cy="peopleLoadingError">Something went wrong</p>
-
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
-
-              <p>There are no people matching the current search criteria</p>
-
-              <PeopleTable />
+              <PeopleTable
+                people={preparedPeople}
+                isLoading={isLoading}
+                hasError={hasError}
+              />
             </div>
           </div>
         </div>
