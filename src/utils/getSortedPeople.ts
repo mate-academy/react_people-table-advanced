d3ef0718b64d5d7bec87ba396dd4e79b7a65088f
Olaf <olaf.chuszno@gmail.com>
2024-05-03 10:48:21 +0200

    Remove redundant condition

--- src/utils/getSortedPeople.ts
+++ src/utils/getSortedPeople.ts
@@ -8,29 +8,27 @@ export const getSortedPeople = (
   const sortedPeople = people;
 
   if (sortBy) {
-    if (sortBy) {
-      switch (sortBy) {
-        case 'name':
-          sortedPeople.sort((personA: Person, personB: Person) =>
-            personA.name.localeCompare(personB.name),
-          );
-          break;
-        case 'sex':
-          sortedPeople.sort((personA: Person, personB: Person) =>
-            personA.sex.localeCompare(personB.sex),
-          );
-          break;
-        case 'born':
-          sortedPeople.sort(
-            (personA: Person, personB: Person) => personA.born - personB.born,
-          );
-          break;
-        case 'died':
-          sortedPeople.sort(
-            (personA: Person, personB: Person) => personA.died - personB.died,
-          );
-          break;
-      }
+    switch (sortBy) {
+      case 'name':
+        sortedPeople.sort((personA: Person, personB: Person) =>
+          personA.name.localeCompare(personB.name),
+        );
+        break;
+      case 'sex':
+        sortedPeople.sort((personA: Person, personB: Person) =>
+          personA.sex.localeCompare(personB.sex),
+        );
+        break;
+      case 'born':
+        sortedPeople.sort(
+          (personA: Person, personB: Person) => personA.born - personB.born,
+        );
+        break;
+      case 'died':
+        sortedPeople.sort(
+          (personA: Person, personB: Person) => personA.died - personB.died,
+        );
+        break;
     }
   }
 
