9ead4dbc062537d5bda3c06e62704326aca38cf2
Maryna Karpova <mari.artarea@gmail.com>
2024-05-16 11:37:34 +0200

    changed active link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,7 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
-  const location = useLocation();
-  const [isActive, setIsActive] = useState<string>('');
-
-  useEffect(() => {
-    const currentLink = location.pathname;
-
-    if (currentLink === '/' || currentLink === '/home') {
-      setIsActive('home');
-    } else if (currentLink.startsWith('/people')) {
-      setIsActive('people');
-    } else {
-      setIsActive('');
-    }
-  }, [location]);
-
-  const handleClick = (link: string) => {
-    setIsActive(link);
-  };
-
   return (
     <nav
       data-cy="nav"
@@ -32,17 +12,19 @@ export const NavBar = () => {
       <div className="container">
         <div className="navbar-brand">
           <NavLink
-            className={`navbar-item ${isActive === 'home' && 'has-background-grey-lighter'}`}
+            className={({ isActive }) =>
+              `navbar-item ${isActive ? 'has-background-grey-lighter' : ''}`
+            }
             to="/"
-            onClick={() => handleClick('home')}
           >
             Home
           </NavLink>
 
           <NavLink
-            className={`navbar-item ${isActive === 'people' && 'has-background-grey-lighter'}`}
+            className={({ isActive }) =>
+              `navbar-item ${isActive ? 'has-background-grey-lighter' : ''}`
+            }
             to="/people"
-            onClick={() => handleClick('people')}
           >
             People
           </NavLink>
