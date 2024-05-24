b795be9a593c3e73e3d17ed7b7373b17293d7e24
Andrii Dudar <andrij.dudar@gmail.com>
2024-05-25 02:20:42 +0300

    Solution

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,71 @@
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { getPeople } from '../api';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { Person } from '../types';
 
 export const PeoplePage = () => {
+  const [peopleFromServer, setPeopleFromServer] = useState<Person[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [noPeople, setNoPeople] = useState(false);
+  const [filtredPeople, setFiltredPeople] =
+    useState<Person[]>(peopleFromServer);
+
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    setLoading(true);
+    getPeople()
+      .then(people => {
+        if (!people.length) {
+          setNoPeople(true);
+        } else {
+          setPeopleFromServer(people);
+        }
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    let filteredPeoples = peopleFromServer;
+
+    const query = searchParams.get('query') || '';
+    const sex = searchParams.get('sex') || '';
+    const centuries = searchParams.getAll('centuries') || [];
+
+    if (sex) {
+      filteredPeoples = filteredPeoples.filter(
+        (person: Person) => person.sex === sex,
+      );
+    }
+
+    if (centuries.length) {
+      filteredPeoples = filteredPeoples.filter((person: Person) => {
+        return centuries.includes(person.born.toString().slice(0, 2));
+      });
+    }
+
+    if (query !== '') {
+      filteredPeoples = filteredPeoples.filter((person: Person) => {
+        return (
+          person.name.toLowerCase().includes(query.toLowerCase()) ||
+          person.fatherName?.toLowerCase().includes(query.toLowerCase()) ||
+          person.motherName?.toLowerCase().includes(query.toLowerCase())
+        );
+      });
+    }
+
+    setFiltredPeople(filteredPeoples);
+  }, [searchParams, peopleFromServer]);
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -10,20 +73,28 @@ export const PeoplePage = () => {
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
           <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
+            {!error && <PeopleFilters />}
           </div>
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
+              {loading && <Loader />}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+              {error && (
+                <p data-cy="peopleLoadingError" className="has-text-danger">
+                  Something went wrong
+                </p>
+              )}
 
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
+              {noPeople && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
 
-              <p>There are no people matching the current search criteria</p>
-
-              <PeopleTable />
+              {!noPeople && !error && !loading && (
+                <PeopleTable people={filtredPeople} />
+              )}
             </div>
           </div>
         </div>
