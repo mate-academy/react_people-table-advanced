28af9318417d9f7cb31ab4f08cef72d3388b8ce0
Grygoryy <kachurovskyy123@gmail.com>
2024-05-29 16:03:21 +0300

    solution

---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable */
+import { useParams } from 'react-router-dom';
+
+import { Person } from '../../types';
+import { PersonLink } from '../PersonLink/PersonLink';
+import { TitleFilter } from '../TitleFilter/TitleFilter';
+
+type Props = {
+  people: Person[];
+};
+
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  const { slug } = useParams();
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <TitleFilter />
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <tr
+            data-cy="person"
+            key={person.slug}
+            className={person.slug === slug ? 'has-background-warning' : ''}
+          >
+            <td>
+              <PersonLink person={person} />
+            </td>
+
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            <td>
+              {people.find(
+                personName => person.motherName === personName.name,
+              ) ? (
+                <PersonLink
+                  person={
+                    people.find(
+                      currPerson => currPerson.name === person.motherName,
+                    )!
+                  }
+                />
+              ) : person.motherName ? (
+                person.motherName
+              ) : (
+                '-'
+              )}
+            </td>
+            <td>
+              {people.find(persName => person.fatherName === persName.name) ? (
+                <PersonLink
+                  person={
+                    people.find(
+                      currPerson => currPerson.name === person.fatherName,
+                    )!
+                  }
+                />
+              ) : person.fatherName ? (
+                person.fatherName
+              ) : (
+                '-'
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
