ed62aa43615ed3051444b17371a24a4f0f63bf49
Andrew Rich <andy.rich3a@gmail.com>
2024-06-02 15:54:17 +0300

    solution

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,11 @@
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { usePeople } from '../providers/PeopleProvider';
 
 export const PeoplePage = () => {
+  const { people, peopleCount, pending, error } = usePeople();
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -10,20 +13,26 @@ export const PeoplePage = () => {
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
           <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
+            {!pending && !!peopleCount && <PeopleFilters />}
           </div>
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
-
-              <p data-cy="peopleLoadingError">Something went wrong</p>
-
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
-
-              <p>There are no people matching the current search criteria</p>
-
-              <PeopleTable />
+              {pending && <Loader />}
+              {error && (
+                <p data-cy="peopleLoadingError" className="has-text-danger">
+                  Something went wrong
+                </p>
+              )}
+              {!pending && !peopleCount && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
+              {!pending && !!peopleCount && !people.length && (
+                <p>There are no people matching the current search criteria</p>
+              )}
+              {!pending && !!people.length && <PeopleTable />}
             </div>
           </div>
         </div>
