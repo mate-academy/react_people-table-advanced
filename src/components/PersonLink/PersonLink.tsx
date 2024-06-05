9001e080affff251f321d7b01716804f566bf66d
katya havriuk <0303gavkat@gmail.com>
2024-06-05 09:31:25 -0600

    fixed resseting search params on person click, fixed resseting all filters

--- src/components/PersonLink/PersonLink.tsx
+++ src/components/PersonLink/PersonLink.tsx
@@ -1,15 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Person } from '../../types';
 
 type Props = {
   person: Person;
 };
 
-export const PersonLink: React.FC<Props> = ({ person }) => (
-  <Link
-    className={person.sex === 'f' ? 'has-text-danger' : ''}
-    to={`/people/${person.slug}`}
-  >
-    {person.name}
-  </Link>
-);
+export const PersonLink: React.FC<Props> = ({ person }) => {
+  const location = useLocation();
+  const search = location.search;
+
+  return (
+    <Link
+      className={person.sex === 'f' ? 'has-text-danger' : ''}
+      to={`/people/${person.slug}${search}`}
+    >
+      {person.name}
+    </Link>
+  );
+};
