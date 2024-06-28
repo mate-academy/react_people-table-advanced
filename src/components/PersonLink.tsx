8deccb5d026c1bb5d5c69e607a5ca072cad1740d
Nazar Pastuschak <kns19pna@gmail.com>
2024-06-28 11:59:28 +0300

    add task solution

---
+++ src/components/PersonLink.tsx
@@ -0,0 +1,55 @@
+import { Link, useLocation, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import { Person } from '../types';
+
+type Props = {
+  person: Person;
+};
+
+export const PersonLink: React.FC<Props> = ({ person }) => {
+  const { slug } = useParams();
+  const location = useLocation();
+
+  return (
+    <tr
+      data-cy="person"
+      className={classNames({ 'has-background-warning': slug === person.slug })}
+    >
+      <td>
+        <Link
+          to={`/people/${person.slug}${location.search}`}
+          className={classNames({
+            'has-text-danger': person.sex === 'f',
+          })}
+        >
+          {person.name}
+        </Link>
+      </td>
+
+      <td>{person.sex}</td>
+      <td>{person.born}</td>
+      <td>{person.died}</td>
+      <td>
+        {person.mother ? (
+          <Link
+            to={`/people/${person.mother.slug}${location.search}`}
+            className="has-text-danger"
+          >
+            {person.motherName}
+          </Link>
+        ) : (
+          person.motherName || '-'
+        )}
+      </td>
+      <td>
+        {person.father ? (
+          <Link to={`/people/${person.father.slug}${location.search}`}>
+            {person.fatherName}
+          </Link>
+        ) : (
+          person.fatherName || '-'
+        )}
+      </td>
+    </tr>
+  );
+};
