bbee4fdaccbc1a24b484670dbae651c2ffb04746
Danylo-Chumakov <danil26032003@gmail.com>
2024-07-12 14:31:07 +0300

    add task solution

--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,12 +1,96 @@
-import React from 'react';
+/* eslint-disable no-param-reassign */
+import React, { useState, useMemo } from 'react';
 import { Person } from '../types/Person';
 import { PersonLink } from './PersonLink';
+import { useSearchParams } from 'react-router-dom';
 
 type Props = {
   people: Person[];
 };
 
+type SortKey = keyof Omit<Person, 'slug' | 'mother' | 'father'>;
+
 export const PeopleTable: React.FC<Props> = ({ people }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [sortConfig, setSortConfig] = useState<{
+    key: SortKey;
+    direction: string;
+  } | null>(null);
+  const [highlightedName, setHighlightedName] = useState<string | null>(null);
+
+  const handleSort = (key: SortKey) => {
+    let direction = 'asc';
+
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === 'asc'
+    ) {
+      direction = 'desc';
+    } else if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === 'desc'
+    ) {
+      direction = '';
+    }
+
+    setSortConfig(direction ? { key, direction } : null);
+    const newParams = new URLSearchParams(searchParams);
+
+    if (direction) {
+      newParams.set('sort', key);
+      newParams.set('order', direction);
+    } else {
+      newParams.delete('sort');
+      newParams.delete('order');
+    }
+
+    setSearchParams(newParams);
+  };
+
+  const sortedPeople = useMemo(() => {
+    if (sortConfig) {
+      return [...people].sort((a, b) => {
+        const aValue = a[sortConfig.key];
+        const bValue = b[sortConfig.key];
+
+        if (aValue === null || bValue === null) {
+          if (aValue === null && bValue !== null) {
+            return sortConfig.direction === 'asc' ? -1 : 1;
+          }
+
+          if (aValue !== null && bValue === null) {
+            return sortConfig.direction === 'asc' ? 1 : -1;
+          }
+
+          return 0;
+        }
+
+        if (aValue < bValue) {
+          return sortConfig.direction === 'asc' ? -1 : 1;
+        }
+
+        if (aValue > bValue) {
+          return sortConfig.direction === 'asc' ? 1 : -1;
+        }
+
+        return 0;
+      });
+    }
+
+    return people;
+  }, [people, sortConfig]);
+
+  const allNames = people.reduce(
+    (acc, person) => {
+      acc[person.name] = person;
+
+      return acc;
+    },
+    {} as { [key: string]: Person },
+  );
+
   return (
     <table
       data-cy="peopleTable"
@@ -14,18 +98,24 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
     >
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Sex</th>
-          <th>Born</th>
-          <th>Died</th>
+          <th onClick={() => handleSort('name')}>Name</th>
+          <th onClick={() => handleSort('sex')}>Sex</th>
+          <th onClick={() => handleSort('born')}>Born</th>
+          <th onClick={() => handleSort('died')}>Died</th>
           <th>Mother</th>
           <th>Father</th>
         </tr>
       </thead>
 
       <tbody>
-        {people.map(person => (
-          <PersonLink person={person} key={person.slug} />
+        {sortedPeople.map(person => (
+          <PersonLink
+            person={person}
+            key={person.slug}
+            allNames={allNames}
+            highlightedName={highlightedName}
+            setHighlightedName={setHighlightedName}
+          />
         ))}
       </tbody>
     </table>
--- src/components/PersonLink.tsx
+++ src/components/PersonLink.tsx
@@ -1,22 +1,63 @@
 import classNames from 'classnames';
 import { Person } from '../types/Person';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import React from 'react';
 
 type Props = {
   person: Person;
+  allNames: { [key: string]: Person };
+  highlightedName: string | null;
+  setHighlightedName: (name: string) => void;
 };
 
-export const PersonLink: React.FC<Props> = ({ person }) => {
+export const PersonLink: React.FC<Props> = ({
+  person,
+  allNames,
+  highlightedName,
+}) => {
   const { slug } = useParams();
   const { name, sex, born, died, mother, father, fatherName, motherName } =
     person;
+  const navigate = useNavigate();
+
+  const getClassName = (personName: string | null | undefined) => {
+    if (personName && allNames[personName]) {
+      return allNames[personName].sex === 'f'
+        ? 'has-text-danger'
+        : 'has-text-link';
+    }
+
+    return '';
+  };
+
+  const handleClick = (personName: string | null | undefined) => {
+    if (personName && allNames[personName]) {
+      navigate(`/people/${allNames[personName].slug}`);
+    }
+  };
+
+  const getPersonLink = (personName: string | null | undefined) => {
+    if (personName && allNames[personName]) {
+      return (
+        <span
+          className={getClassName(personName)}
+          onClick={() => handleClick(personName)}
+          style={{ cursor: 'pointer' }}
+        >
+          {personName}
+        </span>
+      );
+    }
+
+    return personName || '-';
+  };
 
   return (
     <tr
       data-cy="person"
       className={classNames({
         'has-background-warning': slug === person.slug,
+        'has-background-info': highlightedName === name,
       })}
     >
       <td>
@@ -24,6 +65,7 @@ export const PersonLink: React.FC<Props> = ({ person }) => {
           to={`/people/${person.slug}`}
           className={classNames({
             'has-text-danger': sex === 'f',
+            'has-text-link': sex === 'm',
           })}
         >
           {name}
@@ -33,23 +75,8 @@ export const PersonLink: React.FC<Props> = ({ person }) => {
       <td>{sex}</td>
       <td>{born}</td>
       <td>{died}</td>
-      <td>
-        {mother ? (
-          <Link className="has-text-danger" to={`/people/${mother.slug}`}>
-            {mother.name}
-          </Link>
-        ) : (
-          motherName || '-'
-        )}
-      </td>
-
-      <td>
-        {father ? (
-          <Link to={`/people/${father.slug}`}>{father.name}</Link>
-        ) : (
-          fatherName || '-'
-        )}
-      </td>
+      <td>{getPersonLink(mother?.name || motherName)}</td>
+      <td>{getPersonLink(father?.name || fatherName)}</td>
     </tr>
   );
 };
