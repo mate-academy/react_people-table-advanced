8a9d8ac4b61ae8702372b5a6bd9b352d671c2993
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2024-07-25 16:07:02 +0300

    add solution for Q&A

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { PeoplePage } from './components/PeoplePage';
 import { Navbar } from './components/Navbar';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import './App.scss';
 
@@ -8,13 +9,22 @@ export const App = () => {
     <div data-cy="app">
       <Navbar />
 
-      <div className="section">
+      <main className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <Routes>
+            <Route path="/" element={<h1 className="title">Home Page</h1>} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+            <Route path="/people" element={<PeoplePage />}>
+              <Route index element={<PeoplePage />} />
+              <Route path=":humanId" element={<PeoplePage />} />
+            </Route>
+            <Route
+              path="*"
+              element={<h1 className="title">Page not found</h1>}
+            />
+          </Routes>
         </div>
-      </div>
+      </main>
     </div>
   );
 };
