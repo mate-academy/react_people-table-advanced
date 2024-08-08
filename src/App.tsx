923de6dbfddeb5b6cdee827598d4813da0d9b159
Alina Kabanets <aokabanets@gmail.com>
2024-08-08 13:01:49 +0100

    fix Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -20,32 +20,12 @@
 // };
 
 import './App.scss';
-import { NavLink, Outlet } from 'react-router-dom';
-import classNames from 'classnames';
-
-const getLinkClass = ({ isActive }: { isActive: boolean }) =>
-  classNames('navbar-item', { 'has-background-grey-lighter': isActive });
+import { Outlet } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
 
 export const App = () => (
   <div data-cy="app">
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink className={getLinkClass} to="/">
-            Home
-          </NavLink>
-
-          <NavLink className={getLinkClass} to="/people">
-            People
-          </NavLink>
-        </div>
-      </div>
-    </nav>
+    <Navbar />
 
     <main className="section">
       <div className="container">
