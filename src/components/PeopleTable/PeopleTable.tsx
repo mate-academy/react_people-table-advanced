f4d8a424ef21473b4ab7f75759c8cdffb9821442
Alina Kabanets <aokabanets@gmail.com>
2024-08-08 13:08:22 +0100

    add thead to the PeopleTable from the new task

--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -64,10 +64,50 @@ export const PeopleTable = () => {
             >
               <thead>
                 <tr>
-                  <th>Name</th>
-                  <th>Sex</th>
-                  <th>Born</th>
-                  <th>Died</th>
+                  <th>
+                    <span className="is-flex is-flex-wrap-nowrap">
+                      Name
+                      <a href="#/people?sort=name">
+                        <span className="icon">
+                          <i className="fas fa-sort" />
+                        </span>
+                      </a>
+                    </span>
+                  </th>
+
+                  <th>
+                    <span className="is-flex is-flex-wrap-nowrap">
+                      Sex
+                      <a href="#/people?sort=sex">
+                        <span className="icon">
+                          <i className="fas fa-sort" />
+                        </span>
+                      </a>
+                    </span>
+                  </th>
+
+                  <th>
+                    <span className="is-flex is-flex-wrap-nowrap">
+                      Born
+                      <a href="#/people?sort=born&amp;order=desc">
+                        <span className="icon">
+                          <i className="fas fa-sort-up" />
+                        </span>
+                      </a>
+                    </span>
+                  </th>
+
+                  <th>
+                    <span className="is-flex is-flex-wrap-nowrap">
+                      Died
+                      <a href="#/people?sort=died">
+                        <span className="icon">
+                          <i className="fas fa-sort" />
+                        </span>
+                      </a>
+                    </span>
+                  </th>
+
                   <th>Mother</th>
                   <th>Father</th>
                 </tr>
