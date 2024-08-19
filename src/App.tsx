9f3c0e2244eceda2eed2f9aad0540fb8e2e63edc
Vlad <vladkygot@gmail.com>
2024-08-19 16:36:53 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,27 @@ import { PeoplePage } from './components/PeoplePage';
 import { Navbar } from './components/Navbar';
 
 import './App.scss';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
-export const App = () => {
-  return (
-    <div data-cy="app">
-      <Navbar />
+export const App = () => (
+  <div data-cy="app">
+    <Navbar />
 
-      <div className="section">
-        <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
-        </div>
+    <main className="section">
+      <div className="container">
+        <Routes>
+          <Route
+            path="/"
+            element={<h1 className="title">Home Page</h1>}
+          ></Route>
+          <Route path="/home" element={<Navigate to={'/'} />}></Route>
+          <Route path="people">
+            <Route index element={<PeoplePage />} />
+            <Route path=":slug" element={<PeoplePage />} />
+          </Route>
+          <Route path="*" element={<h1 className="title">Page not found</h1>} />
+        </Routes>
       </div>
-    </div>
-  );
-};
+    </main>
+  </div>
+);
