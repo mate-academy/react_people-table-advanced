b481dbae59a4ccedb5bfaa87a283b2fdc0474a37
Iryna Yehorova <yehorova.dev@gmail.com>
2024-09-02 00:53:43 +0300

    add solution

---
+++ src/types/SearchParams.ts
@@ -0,0 +1,9 @@
+export enum SearchParams {
+  sex = 'sex',
+  centuries = 'centuries',
+  query = 'query',
+  sort = 'sort',
+  order = 'order',
+}
+
+export type SearchParamsValue = `${SearchParams}`;
---
+++ src/types/Sex.ts
@@ -0,0 +1,7 @@
+export enum Sex {
+  Female = 'f',
+  Male = 'm',
+  All = '',
+}
+
+export type SexType = `${Sex}`;
