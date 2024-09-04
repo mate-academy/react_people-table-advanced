03e22ecf8af4ff9011886df6c997c1c1c2cd43e1
Maksym <max.borovique@gmail.com>
2024-09-04 15:43:12 +0300

    panic

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -4,6 +4,7 @@ import { Loader } from '../Loader';
 import { PeopleTable } from '../PeopleTable';
 import { Person } from '../../types';
 import { getPreparedPerson } from '../../utils/getPreparedPeople';
+import { PeopleFilters } from '../PeopleFilters/PeopleFilters';
 
 export const PeoplePage = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -33,19 +34,29 @@ export const PeoplePage = () => {
         <div className="container">
           <h1 className="title">People Page</h1>
           <div className="block">
-            <div className="box table-container">
-              {isLoading && <Loader />}
-              {isError && (
-                <p data-cy="peopleLoadingError" className="has-text-danger">
-                  Something went wrong
-                </p>
-              )}
-              {peopleList.length === 0 && !isLoading && !isError && (
-                <p data-cy="noPeopleMessage">
-                  There are no people on the server
-                </p>
-              )}
-              {peopleList.length > 0 && <PeopleTable people={preparedPeople} />}
+            <div className="columns is-desktop is-flex-direction-row-reverse">
+              <div className="column is-7-tablet is-narrow-desktop">
+                <PeopleFilters />
+              </div>
+              <div className="column">
+                <div className="box table-container">
+                  {isLoading && <Loader />}
+                  {isError && (
+                    <p data-cy="peopleLoadingError" className="has-text-danger">
+                      Something went wrong
+                    </p>
+                  )}
+                  {peopleList.length === 0 && !isLoading && !isError && (
+                    <p data-cy="noPeopleMessage">
+                      There are no people on the server
+                    </p>
+                  )}
+                  {peopleList.length > 0 && (
+                    <PeopleTable people={preparedPeople} />
+                  )}
+                  {/* <p>There are no people matching the current search criteria</p> */}
+                </div>
+              </div>
             </div>
           </div>
         </div>
