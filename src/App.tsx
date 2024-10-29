b23ddbe6437ddb40470ee17e7ede1417c124eab1
YuriiMykhailenko <urijmihajlenko3@gmail.com>
2024-10-29 15:26:28 +0200

    fix3

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,6 @@ export const App = () => {
 
       <div className="section">
         <div className="container">
-          {/* <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1> */}
           <Outlet />
         </div>
       </div>
