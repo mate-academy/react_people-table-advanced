bf415da107aa0b72bd0d5234d6f0fa3c913d4b91
Medvid Oleksandr <samaster010604@gmail.com>
2024-12-12 19:05:49 +0200

    People Table Advanced

--- src/components/PersonLink/PersonLink.tsx
+++ src/components/PersonLink/PersonLink.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { FC } from 'react';
 import cn from 'classnames';
 import { Person } from '../../types';
@@ -10,10 +10,14 @@ type Props = {
 
 export const PersonLink: FC<Props> = ({ person }) => {
   const { slug, sex, name } = person;
+  const [searchParams] = useSearchParams();
 
   return (
     <Link
-      to={`/people/${slug}`}
+      to={{
+        pathname: `/people/${slug}`,
+        search: searchParams.toString(),
+      }}
       className={cn({ 'has-text-danger': sex === 'f' })}
     >
       {name}
