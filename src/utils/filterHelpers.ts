3ed301748ccec5f04eca76f71772e80ffb3417a3
Yuliia Zubenko <95251336+yuliiazubenko@users.noreply.github.com>
2025-01-02 14:56:34 +0100

    solution

---
+++ src/utils/filterHelpers.ts
@@ -0,0 +1,9 @@
+export const TableHeaders = ['Name', 'Sex', 'Born', 'Died'];
+
+export const centuries = ['16', '17', '18', '19', '20'];
+
+export enum SexFilter {
+  All = 'all',
+  Female = 'f',
+  Male = 'm',
+}
