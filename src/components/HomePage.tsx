d09667bf9bdb2450ac86f67ddb981ae1d904e7fd
Oleksandr Andrushchenko <aandrushchenko1402@gmail.com>
2025-01-07 23:11:24 +0200

    Solution

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+export const HomePage: React.FC<Props> = props => {
+  const {} = props;
+
+  return <h1 className="title">Home Page</h1>;
+};
---
+++ src/components/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+export const NotFoundPage: React.FC<Props> = props => {
+  const {} = props;
+
+  return <h1 className="title">Page not found</h1>;
+};
