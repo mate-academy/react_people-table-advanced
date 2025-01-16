358f00ab39a2d405e842ec1d89b07c1004e610e8
Viktoriia <vkkishko@gmail.com>
2025-01-16 19:05:36 +0200

    draft7

--- src/utils/useFilter.ts
+++ src/utils/useFilter.ts
@@ -4,23 +4,24 @@ import { useState } from 'react';
 export function useFilter(
   people: Person[],
 ): [Person[], (params: URLSearchParams) => void] {
-  const [filteredPeople, setFilteredPeople] = useState<Person[]>([]);
-
+  const [filteredPeople, setFilteredPeople] = useState<Person[]>(people);
   const filter = (params: URLSearchParams) => {
     const paramsExist = params.toString() !== '';
 
     if (!paramsExist) {
       setFilteredPeople(people);
     } else {
-      const filtered = filteredPeople.filter((person: Person) => {
+      const filtered = people.filter((person: Person) => {
         const personCentury = Math.ceil(person.born / 100).toString();
         const paramsQuery = params.get('query') || '';
-
         const hasMatchingCentury = params
           .getAll('century')
           .includes(personCentury);
-        const hasMatchingSex = person.sex === params.get('sex');
-        const hasMatchingName = person.name.includes(paramsQuery);
+        const hasMatchingSex =
+          params.get('sex') === null || person.sex === params.get('sex');
+        const hasMatchingName =
+          paramsQuery === '' ||
+          person.name.toLowerCase().includes(paramsQuery.toLowerCase());
 
         return hasMatchingCentury || hasMatchingSex || hasMatchingName;
       });
