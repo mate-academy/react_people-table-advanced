74bdacebfa2ae41faa76ecc292e61859e6aa9550
Oleksandr Rad <clavigo365@gmail.com>
2025-01-27 16:48:57 +0200

    save changes

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,62 @@
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { useEffect, useMemo, useState } from 'react';
+import { Person } from '../types';
+import { useLocation, useParams } from 'react-router-dom';
+import { getPeople } from '../api';
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const currentPerson = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const { search } = useLocation();
+
+  useEffect(() => {
+    setIsLoading(true);
+    setIsError(false);
+
+    getPeople()
+      .then(setPeople)
+      .catch(() => setIsError(true))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  const filteredPeople = useMemo(() => {
+    const searchParams = new URLSearchParams(search);
+    const sexFilter = searchParams.get('sex');
+    const queryFilter = searchParams.get('query');
+    const centuriesFilter = searchParams.getAll('centuries');
+
+    return people.filter(person => {
+      if (sexFilter && person.sex !== sexFilter) {
+        return false;
+      }
+
+      if (
+        queryFilter &&
+        !person.name.toLowerCase().includes(queryFilter.toLowerCase()) &&
+        !person.motherName?.toLowerCase().includes(queryFilter.toLowerCase()) &&
+        !person.fatherName?.toLowerCase().includes(queryFilter.toLowerCase())
+      ) {
+        return false;
+      }
+
+      if (centuriesFilter && centuriesFilter.length > 0) {
+        const birthCentury = Math.ceil(person.born / 100).toString(); // Століття як рядок
+
+        if (!centuriesFilter.includes(birthCentury)) {
+          return false;
+        } // Якщо століття не в масиві, виключаємо
+      }
+
+      return true;
+    });
+  }, [search, people]);
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -10,20 +64,36 @@ export const PeoplePage = () => {
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
           <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
+            {people.length > 0 && <PeopleFilters />}
           </div>
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
+              {isLoading && <Loader />}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+              {isError && (
+                <p data-cy="peopleLoadingError" className="has-text-danger">
+                  Something went wrong
+                </p>
+              )}
 
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
+              {people.length === 0 && !isError && !isLoading && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
 
-              <p>There are no people matching the current search criteria</p>
+              {filteredPeople.length === 0 && (
+                <p>There are no people matching the current search criteria</p>
+              )}
 
-              <PeopleTable />
+              {filteredPeople.length > 0 && (
+                <PeopleTable
+                  people={people}
+                  filteredPeople={filteredPeople}
+                  currentPerson={currentPerson.personSlug}
+                />
+              )}
             </div>
           </div>
         </div>
