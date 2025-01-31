b344bc1a72180ef814ce83f4a36dc5e99ac50fa8
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2025-01-31 09:21:19 +0200

    add solution

--- src/components/PeopleTable/PeopleTable .tsx
+++ src/components/PeopleTable/PeopleTable .tsx
@@ -1,27 +1,88 @@
-import { useState } from 'react';
 import { Person } from '../../types';
 import { PeopleLink } from '../PeopleLink/PeopleLink';
-import { SortKey, SortState } from '../../types/Sort';
+import { SortKey } from '../../types/Sort';
 import classNames from 'classnames';
 import { NavLink, useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export const PeopleTable: React.FC<{ people: Person[] }> = ({ people }) => {
-  const [sortState, setSortState] = useState<SortState>({
-    name: null,
-    sex: null,
-    born: null,
-    died: null,
-  });
-
+  const [filtredPeople, setFiltredPeople] = useState<Person[]>(people);
   const [searchParams, setSearchParams] = useSearchParams();
-  const sort = searchParams.get('sort') || '';
-  const order = searchParams.get('order') || '';
+  const currentSort = searchParams.get('sort');
+  const currentOrder = searchParams.get('order');
+  const query = searchParams.get('query') || '';
+  const sex = searchParams.get('sex') || '';
+  const centuries = searchParams.getAll('centuries') || [];
+
+  const filterByQuery = (allPeople: Person[]): Person[] => {
+    const lowerQuery = query.toLowerCase();
+
+    return allPeople.filter(
+      item =>
+        item.name.toLowerCase().includes(lowerQuery) ||
+        item.fatherName?.toLowerCase().includes(lowerQuery) ||
+        item.motherName?.toLowerCase().includes(lowerQuery),
+    );
+  };
+
+  const filterBySex = (allPeople: Person[]): Person[] => {
+    return allPeople.filter(item => item.sex === sex);
+  };
+
+  const filterByCenturies = (allPeople: Person[]): Person[] => {
+    return allPeople.filter(item => {
+      const century = Math.ceil(item.born / 100).toString();
+
+      return centuries.includes(century);
+    });
+  };
+
+  const filterPeople = (allPeople: Person[]): Person[] => {
+    let filteredPeople = allPeople;
+
+    if (query) {
+      filteredPeople = filterByQuery(filteredPeople);
+    }
+
+    if (sex) {
+      filteredPeople = filterBySex(filteredPeople);
+    }
+
+    if (centuries.length > 0) {
+      filteredPeople = filterByCenturies(filteredPeople);
+    }
+
+    return filteredPeople;
+  };
+
+  const sortPeople = (allPeople: Person[]): Person[] => {
+    if (!currentSort) {
+      return allPeople;
+    }
+
+    const key = currentSort as keyof Person;
+    const multiplier = currentOrder === 'desc' ? -1 : 1;
+
+    return [...allPeople].sort((a, b) => {
+      if (typeof a[key] === 'string' && typeof b[key] === 'string') {
+        return (a[key] as string).localeCompare(b[key] as string) * multiplier;
+      }
+
+      return ((a[key] as number) - (b[key] as number)) * multiplier;
+    });
+  };
+
+  useEffect(() => {
+    let updatedPeople = filterPeople(people);
+
+    updatedPeople = sortPeople(updatedPeople);
+
+    setFiltredPeople(updatedPeople);
+  }, [people, query, sex, centuries, currentSort, currentOrder]);
 
   const toggleSort = (event: React.MouseEvent, key: SortKey) => {
     event.preventDefault();
     const params = new URLSearchParams(searchParams);
-    const currentSort = params.get('sort');
-    const currentOrder = params.get('order');
 
     if (!currentSort) {
       params.set('sort', key);
@@ -38,41 +99,11 @@ export const PeopleTable: React.FC<{ people: Person[] }> = ({ people }) => {
     setSearchParams(params);
   };
 
-  // const toggleSort = (key: SortKey) => {
-  //   setSortState(currentSortState => {
-  //     const newSortState = { ...currentSortState };
-
-  //     Object.keys(newSortState).forEach(k => {
-  //       if (k !== key) {
-  //         newSortState[k as SortKey] = null;
-  //       }
-  //     });
-
-  //     switch (newSortState[key]) {
-  //       case null: {
-  //         newSortState[key] = 'asc';
-  //         break;
-  //       }
-
-  //       case 'asc': {
-  //         newSortState[key] = 'desc';
-  //         break;
-  //       }
-
-  //       default:
-  //         newSortState[key] = null;
-  //         break;
-  //     }
-
-  //     return newSortState;
-  //   });
-  // };
-
   const getSortIconClass = (key: SortKey) => {
     return classNames('fas', {
-      'fa-sort-up': sortState[key] === 'asc',
-      'fa-sort-down': sortState[key] === 'desc',
-      'fa-sort': sortState[key] === null,
+      'fa-sort-up': currentSort === key && currentOrder !== 'desc',
+      'fa-sort-down': currentSort === key && currentOrder === 'desc',
+      'fa-sort': currentSort !== key,
     });
   };
 
@@ -91,11 +122,11 @@ export const PeopleTable: React.FC<{ people: Person[] }> = ({ people }) => {
             <span className="is-flex is-flex-wrap-nowrap">
               Name
               <NavLink to={`#`}>
-                <span className="icon">
-                  <i
-                    className={getSortIconClass(SortKey.name)}
-                    onClick={event => toggleSort(event, SortKey.name)}
-                  />
+                <span
+                  className="icon"
+                  onClick={event => toggleSort(event, SortKey.name)}
+                >
+                  <i className={getSortIconClass(SortKey.name)} />
                 </span>
               </NavLink>
             </span>
@@ -104,11 +135,11 @@ export const PeopleTable: React.FC<{ people: Person[] }> = ({ people }) => {
             <span className="is-flex is-flex-wrap-nowrap">
               Sex
               <NavLink to={`#`}>
-                <span className="icon">
-                  <i
-                    className={getSortIconClass(SortKey.sex)}
-                    onClick={event => toggleSort(event, SortKey.sex)}
-                  />
+                <span
+                  className="icon"
+                  onClick={event => toggleSort(event, SortKey.sex)}
+                >
+                  <i className={getSortIconClass(SortKey.sex)} />
                 </span>
               </NavLink>
             </span>
@@ -117,11 +148,11 @@ export const PeopleTable: React.FC<{ people: Person[] }> = ({ people }) => {
             <span className="is-flex is-flex-wrap-nowrap">
               Born
               <NavLink to={`#`}>
-                <span className="icon">
-                  <i
-                    className={getSortIconClass(SortKey.born)}
-                    onClick={event => toggleSort(event, SortKey.born)}
-                  />
+                <span
+                  className="icon"
+                  onClick={event => toggleSort(event, SortKey.born)}
+                >
+                  <i className={getSortIconClass(SortKey.born)} />
                 </span>
               </NavLink>
             </span>
@@ -130,11 +161,11 @@ export const PeopleTable: React.FC<{ people: Person[] }> = ({ people }) => {
             <span className="is-flex is-flex-wrap-nowrap">
               Died
               <NavLink to={`#`}>
-                <span className="icon">
-                  <i
-                    className={getSortIconClass(SortKey.died)}
-                    onClick={event => toggleSort(event, SortKey.died)}
-                  />
+                <span
+                  className="icon"
+                  onClick={event => toggleSort(event, SortKey.died)}
+                >
+                  <i className={getSortIconClass(SortKey.died)} />
                 </span>
               </NavLink>
             </span>
@@ -144,7 +175,7 @@ export const PeopleTable: React.FC<{ people: Person[] }> = ({ people }) => {
         </tr>
       </thead>
       <tbody>
-        {people.map(person => {
+        {filtredPeople.map(person => {
           return <PeopleLink key={person.slug} {...person} />;
         })}
       </tbody>
