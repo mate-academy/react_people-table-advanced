e515dfd43c6836ced1a2ac4add85e28d1ea895b1
Denys Raboshchuk <kontroler444@gmail.com>
2025-02-17 12:09:07 +0200

    Add filtering and sorting to React People Table

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,41 @@
+import classNames from 'classnames';
+import { NavLink, useSearchParams } from 'react-router-dom';
+
+export const Navigation = () => {
+  const [searchParams] = useSearchParams();
+
+  return (
+    <nav
+      data-cy="nav"
+      className="navbar is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink
+            className={({ isActive }) =>
+              classNames('navbar-item', {
+                'has-background-grey-lighter': isActive,
+              })
+            }
+            to="/"
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              classNames('navbar-item', {
+                'has-background-grey-lighter': isActive,
+              })
+            }
+            to={{ pathname: '/people', search: searchParams.toString() }}
+          >
+            People
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
