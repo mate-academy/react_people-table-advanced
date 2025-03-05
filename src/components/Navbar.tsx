88ecae4296682e8acaa409990646dfdd275ed95a
Dmytro Moshkivskyi <dmosh@ukr.net>
2025-03-05 17:28:11 +0200

    react_people-table-advanced by Dimoshua

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,12 @@
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+
+const getLinkActive = ({ isActive }: { isActive: boolean }) => {
+  return classNames('navbar-item', {
+    'has-background-grey-lighter': isActive,
+  });
+};
+
 export const Navbar = () => {
   return (
     <nav
@@ -8,17 +17,13 @@ export const Navbar = () => {
     >
       <div className="container">
         <div className="navbar-brand">
-          <a className="navbar-item" href="#/">
+          <NavLink to="/" className={getLinkActive}>
             Home
-          </a>
+          </NavLink>
 
-          <a
-            aria-current="page"
-            className="navbar-item has-background-grey-lighter"
-            href="#/people"
-          >
+          <NavLink to="/people" aria-current="page" className={getLinkActive}>
             People
-          </a>
+          </NavLink>
         </div>
       </div>
     </nav>
