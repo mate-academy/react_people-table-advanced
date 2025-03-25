12e73801ac9688e52c88651c87e5cf610cb64864
Taras Stanishevskyy <stanishevskyy98@gmail.com>
2025-03-25 15:16:49 +0200

    add structure for peoplePage

---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { PersonLink } from '../PersonLink';
+import { Person } from '../../types';
+
+type Props = {
+  people: Person[];
+};
+
+/* eslint-disable jsx-a11y/control-has-associated-label */
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  const getPeopleSlug = (parentName: string): Person | undefined => {
+    return people.find(person => person.name === parentName);
+  };
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Name
+              <a href="#/people?sort=name">
+                <span className="icon">
+                  <i className="fas fa-sort" />
+                </span>
+              </a>
+            </span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Sex
+              <a href="#/people?sort=sex">
+                <span className="icon">
+                  <i className="fas fa-sort" />
+                </span>
+              </a>
+            </span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Born
+              <a href="#/people?sort=born&amp;order=desc">
+                <span className="icon">
+                  <i className="fas fa-sort-up" />
+                </span>
+              </a>
+            </span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Died
+              <a href="#/people?sort=died">
+                <span className="icon">
+                  <i className="fas fa-sort" />
+                </span>
+              </a>
+            </span>
+          </th>
+
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <PersonLink
+            person={person}
+            key={person.slug}
+            getPeopleSlug={getPeopleSlug}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
