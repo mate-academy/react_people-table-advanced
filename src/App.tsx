6195150926c5d9ac0be5f62558b27e36549985da
Sofiia Pelenska <soniapelenskaoe2002@gmail.com>
2025-04-10 17:02:42 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss';
 import { Outlet } from 'react-router-dom';
-import { NavBar } from './components/NavBar';
+import { NavBar } from './components/NavBar.tsx';
 
 export const App = () => (
   <div data-cy="app">
