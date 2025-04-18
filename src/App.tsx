91dcadafa1fb8807cd49e5b7e524ecbe86f1195d
Yurij Lehin <yurijgreyd03@gmail.com>
2025-04-18 11:46:25 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { PeoplePage } from './components/PeoplePage';
 import { Navbar } from './components/Navbar';
 
 import './App.scss';
-
-export const App = () => {
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+export const App: React.FC = () => {
   return (
     <div data-cy="app">
       <Navbar />
 
-      <div className="section">
+      <main className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <Outlet />
         </div>
-      </div>
+      </main>
     </div>
   );
 };
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const HomePage: React.FC = () => <h1 className="title">Home Page</h1>;
