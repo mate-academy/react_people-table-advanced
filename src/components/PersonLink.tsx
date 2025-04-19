de55a9e2fdb0156f4f76b08e592b6819dd297447
Anna Tuhusova <bilachayka@mail.com>
2025-04-19 11:26:34 +0200

    some commit message

--- src/components/PersonLink.tsx
+++ src/components/PersonLink.tsx
@@ -1,19 +1,23 @@
 import { Link } from 'react-router-dom';
 import { Person } from '../types/Person';
 import cn from 'classnames';
+import { useLocation } from 'react-router-dom';
+
 
 type Props = {
   person: Person | null;
 };
 
 export const PersonLink: React.FC<Props> = ({ person }) => {
+  const location = useLocation();
+
   if (!person) {
     return <span>-</span>;
   }
 
   return (
     <Link
-      to={`/people/${person.slug}`}
+      to={`/people/${person.slug}${location.search}`}
       className={cn({ 'has-text-danger': person.sex === 'f' })}
     >
       {person.name}
