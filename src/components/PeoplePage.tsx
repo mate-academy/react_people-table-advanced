e24e3ce93144152e24c2fba8bf53494ff326b1a4
beliyandr <beliyandr@gmail.com>
2025-04-23 22:47:46 +0300

    start

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,50 @@
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { Person } from '../types';
+import { useEffect, useState } from 'react';
+import { set } from 'cypress/types/lodash';
+import { getPeople } from '../api';
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  const makeFullPeopleInfo = (people: Person[]) => {
+    const convertedPeopleObjects = people.reduce<Record<string, Person>>(
+      (acc, person) => {
+        return {
+          ...acc,
+          [person.name]: person,
+        };
+      },
+      {},
+    );
+
+    return people.map(person => {
+      return {
+        ...person,
+        mother: person.motherName
+          ? convertedPeopleObjects[person.motherName]
+          : null,
+        father: person.fatherName
+          ? convertedPeopleObjects[person.fatherName]
+          : null,
+      };
+    });
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    getPeople()
+      .then(data => setPeople(makeFullPeopleInfo(data)))
+      .catch(() => setError('Something went wrong'))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -16,14 +58,17 @@ export const PeoplePage = () => {
           <div className="column">
             <div className="box table-container">
               <Loader />
+              {error && <p data-cy="peopleLoadingError">{error}</p>}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
-
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
+              {!loading && !error && people.length > 0 && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
 
               <p>There are no people matching the current search criteria</p>
 
-              <PeopleTable />
+              <PeopleTable people={people} />
             </div>
           </div>
         </div>
