a5a171ed9a7d8334ff18910afcc74d656bc9c587
beliyandr <beliyandr@gmail.com>
2025-05-02 18:47:14 +0300

    4

--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -3,6 +3,7 @@ import { Person } from '../types';
 import { Link, NavLink, useParams, useSearchParams } from 'react-router-dom';
 import { PersonLink } from './PersonLink';
 import { getSearchWith } from '../utils/searchHelper';
+import classNames from 'classnames';
 
 type Props = {
   people: Person[];
@@ -15,81 +16,84 @@ export const PeopleTable: FC<Props> = ({ people }) => {
   const [sortName, setSortName] = useState<string>('');
   const [sortOrder, setSortOrder] = useState('');
 
-  const filterderPeople = (people: Person[], sort, order: string = '') => {
-    if (sort) {
-      const sortedPeople = [...people].sort((a, b) => {
-        if (sort === 'name') {
-          return a.name.localeCompare(b.name);
-        } else if (sort === 'born') {
-          return a.born - b.born;
-        } else if (sort === 'died') {
-          return a.died - b.died;
-        }
-        return 0;
-      });
+  // const filterderPeople = (people: Person[], sort, order: string = '') => {
+  //   if (sort) {
+  //     const sortedPeople = [...people].sort((a, b) => {
+  //       if (sort === 'name') {
+  //         return a.name.localeCompare(b.name);
+  //       } else if (sort === 'born') {
+  //         return a.born - b.born;
+  //       } else if (sort === 'died') {
+  //         return a.died - b.died;
+  //       }
+  //       return 0;
+  //     });
 
-      if (order === 'desc') {
-        sortedPeople.reverse();
-      }
+  //     if (order === 'desc') {
+  //       sortedPeople.reverse();
+  //     }
 
-      return sortedPeople;
-    }
-    return people;
-  };
+  //     return sortedPeople;
+  //   }
+  //   return people;
+  // };
 
-  const sortPeople = (people: Person[]) => {
+  const handleSort = () => {
     if (searchParams.get('sort') !== sortName) {
       setSortName(searchParams.get('sort') || '');
+      setSortOrder('asc');
       // filterderPeople(people);
     }
-    if (searchParams.get('sort') === sortName && sortName === '') {
-      setSortOrder(searchParams.get('order') || '');
+    if (searchParams.get('sort') === sortName && sortOrder === 'asc') {
+      setSortOrder('desc');
     }
-    if (searchParams.get('sort') === sortName && sortName === 'desc') {
+    if (searchParams.get('sort') === sortName && sortOrder === 'desc') {
       setSortOrder('');
       setSortName('');
-      return people;
     }
-
-    setSortName(prevSortName => {
-      if (!prevSortName) {
-        return searchParams.get('sort') || '';
-      }
-      if (prevSortName !== sortName) {
-        setOrderFlag(false);
-        setSortOrder('');
-        return searchParams.get('sort') || '';
-      }
-      if (prevSortName === sortName && !orderFlag) {
-        setOrderFlag(true);
-        setSortOrder('desc');
-      }
-      if (prevSortName === sortName && orderFlag) {
-        setOrderFlag(false);
-        setSortOrder('');
-        setSortName('');
-      }
-
-      return '';
-    });
-
-    const sortedPeople = [...people].sort((a, b) => {
-      if (sortName === 'name') {
-        return a.name.localeCompare(b.name);
-      } else if (sortName === 'born') {
-        return a.born - b.born;
-      } else if (sortName === 'died') {
-        return a.died - b.died;
-      }
-      return 0;
-    });
-
-    if (sortOrder === 'desc') {
-      sortedPeople.reverse();
-    }
-
-    return sortedPeople;
   };
+  console.log('sortName', sortName);
+  console.log('sortOrder', sortOrder);
+
+  //   setSortName(prevSortName => {
+  //     if (!prevSortName) {
+  //       return searchParams.get('sort') || '';
+  //     }
+  //     if (prevSortName !== sortName) {
+  //       setOrderFlag(false);
+  //       setSortOrder('');
+  //       return searchParams.get('sort') || '';
+  //     }
+  //     if (prevSortName === sortName && !orderFlag) {
+  //       setOrderFlag(true);
+  //       setSortOrder('desc');
+  //     }
+  //     if (prevSortName === sortName && orderFlag) {
+  //       setOrderFlag(false);
+  //       setSortOrder('');
+  //       setSortName('');
+  //     }
+
+  //     return '';
+  //   });
+
+  //   const sortedPeople = [...people].sort((a, b) => {
+  //     if (sortName === 'name') {
+  //       return a.name.localeCompare(b.name);
+  //     } else if (sortName === 'born') {
+  //       return a.born - b.born;
+  //     } else if (sortName === 'died') {
+  //       return a.died - b.died;
+  //     }
+  //     return 0;
+  //   });
+
+  //   if (sortOrder === 'desc') {
+  //     sortedPeople.reverse();
+  //   }
+
+  //   return sortedPeople;
+  // };
 
   return (
     <table
@@ -105,9 +109,7 @@ export const PeopleTable: FC<Props> = ({ people }) => {
                 to={{
                   search: getSearchWith(searchParams, { sort: 'name' }),
                 }}
-                onClick={() => {
-                  setSortName(searchParams.get('sort') || '');
-                }}
+                onClick={handleSort}
               >
                 <span className="icon">
                   <i className="fas fa-sort" />
@@ -123,9 +125,17 @@ export const PeopleTable: FC<Props> = ({ people }) => {
                 to={{
                   search: getSearchWith(searchParams, { sort: 'sex' }),
                 }}
+                onClick={handleSort}
               >
                 <span className="icon">
-                  <i className="fas fa-sort" />
+                  <i
+                    className={classNames('fas', {
+                      'fa-sort': sortOrder === '' && sortName !== 'sex',
+                      'fa-sort-up': sortOrder === 'asc' && sortName === 'sex',
+                      'fa-sort-down':
+                        sortOrder === 'desc' && sortName === 'sex',
+                    })}
+                  />
                 </span>
               </Link>
             </span>
@@ -138,6 +148,7 @@ export const PeopleTable: FC<Props> = ({ people }) => {
                 to={{
                   search: getSearchWith(searchParams, { sort: 'born' }),
                 }}
+                onClick={handleSort}
               >
                 <span className="icon">
                   <i className="fas fa-sort-up" />
