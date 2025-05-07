92448fe6d96e86dd02880a64aa8ab6414720a5e0
LuisCampelo2 <luisfilipecab@gmail.com>
2025-05-07 20:58:11 -0300

    primeiro commit

--- src/api.ts
+++ src/api.ts
@@ -4,9 +4,13 @@ import { Person } from './types/Person';
 const API_URL =
   'https://mate-academy.github.io/react_people-table/api/people.json';
 
-function wait(delay: number) {
+export function wait(delay: number) {
   return new Promise(resolve => setTimeout(resolve, delay));
 }
+
+export const getSlug = (name: string, born: number) => {
+  return `${name.toLowerCase().replace(/\s+/g, '-')}-${born}`;
+};
 
 export async function getPeople(): Promise<Person[]> {
   // keep this delay for testing purpose
