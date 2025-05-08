79c4aebd40bb9bf58996dfd695006eeae4318bf6
sirserg <sirserg05@gmail.com>
2025-05-08 15:22:25 +0300

    feat: add task solution

---
+++ src/components/PeopleFilters/PeopleFilters.tsx
@@ -0,0 +1,110 @@
+import { useSearchParams } from 'react-router-dom';
+import { getSearchWith } from '../../utils/searchHelper';
+import classNames from 'classnames';
+import { SearchLink } from '../SearchLink/SearchLink';
+
+export const PeopleFilters = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') || '';
+  const centuries = searchParams.getAll('centuries') || [];
+
+  const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchParams(
+      getSearchWith(searchParams, { query: e.target.value || null }),
+    );
+  };
+
+  const getCenturies = (century: string) => {
+    const newCenturies = centuries.includes(century)
+      ? centuries.filter(c => century !== c)
+      : [...centuries, century];
+
+    return newCenturies;
+  };
+
+  return (
+    <nav className="panel">
+      <p className="panel-heading">Filters</p>
+
+      <p className="panel-tabs" data-cy="SexFilter">
+        <SearchLink
+          className={classNames({ 'is-active': !searchParams.has('sex') })}
+          params={{ sex: null }}
+        >
+          All
+        </SearchLink>
+        <SearchLink
+          className={classNames({
+            'is-active': searchParams.get('sex') === 'm',
+          })}
+          params={{ sex: 'm' }}
+        >
+          Male
+        </SearchLink>
+        <SearchLink
+          className={classNames({
+            'is-active': searchParams.get('sex') === 'f',
+          })}
+          params={{ sex: 'f' }}
+        >
+          Female
+        </SearchLink>
+      </p>
+
+      <div className="panel-block">
+        <p className="control has-icons-left">
+          <input
+            data-cy="NameFilter"
+            type="search"
+            className="input"
+            placeholder="Search"
+            value={query}
+            onChange={handleQueryChange}
+          />
+
+          <span className="icon is-left">
+            <i className="fas fa-search" aria-hidden="true" />
+          </span>
+        </p>
+      </div>
+
+      <div className="panel-block">
+        <div className="level is-flex-grow-1 is-mobile" data-cy="CenturyFilter">
+          <div className="level-left">
+            {[16, 17, 18, 19, 20].map(century => (
+              <SearchLink
+                key={century}
+                data-cy="century"
+                className={classNames('button mr-1', {
+                  'is-info': searchParams.has('centuries', century.toString()),
+                })}
+                params={{ centuries: getCenturies(century.toString()) }}
+              >
+                {century}
+              </SearchLink>
+            ))}
+          </div>
+
+          <div className="level-right ml-4">
+            <SearchLink
+              data-cy="centuryALL"
+              className="button is-success is-outlined"
+              params={{ centuries: [] }}
+            >
+              All
+            </SearchLink>
+          </div>
+        </div>
+      </div>
+
+      <div className="panel-block">
+        <SearchLink
+          className="button is-link is-outlined is-fullwidth"
+          params={{ sex: null, query: null, centuries: [] }}
+        >
+          Reset all filters
+        </SearchLink>
+      </div>
+    </nav>
+  );
+};
---
+++ src/components/PersonLink/PersonLink.tsx
@@ -0,0 +1,80 @@
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { Person } from '../../types';
+import classNames from 'classnames';
+
+type Props = {
+  person: Person;
+  findInList: (name: string) => Person | undefined;
+};
+
+export const PersonLink: React.FC<Props> = ({ person, findInList }) => {
+  const { search } = useLocation();
+
+  const {
+    slug: personSlug,
+    name,
+    sex,
+    born,
+    died,
+    motherName,
+    fatherName,
+  } = person;
+
+  const { slug } = useParams();
+
+  const mother = findInList(motherName || '');
+  const father = findInList(fatherName || '');
+
+  return (
+    <tr
+      key={personSlug}
+      data-cy="person"
+      className={classNames({ 'has-background-warning': personSlug === slug })}
+    >
+      <td>
+        <Link
+          to={{ pathname: `${personSlug}`, search }}
+          className={classNames({
+            'has-text-danger': sex === 'f',
+          })}
+        >
+          {name}
+        </Link>
+      </td>
+
+      <td>{sex}</td>
+      <td>{born}</td>
+      <td>{died}</td>
+      <td>
+        {motherName ? (
+          mother ? (
+            <Link
+              className="has-text-danger"
+              to={{ pathname: `${mother.slug}`, search }}
+            >
+              {motherName}
+            </Link>
+          ) : (
+            motherName
+          )
+        ) : (
+          '-'
+        )}
+      </td>
+
+      <td>
+        {fatherName ? (
+          father ? (
+            <Link to={{ pathname: `${father.slug}`, search }}>
+              {fatherName}
+            </Link>
+          ) : (
+            fatherName
+          )
+        ) : (
+          '-'
+        )}
+      </td>
+    </tr>
+  );
+};
