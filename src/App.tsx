0b7079ba47771c0318b983a1f6a7df08fb74b1a0
Lirik <melnicukkiril2002@gmail.com>
2025-05-22 21:41:08 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { PeoplePage } from './components/PeoplePage';
 import { Navbar } from './components/Navbar';
-
 import './App.scss';
+import { Outlet } from 'react-router-dom';
 
 export const App = () => {
   return (
@@ -10,9 +9,10 @@ export const App = () => {
 
       <div className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          {/* <h1 className="title">Home Page</h1>
+          <h1 className="title">Page not found</h1> */}
+          {/* <PeoplePage /> */}
+          <Outlet />
         </div>
       </div>
     </div>
