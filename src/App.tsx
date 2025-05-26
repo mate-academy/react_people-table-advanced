26470b12239f22cc5ec501dd7d9710481b5c0fc8
Ralfendeck25 <poseidoncenghizerzhemetmhossad@outlook.com>
2025-05-26 00:54:19 -0300

    Prodigue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.scss';
 import { Outlet } from 'react-router-dom';
-import { NavBar } from './components/NavBar/NavBar';
+import { Navbar } from './components/Navbar/Navbar';
 
 export const App = () => (
   <div data-cy="app">
-    <NavBar />
+    <Navbar />
     <main className="section">
       <div className="section">
         <div className="container">
