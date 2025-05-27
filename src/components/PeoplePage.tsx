60f10c9f075ed32148676cebae9738add183f2ec
Mateusz Cieplak <m.cieplak97@gmail.com>
2025-05-27 14:45:35 +0200

    Task solution

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,29 +1,73 @@
+/* eslint-disable prettier/prettier */
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { ErrorMessage } from '../types/ErrorMessage';
+import { useEffect, useState } from 'react';
+import { Person } from '../types';
+import { getPeople } from '../api';
 
 export const PeoplePage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<ErrorMessage | null>(null);
+  const [people, setPeople] = useState<Person[]>([]);
+
+  const handleErrorMessage = (error: ErrorMessage | null) => {
+    setErrorMessage(error);
+  };
+
+  useEffect(() => {
+    if (people.length === 0) {
+      setIsLoading(true);
+      getPeople()
+        .then(response => {
+          if (response.length === 0) {
+            handleErrorMessage(ErrorMessage.NO_PEOPLE_ON_SERVER);
+          } else {
+            setPeople(response);
+          }
+        })
+        .catch(() => handleErrorMessage(ErrorMessage.OTHER_ERRORS))
+        .finally(() => setIsLoading(false));
+    }
+  }, []);
+
   return (
     <>
       <h1 className="title">People Page</h1>
 
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
-          <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
-          </div>
+          {!isLoading &&
+            errorMessage !== ErrorMessage.OTHER_ERRORS &&
+            errorMessage !== ErrorMessage.NO_PEOPLE_ON_SERVER && (
+            <div className="column is-7-tablet is-narrow-desktop">
+              <PeopleFilters />
+            </div>
+          )}
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
-
-              <p data-cy="peopleLoadingError">Something went wrong</p>
-
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
-
-              <p>There are no people matching the current search criteria</p>
-
-              <PeopleTable />
+              {isLoading && <Loader />}
+              {errorMessage === ErrorMessage.NO_PEOPLE_ON_SERVER && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
+              {errorMessage === ErrorMessage.OTHER_ERRORS && (
+                <p data-cy="peopleLoadingError">Something went wrong</p>
+              )}
+              {errorMessage === ErrorMessage.NO_PEOPLE_MATCHING && (
+                <p>There are no people matching the current search criteria</p>
+              )}
+              {!isLoading &&
+                errorMessage !== ErrorMessage.OTHER_ERRORS &&
+                errorMessage !== ErrorMessage.NO_PEOPLE_ON_SERVER && (
+                <PeopleTable
+                  people={people}
+                  handleErrorMessage={handleErrorMessage}
+                />
+              )}
             </div>
           </div>
         </div>
---
+++ src/types/ErrorMessage.ts
@@ -0,0 +1,5 @@
+export enum ErrorMessage {
+  NO_PEOPLE_ON_SERVER = 'There are no people on the server',
+  OTHER_ERRORS = 'Something went wrong',
+  NO_PEOPLE_MATCHING = 'There are no people matching the current search',
+}
